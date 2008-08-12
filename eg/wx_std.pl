9e8d473032899a33c076441754be813f2eeae6c7
Gabor Szabo <szabgab@gmail.com>
2008-08-12 05:54:24 +0000

    separet the Padre::Demo from the wx_std.pl
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@20 18eddd70-d622-4293-bd05-fe65668e8bde

--- eg/wx_std.pl
+++ eg/wx_std.pl
@@ -2,86 +2,16 @@
 use strict;
 use warnings;
 
-$| = 1;
-
-my $app = Demo::App->new;
-$app->MainLoop;
+use lib 'lib';
+use Padre::Demo;
+Padre::Demo->run(\&main);
 
 sub main {
    my ($frame, $output) = @_;
-   $frame->EVT_ACTIVATE(sub {}); 
+
    my $name = $frame->prompt("What is your name?\n");
    $output->AddText("How are you $name today?\n");
 
-
    return;
-}
-
-
-package Demo::App;
-use strict;
-use warnings;
-use base 'Wx::App';
-
-
-our $frame;
-sub OnInit {
-    $frame = Demo::App::Frame->new;
-
-
-    $frame->Show( 1 );
-}
-
-package Demo::App::Frame;
-use strict;
-use warnings;
-use Wx qw(:everything);
-use Wx::Event qw(:everything);
-
-use base 'Wx::Frame';
-
-
-sub prompt {
-    my ($self, $text) = @_;
-
-    my $dialog = Wx::TextEntryDialog->new( $self, $text, "", '' );
-    if ($dialog->ShowModal == wxID_CANCEL) {
-        return;
-    }   
-    my $resp = $dialog->GetValue;
-    $dialog->Destroy;
-    return $resp;
-}
-
-sub new {
-    my ($class) = @_;
-
-    my $self = $class->SUPER::new( undef, -1,
-                                 'Demo::App',
-                                  wxDefaultPosition,  wxDefaultSize,
-                                 );
-
-
-use Wx::STC;
-#use Wx::StyledTextCtrl;
-    my $editor = Wx::StyledTextCtrl->new($self, -1, [-1, -1], [750, 700]);
-    $editor->SetMarginWidth(1, 0);
-
-    EVT_ACTIVATE($self, sub {main::main($_[0], $editor) });
-
-
-#    my $button = Wx::Button->new( $self, -1, "Run" );
-#    Wx::Event::EVT_BUTTON( $self, $button, sub {
-#         my ( $self, $event ) = @_;
-#         print "preparing to popup window...\n";
-#         Wx::MessageBox( "This is the smell of an Onion", "Title", wxOK|wxCENTRE, $self );
-#    });
-#    $self->SetSize($button->GetSizeWH);
-
-    Wx::Event::EVT_CLOSE( $self,  sub {
-         my ( $self, $event ) = @_;
-         $event->Skip;
-    });
-    return $self;
 }
 
