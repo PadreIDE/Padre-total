0b4dab52624dfcc18d0d9139ed4342a02907b3ff
Gabor Szabo <szabgab@gmail.com>
2008-08-17 15:09:09 +0000

    factor out the unused Padre::Wx::Popup code
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@63 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ lib/Padre/Wx/Popup.pm
@@ -0,0 +1,63 @@
+package Padre::Popup;
+use strict;
+use warnings;
+
+#use base 'Wx::ComboPopup';
+#use base 'Wx::PopupTransientWindow';
+#use base 'Wx::PopupWindow';
+use base qw(Wx::PlPopupTransientWindow);
+
+use Wx        qw(:everything);
+use Wx::Event qw(:everything);
+
+sub on_paint {
+    my( $self, $event ) = @_;
+#    my $dc = Wx::PaintDC->new( $self );
+#    $dc->SetBrush( Wx::Brush->new( Wx::Colour->new( 0, 192, 0 ), wxSOLID ) );
+#    $dc->SetPen( Wx::Pen->new( Wx::Colour->new( 0, 0, 0 ), 1, wxSOLID ) );
+#    $dc->DrawRectangle( 0, 0, $self->GetSize->x, $self->GetSize->y );
+
+    
+}
+sub new {
+    my $class = shift;
+    my $self = $class->SUPER::new(@_);
+EVT_PAINT( $self, \&on_paint);
+
+print "xxx $self\n";
+#    my $panel =  Wx::Panel->new( $self, -1 );
+#print "panel $panel\n";
+    #$panel->SetBackgroundColour(Wx::wxWHITE);
+#    $self->SetBackgroundColour(Wx::wxWHITE);
+#print "aa\n";
+#    my $dialog = Wx::Dialog->new( $self, -1, "", [-1, -1], [550, 200]);
+#print "d $dialog\n";
+
+#    my $st = Wx::StaticText->new($panel, -1, 
+#           "abc adsda\n" .
+#           "Some more\n" .
+#           "and more\n"
+#           , [10, 10], [-1, -1]);
+#print "zz $st\n";
+#    my $sz = $st->GetBestSize();
+#    $self->SetSize( ($sz->GetWidth()+20, $sz->GetHeight()+20) );
+    #$self->SetSize( $panel->GetSize());
+
+    return $self;
+}
+
+sub ProcessLeftDown {
+    my ($self, $event) = @_;
+    print "Process Left $event\n";
+    #$event->Skip;
+    return 0;
+}
+
+sub OnDismiss {
+    my ($self, $event) = @_;
+    print "OnDismiss\n";
+    #$event->Skip;
+}
+
+1;
+
