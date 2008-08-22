6c8ac1094828f21dda118f52957a569a43c26041
Adam Kennedy <adamk@cpan.org>
2008-08-22 14:03:56 +0000

    Clearer call method, and add a -d flag for debugging
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@80 18eddd70-d622-4293-bd05-fe65668e8bde

--- dev.pl
+++ dev.pl
@@ -10,7 +10,23 @@ use warnings;
 
 use FindBin;
 use Probe::Perl;
-$ENV{PADRE_DEV} = 1;
+$ENV{PADRE_DEV}  = 1;
 $ENV{PADRE_HOME} = $FindBin::Bin;
 my $perl = Probe::Perl->find_perl_interpreter;
-system qq["$perl" -I$FindBin::Bin/lib -I$FindBin::Bin/../plugins/par/lib $FindBin::Bin/script/padre @ARGV]; 
+my @cmd  = (
+        qq["$perl"],
+        qq[-I$FindBin::Bin/lib],
+        qq[-I$FindBin::Bin/../plugins/par/lib],
+);
+if ( grep { $_ eq '-d' } @ARGV ) {
+        @ARGV = grep { $_ ne '-d' } @ARGV;
+        push @cmd, '-d';
+}
+push @cmd, qq[$FindBin::Bin/script/padre], @ARGV;
+print join( ' ', @cmd ) . "\n";
+system( @cmd );
+
+#my $cmd  = qq["$perl" -I$FindBin::Bin/lib -I$FindBin::Bin/../plugins/par/lib $FindBin::Bin/script/padre @ARGV];
+#print $cmd . "\n";
+#system $cmd;
+
