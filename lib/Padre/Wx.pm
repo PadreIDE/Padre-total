5a863d15f7866f89b1271ff515434701ca49a63b
Gabor Szabo <szabgab@gmail.com>
2008-09-17 12:15:49 +0000

    ensure that we include and then find the shared files when building executable using PAR no need for File::ShareDir::PAR for this
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@303 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Wx.pm
+++ lib/Padre/Wx.pm
@@ -6,25 +6,28 @@ use 5.008;
 use strict;
 use warnings;
 use File::Spec;
-use File::ShareDir::PAR;
+use File::ShareDir;
 use Wx qw{
 	wxBITMAP_TYPE_XPM
 };
 
+sub _dir {
+    return $ENV{PADRE_DEV}      ?  File::Spec->catdir($FindBin::Bin, '..', 'share')
+         : $ENV{PADRE_PAR_PATH} ?  File::Spec->catdir($ENV{PADRE_PAR_PATH}, 'inc', 'share')
+         :                         File::ShareDir::dist_dir('Padre')
+         ;
+}
+
 sub bitmap {
     my $file = shift;
-    my $dir  = $ENV{PADRE_DEV}
-        ? File::Spec->catdir($FindBin::Bin, '..', 'share')
-        : File::ShareDir::PAR::dist_dir('Padre');
+    my $dir  = _dir();
     my $path = File::Spec->catfile($dir , 'docview', "$file.xpm");
     return Wx::Bitmap->new( $path, wxBITMAP_TYPE_XPM );
 }
 
 sub icon {
     my $file = shift;
-    my $dir  = $ENV{PADRE_DEV}
-        ? File::Spec->catdir($FindBin::Bin, '..', 'share')
-        : File::ShareDir::PAR::dist_dir('Padre');
+    my $dir  = _dir();
     my $path = File::Spec->catfile($dir , 'docview', "$file.xpm");
     return Wx::Icon->new( $path, wxBITMAP_TYPE_XPM );
 }
