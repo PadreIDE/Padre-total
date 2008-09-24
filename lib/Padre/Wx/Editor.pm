2ffca45844bc56aed970175e6811be94269f17a4
Gabor Szabo <szabgab@gmail.com>
2008-09-24 22:22:55 +0000

    turn the color definition into a hash
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@392 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Wx/Editor.pm
+++ lib/Padre/Wx/Editor.pm
@@ -9,18 +9,7 @@ our $VERSION = '0.10';
 use Wx::STC;
 use base 'Wx::StyledTextCtrl';
 
-use Wx   qw(wxTELETYPE wxNORMAL wxSTC_STYLE_DEFAULT wxSTC_H_TAG wxLayout_LeftToRight
-			wxSTC_PL_DEFAULT wxSTC_PL_ERROR wxSTC_PL_COMMENTLINE wxSTC_PL_POD wxSTC_PL_NUMBER 
-			wxSTC_PL_WORD wxSTC_PL_STRING wxSTC_PL_CHARACTER wxSTC_PL_PUNCTUATION 
-			wxSTC_PL_PREPROCESSOR wxSTC_PL_OPERATOR wxSTC_PL_IDENTIFIER wxSTC_PL_SCALAR 
-			wxSTC_PL_ARRAY wxSTC_PL_HASH wxSTC_PL_REGEX wxSTC_PL_REGSUBST wxSTC_PL_HERE_Q 
-			wxSTC_PL_STRING_Q wxSTC_PL_STRING_QQ wxSTC_PL_SYMBOLTABLE wxSTC_PL_LONGQUOTE
-			wxSTC_PL_BACKTICKS wxSTC_PL_DATASECTION wxSTC_PL_HERE_DELIM wxSTC_PL_HERE_QQ
-			wxSTC_PL_HERE_QX wxSTC_PL_STRING_QX wxSTC_PL_STRING_QR wxSTC_PL_STRING_QW
-	);
-	# missing SCE_PL_VARIABLE_INDEXER (16)  
-
-
+use Wx   qw( wxTELETYPE wxNORMAL wxSTC_STYLE_DEFAULT wxSTC_H_TAG wxLayout_LeftToRight );
 sub new {
 	my( $class, $parent ) = @_;
 
@@ -74,44 +63,53 @@ sub padre_setup_perl {
 
 	$self->padre_setup_plain;
 
+	no strict "refs";
+	#my $str = "wxSTC_PL_DEFAULT";
+	my %colors = (
+		wxSTC_PL_DEFAULT       => '00007f',
+		wxSTC_PL_ERROR         => 'ff0000',
+		wxSTC_PL_COMMENTLINE   => '007f00', # line green
+		wxSTC_PL_POD           => '7f7f7f',
+		wxSTC_PL_NUMBER        => '007f7f',
+		wxSTC_PL_WORD          => '00007f',
+		wxSTC_PL_STRING        => 'ff7f00',  # orange
+		wxSTC_PL_CHARACTER     => '7f007f',
+		wxSTC_PL_PUNCTUATION   => '000000',
+		wxSTC_PL_PREPROCESSOR  => '7f7f7f',
+		wxSTC_PL_OPERATOR      => '00007f', # dark blue
+		wxSTC_PL_IDENTIFIER    => '0000ff', # bright blue
+		wxSTC_PL_SCALAR        => '7f007f', # purple
+		wxSTC_PL_ARRAY         => '4080ff', # light blue
+		wxSTC_PL_HASH          => '0080ff',
+		wxSTC_PL_SYMBOLTABLE   => '000000',
+		# missing SCE_PL_VARIABLE_INDEXER (16)  
+		wxSTC_PL_REGEX         => 'ff007f', # red
+		wxSTC_PL_REGSUBST      => '7f7f00', # light olive
+		# wxSTC_PL_LONGQUOTE (19)
+		# wxSTC_PL_BACKTICKS (20)
+		# wxSTC_PL_DATASECTION (21)
+		# wxSTC_PL_HERE_DELIM (22)
+		wxSTC_PL_HERE_Q        => '7f007f',
+		# wxSTC_PL_HERE_QQ (24)
+		# wxSTC_PL_HERE_QX (25)
+		wxSTC_PL_STRING_Q      => '7f007f',
+		wxSTC_PL_STRING_QQ     => 'ff7f00', # orange
+		# wxSTC_PL_STRING_QX  (28)
+		wxSTC_PL_STRING_QR     => 'ff007f', # red
+		wxSTC_PL_STRING_QW     => '7f007f',
 
-	$self->StyleSetForeground( wxSTC_PL_DEFAULT,      Wx::Colour->new(0x00, 0x00, 0x7f));
-	$self->StyleSetForeground( wxSTC_PL_ERROR,        Wx::Colour->new(0xff, 0x00, 0x00));
-	$self->StyleSetForeground( wxSTC_PL_COMMENTLINE,  Wx::Colour->new(0x00, 0x7f, 0x00)); # line green
-	$self->StyleSetForeground( wxSTC_PL_POD,          Wx::Colour->new(0x7f, 0x7f, 0x7f));
-	$self->StyleSetForeground( wxSTC_PL_NUMBER,       Wx::Colour->new(0x00, 0x7f, 0x7f));
-	$self->StyleSetForeground( wxSTC_PL_WORD,         Wx::Colour->new(0x00, 0x00, 0x7f));
-	$self->StyleSetForeground( wxSTC_PL_STRING,       Wx::Colour->new(0xff, 0x7f, 0x00)); # orange
-	$self->StyleSetForeground( wxSTC_PL_CHARACTER,    Wx::Colour->new(0x7f, 0x00, 0x7f));
-	$self->StyleSetForeground( wxSTC_PL_PUNCTUATION,  Wx::Colour->new(0x00, 0x00, 0x00));
-	$self->StyleSetForeground( wxSTC_PL_PREPROCESSOR, Wx::Colour->new(0x7f, 0x7f, 0x7f));
-	$self->StyleSetForeground( wxSTC_PL_OPERATOR,     Wx::Colour->new(0x00, 0x00, 0x7f)); # dark blue
-	$self->StyleSetForeground( wxSTC_PL_IDENTIFIER,   Wx::Colour->new(0x00, 0x00, 0xff)); # bright blue
-	$self->StyleSetForeground( wxSTC_PL_SCALAR,       Wx::Colour->new(0x7f, 0x00, 0x7f)); # purple
-	$self->StyleSetForeground( wxSTC_PL_ARRAY,        Wx::Colour->new(0x40, 0x80, 0xff)); # light blue
-	$self->StyleSetForeground( wxSTC_PL_HASH,         Wx::Colour->new(0x00, 0x80, 0xff));
-	# wxSTC_PL_SYMBOLTABLE (15)
-	# missing SCE_PL_VARIABLE_INDEXER (16)  
-	$self->StyleSetForeground( wxSTC_PL_REGEX,        Wx::Colour->new(0xff, 0x00, 0x7f)); # red
-	$self->StyleSetForeground( wxSTC_PL_REGSUBST,     Wx::Colour->new(0x7f, 0x7f, 0x00)); # light olive
-	# wxSTC_PL_LONGQUOTE (19)
-	# wxSTC_PL_BACKTICKS (20)
-	# wxSTC_PL_DATASECTION (21)
-	# wxSTC_PL_HERE_DELIM (22)
-	$self->StyleSetForeground( wxSTC_PL_HERE_Q,       Wx::Colour->new(0x7f, 0x00, 0x7f));
-	# wxSTC_PL_HERE_QQ (24)
-	# wxSTC_PL_HERE_QX (25)
-	$self->StyleSetForeground( wxSTC_PL_STRING_Q,     Wx::Colour->new(0x7f, 0x00, 0x7f));
-	$self->StyleSetForeground( wxSTC_PL_STRING_QQ,    Wx::Colour->new(0xff, 0x7f, 0x00)); # orange
-	# wxSTC_PL_STRING_QX  (28)
-	$self->StyleSetForeground( wxSTC_PL_STRING_QR,    Wx::Colour->new(0xff, 0x00, 0x7f)); # red
-	$self->StyleSetForeground( wxSTC_PL_STRING_QW,    Wx::Colour->new(0x7f, 0x00, 0x7f));
+		# missing:
+		#define SCE_PL_POD_VERB 31
+		#define SCE_PL_SUB_PROTOTYPE 40
+		#define SCE_PL_FORMAT_IDENT 41
+		#define SCE_PL_FORMAT 42
+	);
+	Wx->import(keys %colors);
 
-	# missing:
-	#define SCE_PL_POD_VERB 31
-	#define SCE_PL_SUB_PROTOTYPE 40
-	#define SCE_PL_FORMAT_IDENT 41
-	#define SCE_PL_FORMAT 42
+	foreach my $k (keys %colors) {
+		my @c = map {hex($_)} $colors{$k} =~ /(..)(..)(..)/;
+		$self->StyleSetForeground( $k->(), Wx::Colour->new(@c));
+	}
 
 	# Set a style 12 bold
 	$self->StyleSetBold(12,  1);
