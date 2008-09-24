40c5e1b720f47b04934bb98f8e5dc4e09d87b292
Gabor Szabo <szabgab@gmail.com>
2008-09-24 20:12:21 +0000

    separate out the unused code for right-click handling
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@384 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Wx/MainWindow.pm
+++ lib/Padre/Wx/MainWindow.pm
@@ -338,42 +338,6 @@ sub on_autocompletition {
 	return;
 }
 
-sub on_right_click {
-	my ($self, $event) = @_;
-#print "right\n";
-	my @options = qw(abc def);
-	my $HEIGHT = 30;
-	my $dialog = Wx::Dialog->new( $self, -1, "", [-1, -1], [100, 50 + $HEIGHT * $#options], wxBORDER_SIMPLE);
-	#$dialog->;
-	foreach my $i (0..@options-1) {
-		EVT_BUTTON( $dialog, Wx::Button->new( $dialog, -1, $options[$i], [10, 10+$HEIGHT*$i] ), sub {on_right(@_, $i)} );
-	}
-	my $ret = $dialog->Show;
-#print "ret\n";
-	#my $pop = Padre::Wx::Popup->new($self); #, wxSIMPLE_BORDER);
-	#$pop->Move($event->GetPosition());
-	#$pop->SetSize(300, 200);
-	#$pop->Popup;
-
-#Hide
-#Destroy
-
-	#my $choices = [ 'This', 'is one of my',  'really', 'wonderful', 'examples', ];
-	#my $combo = Wx::BitmapComboBox->new($self,-1,"This",[2,2],[10,10],$choices );
-
-	return;
-}
-
-sub on_right {
-	my ($self, $event, $val) = @_;
-#print "$self $event $val\n";
-#print ">", $event->GetClientObject, "<\n";
-	$self->Hide;
-	$self->Destroy;
-
-	return;
-}
-
 sub on_exit {
 	$_[0]->Close;
 	return;
---
+++ lib/Padre/Wx/RightClick.pm
@@ -0,0 +1,51 @@
+package Padre::Wx::RightClick;
+
+use 5.008;
+use strict;
+use warnings;
+
+# Find and Replace widget of Padre
+
+use Wx             qw(:everything);
+use Wx::Event      qw(:everything);
+
+our $VERSION = '0.10';
+
+sub on_right_click {
+	my ($self, $event) = @_;
+#print "right\n";
+	my @options = qw(abc def);
+	my $HEIGHT = 30;
+	my $dialog = Wx::Dialog->new( $self, -1, "", [-1, -1], [100, 50 + $HEIGHT * $#options], wxBORDER_SIMPLE);
+	#$dialog->;
+	foreach my $i (0..@options-1) {
+		EVT_BUTTON( $dialog, Wx::Button->new( $dialog, -1, $options[$i], [10, 10+$HEIGHT*$i] ), sub {on_right(@_, $i)} );
+	}
+	my $ret = $dialog->Show;
+#print "ret\n";
+	#my $pop = Padre::Wx::Popup->new($self); #, wxSIMPLE_BORDER);
+	#$pop->Move($event->GetPosition());
+	#$pop->SetSize(300, 200);
+	#$pop->Popup;
+
+#Hide
+#Destroy
+
+	#my $choices = [ 'This', 'is one of my',  'really', 'wonderful', 'examples', ];
+	#my $combo = Wx::BitmapComboBox->new($self,-1,"This",[2,2],[10,10],$choices );
+
+	return;
+}
+
+sub on_right {
+	my ($self, $event, $val) = @_;
+#print "$self $event $val\n";
+#print ">", $event->GetClientObject, "<\n";
+	$self->Hide;
+	$self->Destroy;
+
+	return;
+}
+
+
+1;
