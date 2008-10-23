dfd2bd00948f772a90a1f3fb5e76cc14a86c349c
bricas <bricas@svngit.padre.perlide.org>
2008-10-23 14:56:21 +0000

    move perltidy plugin to its own tree
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/plugins/perltidy@435 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ lib/Padre/Plugin/PerlTidy.pm
@@ -0,0 +1,75 @@
+package Padre::Plugin::PerlTidy;
+use strict;
+use warnings;
+use Perl::Tidy;
+
+use Wx qw(:everything);
+use Wx::Event qw(:everything);
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Padre::Plugin::PerlTidy - Format perl files using Perl::Tidy
+
+=head1 SYNOPIS
+
+This is a simple plugin to run Perl::Tidy on your source code.
+
+Currently there are no customisable options (since the Padre plugin system
+doesn't support that yet) - however Perl::Tidy will use your normal .perltidyrc 
+file if it exists (see Perl::Tidy documentation).
+
+You'll get interesting results if you run this plugin on non-perl files..
+
+=cut
+
+my @menu = ( [ "Perl Tidy", \&on_run ], );
+
+sub menu {
+    my ($self) = @_;
+    return @menu;
+}
+
+sub on_run {
+    my ( $self, $event ) = @_;
+
+    my $src = $self->get_page_text;
+
+    do {
+
+        # Undefine @ARGV so that Perl::Tidy doesn't try to use it
+        local @ARGV;
+
+        # Doooit
+        my $output;
+        eval { perltidy( source => \$src, destination => \$output ); };
+        if ($@) {
+            my $error_string = $@;
+            Wx::MessageBox( $error_string, "PerlTidy Error", wxOK | wxCENTRE, $self );
+        }
+        else {
+            $self->set_page_text($output);
+        }
+    };
+    return;
+}
+
+=head1 COPYRIGHT
+
+(c) 2008 Patrick Donelan http://www.patspam.com
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl 5 itself.
+
+=head1 WARRANTY
+
+There is no warranty whatsoever.
+If you lose data or your hair because of this program,
+that's your problem.
+
+=cut
+
+1;
