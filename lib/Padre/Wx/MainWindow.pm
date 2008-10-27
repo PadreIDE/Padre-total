efe51e0eedfb1c0919b8a5d1a9e14533766c0232
Gabor Szabo <szabgab@gmail.com>
2008-10-27 19:04:58 +0000

    rename variable in post_init method
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@538 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Wx/MainWindow.pm
+++ lib/Padre/Wx/MainWindow.pm
@@ -217,17 +217,20 @@ sub new {
 }
 
 sub post_init { 
-	$_[0]->on_toggle_status_bar; 
-	$_[0]->refresh_all;
+	my ($self) = @_;
+	$self->on_toggle_status_bar;
+	$self->refresh_all;
 
-	my $output = $_[0]->{menu}->{view_output}->IsChecked;
+	my $output = $self->{menu}->{view_output}->IsChecked;
 	# First we show the output window and then hide it if necessary
 	# in order to avoide some weird visual artifacts (empty square at
 	# top left part of the whole application)
 	# TODO maybe some users want to make sure the output window is always
 	# off at startup.
-	$_[0]->show_output(1);
-	$_[0]->show_output($output) if not $output;
+	$self->show_output(1);
+	$self->show_output($output) if not $output;
+
+	return;
 }
 
 
