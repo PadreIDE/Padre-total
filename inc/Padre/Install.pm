8d3d184276f50dd9c7c7b68244cdbb45f8af2cbc
Gabor Szabo <szabgab@gmail.com>
2008-10-31 10:27:21 +0000

    add license/copyright to all the .pm files
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@604 18eddd70-d622-4293-bd05-fe65668e8bde

--- inc/Padre/Install.pm
+++ inc/Padre/Install.pm
@@ -110,3 +110,8 @@ sub modules {
 
 
 1;
+
+# Copyright 2008 Gabor Szabo.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
