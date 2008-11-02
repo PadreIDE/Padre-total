034aeea0ec4f2abc2daf8fe1bd0112b19a7cb874
Gabor Szabo <szabgab@gmail.com>
2008-11-02 09:08:43 +0000

    build_layout now receives named parameters
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@650 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Wx/Dialog.pm
+++ lib/Padre/Wx/Dialog.pm
@@ -26,19 +26,20 @@ sub new {
 	my ($class, %args) = @_;
 
 	my %default = (
-		parent   => undef,
-		id       => -1,
-		style    => Wx::wxDEFAULT_FRAME_STYLE,
-		title    => '',
-		pos      => [-1, -1],
-		size     => [-1, -1],
-		top_left => [0, 0],
+		parent          => undef,
+		id              => -1,
+		style           => Wx::wxDEFAULT_FRAME_STYLE,
+		title           => '',
+		pos             => [-1, -1],
+		size            => [-1, -1],
+		
+		top_left        => [0, 0],
 		element_spacing => [0, 5],
 	);
 	%args = (%default, %args);
 
 	my $self = $class->SUPER::new( @args{qw(parent id title pos size style)});
-	$self->build_layout($args{layout}, $args{width}, $args{top_left}, $args{element_spacing});
+	$self->build_layout( map {$_ => $args{$_} } qw(layout width top_left element_spacing) );
 	$self->{_layout_} = $args{layout};
 
 	return $self;
@@ -46,7 +47,12 @@ sub new {
 
 =head2 build_layout
 
- build_layout($dialog, $layout, $width, $top_left_offset, $element_spacing);
+ $dialog->build_layout(
+	layout          => $layout,
+	width           => $width,
+	top_left        => $top_left, 
+	element_spacing => $element_spacing,
+	);
  
 The layout is reference to a two dimensional array.
 Every element (an array) represents one line in the dialog.
@@ -92,33 +98,33 @@ Supported widgets and their parameters:
 =cut
 
 sub build_layout {
-	my ($dialog, $layout, $width, $top_left_offset, $element_spacing) = @_;
-	$top_left_offset = [0, 0] if not ref($top_left_offset);
-	$element_spacing = [0, 0] if not ref($element_spacing);
+	my ($dialog, %args) = @_;
 
 	# TODO make sure width has enough elements to the widest row
 	# or maybe we should also check that all the rows has the same number of elements
 	my $box  = Wx::BoxSizer->new( Wx::wxVERTICAL );
 	# Add Y-offset
-	$box->Add(0, $top_left_offset->[1], 0) if $top_left_offset->[1];
+	$box->Add(0, $args{top_left}[1], 0) if $args{top_left}[1];
 
-	foreach my $i (0..@$layout-1) {
+	foreach my $i (0..@{$args{layout}}-1) {
 		my $row = Wx::BoxSizer->new( Wx::wxHORIZONTAL );
 		# Add X-offset
-		$row->Add($top_left_offset->[0], 0, 0) if $top_left_offset->[0];
-		$box->Add(0, $element_spacing->[1], 0) if $element_spacing->[1];
+		$row->Add($args{top_left}[0], 0, 0) if $args{top_left}[0];
+		$box->Add(0, $args{element_spacing}[1], 0) if $args{element_spacing}[1];
 		$box->Add($row);
-		foreach my $j (0..@{$layout->[$i]}-1) {
-			if (not @{ $layout->[$i][$j] } ) {  # [] means Expand
-				$row->Add($width->[$j], 0, 0, Wx::wxEXPAND, 0);
+		foreach my $j (0..@{$args{layout}[$i]}-1) {
+			my $width = [$args{width}[$j], -1];
+
+			if (not @{ $args{layout}[$i][$j] } ) {  # [] means Expand
+				$row->Add($args{width}[$j], 0, 0, Wx::wxEXPAND, 0);
 				next;
 			}
-		        $row->Add($element_spacing->[0], 0, 0) if $element_spacing->[0];
-			my ($class, $name, $arg, @params) = @{ $layout->[$i][$j] };
+		        $row->Add($args{element_spacing}[0], 0, 0) if $args{element_spacing}[0];
+			my ($class, $name, $arg, @params) = @{ $args{layout}[$i][$j] };
 
 			my $widget;
 			if ($class eq 'Wx::StaticText') {
-				$widget = $class->new( $dialog, -1, $arg, Wx::wxDefaultPosition, [$width->[$j], -1] );
+				$widget = $class->new( $dialog, -1, $arg, Wx::wxDefaultPosition, $width );
 			} elsif ($class eq 'Wx::Button') {
 				my $s = Wx::Button::GetDefaultSize;
 				#print $s->GetWidth, " ", $s->GetHeight, "\n";
@@ -126,24 +132,24 @@ sub build_layout {
 				$widget = $class->new( $dialog, @args );
 			} elsif ($class eq 'Wx::DirPickerCtrl') {
 				my $title = shift(@params) || '';
-				$widget = $class->new( $dialog, -1, $arg, $title, Wx::wxDefaultPosition, [$width->[$j], -1] );
+				$widget = $class->new( $dialog, -1, $arg, $title, Wx::wxDefaultPosition, $width );
 				# it seems we cannot set the default directory and 
 				# we still have to set this directory in order to get anything back in
 				# GetPath
 				$widget->SetPath(Cwd::cwd());
 			} elsif ($class eq 'Wx::TextCtrl') {
-				$widget = $class->new( $dialog, -1, $arg, Wx::wxDefaultPosition, [$width->[$j], -1]);
+				$widget = $class->new( $dialog, -1, $arg, Wx::wxDefaultPosition, $width );
 			} elsif ($class eq 'Wx::CheckBox') {
 				my $default = shift @params;
-				$widget = $class->new( $dialog, -1, $arg, Wx::wxDefaultPosition, [$width->[$j], -1], @params );
+				$widget = $class->new( $dialog, -1, $arg, Wx::wxDefaultPosition, $width, @params );
 				$widget->SetValue($default);
 			} elsif ($class eq 'Wx::ComboBox') {
-				$widget = $class->new( $dialog, -1, $arg, Wx::wxDefaultPosition, [$width->[$j], -1], @params );
+				$widget = $class->new( $dialog, -1, $arg, Wx::wxDefaultPosition, $width, @params );
 			} elsif ($class eq 'Wx::Choice') {
-				$widget = $class->new( $dialog, -1, Wx::wxDefaultPosition, [$width->[$j], -1], $arg, @params );
+				$widget = $class->new( $dialog, -1, Wx::wxDefaultPosition, $width, $arg, @params );
 			} elsif ($class eq 'Wx::Treebook') {
 				my $height = @$arg * 27; # should be height of font
-				$widget = $class->new( $dialog, -1, Wx::wxDefaultPosition, [$width->[$j], $height] );
+				$widget = $class->new( $dialog, -1, Wx::wxDefaultPosition, [$args{width}[$j], $height] );
 				foreach my $name ( @$arg ) {
 					my $count = $widget->GetPageCount;
 					my $page  = Wx::Panel->new( $widget );
