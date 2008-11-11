8274923c5b149476e68d131236ea21457a183a66
Adam Kennedy <adamk@cpan.org>
2008-11-11 04:14:52 +0000

    Re-ordering toolbar, make more readable/maintainable layout, and removed extra seperator
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@875 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Wx/ToolBar.pm
+++ lib/Padre/Wx/ToolBar.pm
@@ -19,51 +19,113 @@ sub new {
 								   Wx::wxNO_BORDER | Wx::wxTB_HORIZONTAL | Wx::wxTB_FLAT | Wx::wxTB_DOCKABLE, 5050, );
 
 	# Automatically populate
-	$self->AddTool( Wx::wxID_NEW, '', Padre::Wx::tango( catfile( 'actions', 'document-new.png' ) ),
-					gettext('New File') );
-	$self->AddTool( Wx::wxID_OPEN, '', Padre::Wx::tango( catfile( 'actions', 'document-open.png' ) ),
-					gettext('Open File') );
-	$self->AddTool( Wx::wxID_SAVE, '', Padre::Wx::tango( catfile( 'actions', 'document-save.png' ) ),
-					gettext('Save File') );
-	$self->AddTool( Wx::wxID_CLOSE, '', Padre::Wx::tango( catfile( 'emblems', 'emblem-unreadable.png' ) ),
-					gettext('Close File') );
+	$self->AddTool(
+		Wx::wxID_NEW, '',
+		Padre::Wx::tango( catfile( 'actions', 'document-new.png' ) ),
+		gettext('New File'),
+	);
+	Wx::Event::EVT_TOOL(
+		$parent,
+		Wx::wxID_NEW,
+		sub { $_[0]->setup_editor; return; },
+	);
+	$self->AddTool(
+		Wx::wxID_OPEN, '',
+		Padre::Wx::tango( catfile( 'actions', 'document-open.png' ) ),
+		gettext('Open File'),
+	);
+	$self->AddTool(
+		Wx::wxID_SAVE, '',
+		Padre::Wx::tango( catfile( 'actions', 'document-save.png' ) ),
+		gettext('Save File'),
+	);
+	$self->AddTool(
+		Wx::wxID_CLOSE, '',
+		Padre::Wx::tango( catfile( 'emblems', 'emblem-unreadable.png' ) ),
+		gettext('Close File'),
+	);
+	Wx::Event::EVT_TOOL(
+		$parent,
+		Wx::wxID_CLOSE,
+		\&Padre::Wx::MainWindow::on_close,
+	);
 	$self->AddSeparator;
 
 	# TODO, how can we make sure these numbers are unique?
 	#$self->AddTool( 1000, '', Padre::Wx::tango(catfile('actions', 'bookmark-new.png')), 'Bookmark' );
 	#Wx::Event::EVT_TOOL($parent, 1000, sub { Padre::Wx::Dialog::Bookmarks->set_bookmark($_[0]) } );
 
-	Wx::Event::EVT_TOOL( $parent, Wx::wxID_CLOSE, \&Padre::Wx::MainWindow::on_close );
-	Wx::Event::EVT_TOOL( $parent, Wx::wxID_NEW, sub { $_[0]->setup_editor; return; } );
 
+
+
+
+	# Undo/Redo Support
+	$self->AddTool(
+		Wx::wxID_UNDO, '',
+		Padre::Wx::tango( catfile( 'actions', 'edit-undo.png' ) ),
+		gettext('Undo'),
+	);
+	$self->AddTool(
+		Wx::wxID_REDO, '',
+		Padre::Wx::tango( catfile( 'actions', 'edit-redo.png' ) ),
+		gettext('Redo'),
+	);
 	$self->AddSeparator;
 
-	$self->AddTool( Wx::wxID_UNDO, '', Padre::Wx::tango( catfile( 'actions', 'edit-undo.png' ) ), gettext('Undo') );
-	$self->AddTool( Wx::wxID_REDO, '', Padre::Wx::tango( catfile( 'actions', 'edit-redo.png' ) ), gettext('Redo') );
 
-	$self->AddSeparator;
 
-	$self->AddTool( Wx::wxID_SELECTALL, '', Padre::Wx::tango( catfile( 'actions', 'edit-select-all.png' ) ),
-					gettext('Select all') );
-	$self->AddTool( Wx::wxID_COPY,  '', Padre::Wx::tango( catfile( 'actions', 'edit-copy.png' ) ),  gettext('Copy') );
-	$self->AddTool( Wx::wxID_CUT,   '', Padre::Wx::tango( catfile( 'actions', 'edit-cut.png' ) ),   gettext('Cut') );
-	$self->AddTool( Wx::wxID_PASTE, '', Padre::Wx::tango( catfile( 'actions', 'edit-paste.png' ) ), gettext('Paste') );
 
-	Wx::Event::EVT_TOOL( $parent, Wx::wxID_SELECTALL, sub { \&Padre::Wx::Editor::text_select_all(@_) } );
-	Wx::Event::EVT_TOOL( $parent, Wx::wxID_COPY, sub { \&Padre::Wx::Editor::text_copy_to_clipboard(@_) } );
-	Wx::Event::EVT_TOOL( $parent, Wx::wxID_CUT, sub { \&Padre::Wx::Editor::text_cut_to_clipboard(@_) } );
-	Wx::Event::EVT_TOOL( $parent, Wx::wxID_PASTE, sub { \&Padre::Wx::Editor::text_paste_from_clipboard(@_) } );
+
+	# Cut/Copy/Paste
+	$self->AddTool(
+		Wx::wxID_CUT, '',
+		Padre::Wx::tango( catfile( 'actions', 'edit-cut.png' ) ),
+		gettext('Cut'),
+	);
+	Wx::Event::EVT_TOOL(
+		$parent,
+		Wx::wxID_CUT,
+		sub { \&Padre::Wx::Editor::text_cut_to_clipboard(@_) },
+	);
+	$self->AddTool(
+		Wx::wxID_COPY,  '',
+		Padre::Wx::tango( catfile( 'actions', 'edit-copy.png' ) ),
+		gettext('Copy'),
+	);
+	Wx::Event::EVT_TOOL(
+		$parent,
+		Wx::wxID_COPY,
+		sub { \&Padre::Wx::Editor::text_copy_to_clipboard(@_) },
+	);
+	$self->AddTool(
+		Wx::wxID_PASTE, '',
+		Padre::Wx::tango( catfile( 'actions', 'edit-paste.png' ) ),
+		gettext('Paste'),
+	);
+	Wx::Event::EVT_TOOL(
+		$parent,
+		Wx::wxID_PASTE,
+		sub { \&Padre::Wx::Editor::text_paste_from_clipboard(@_) },
+	);
+	$self->AddTool(
+		Wx::wxID_SELECTALL, '',
+		Padre::Wx::tango( catfile( 'actions', 'edit-select-all.png' ) ),
+		gettext('Select all'),
+	);
+	Wx::Event::EVT_TOOL(
+		$parent,
+		Wx::wxID_SELECTALL,
+		sub { \&Padre::Wx::Editor::text_select_all(@_) },
+	);
 
 	return $self;
-} ## end sub new
+}
 
 sub refresh {
-	my $self = shift;
-	my $doc  = shift;
-
+	my $self    = shift;
+	my $doc     = shift;
 	my $enabled = !!( $doc and $doc->is_modified );
 	$self->EnableTool( Wx::wxID_SAVE, $enabled );
-
 	$self->EnableTool( Wx::wxID_CLOSE, ( defined Padre::Documents->current ? 1 : 0 ) );
 	return 1;
 }
