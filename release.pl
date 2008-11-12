5599ca8118b68ac064615ef6a05ca81e34c6dff5
Gabor Szabo <szabgab@gmail.com>
2008-11-12 19:59:21 +0000

    further automate the release script of Padre using svn export
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@930 18eddd70-d622-4293-bd05-fe65668e8bde

--- release.pl
+++ release.pl
@@ -2,21 +2,54 @@
 use strict;
 use warnings;
 
-
 # check if there are versions in every module and if they are in the same
 # allow updating version numbers to one specific version.
 
-use File::Find qw(find);
+use autodie qw(:default system);
+# needs IPC::System::Simple
+
+use File::Temp ();
+use File::Find  qw(find);
 use File::Slurp qw(read_file write_file);
+use File::Copy  qw(copy);
 
-my $version = shift;
-die "Usage: $0 VERSION\n" if not $version or $version !~ /^\d\.\d\d$/;
-print "Setting VERSION $version\n";
+my $TRUNK = "http://svn.perlide.org/padre/trunk";
+my $TAGS  = "http://svn.perlide.org/padre/tags";
+my $error = 0;
 
-find(\&xversion, 'lib');
+my ($rev, $version, $tag) = @ARGV;
+die "Usage: $0 REV VERSION [--tag]\n"
+	if not $version or $version !~ /^\d\.\d\d$/ or $rev !~ /^\d+$/;
+
+my $dir = File::Temp::tempdir( CLEANUP => 1 );
+chdir $dir;
+print "DIR $dir\n";
+
+_system("svn export --quiet -r$rev $TRUNK padre");
+chdir 'padre';
+_system("msgfmt -o share/locale/de.mo share/locale/de.po");
+# TODO add de.po to MANIFEST
+if (open my $fh, '>>', 'MANIFEST') {
+	print {$fh} "\nshare/locale/de.mo\n";
+	close $fh;
+}
+
+#print "Setting VERSION $version\n";
+find(\&set_version, 'lib');
+die if $error;
+
+_system("$^X Build.PL");
+_system("$^X Build");
+_system("$^X Build test");
+_system("$^X Build dist");
+copy("Padre-$version.tar.gz", "/home/gabor/tmp") or die $!;
+if ($tag) {
+	_system("svn cp -r$rev $TRUNK $TAGS/Padre-$version -m'tag Padre-$version'");
+}
+chdir "/home/gabor/tmp";
 
 
-sub xversion {
+sub set_version {
     return if $File::Find::name =~ /\.svn/;
     return if $_ !~ /\.pm/;
     my @data = read_file($_);
@@ -25,6 +58,13 @@ sub xversion {
        write_file($_, @new);
     } else {
        warn "No VERSION in $File::Find::name\n";
+       $error++;
     }
+    return;
 }
 
+sub _system {
+	my $cmd = shift;
+	print "$cmd\n";
+	system $cmd;
+}
