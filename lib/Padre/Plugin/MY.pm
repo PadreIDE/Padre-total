38fb0baaf539fd11fc21136d96e76546f07b6c54
Adam Kennedy <adamk@cpan.org>
2008-11-14 05:00:15 +0000

    Some changes to make the default plugin more approachable
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@975 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Plugin/MY.pm
+++ lib/Padre/Plugin/MY.pm
@@ -1,23 +1,26 @@
 package Padre::Plugin::MY;
+
 use strict;
 use warnings;
 
 our $VERSION = '0.01';
 
+sub menu_name { 'My Plugin' }
+
 sub menu {
 	return ( 
-		['About'          => \&about],
+		['About' => \&about],
 
-# to get another menu item comment out the following line
-# and implement appropriate function		
-#		 ['Do something quick'   => \&do_something_quick],
+		# To get another menu item comment out the following line
+		# and implement appropriate function		
+		# ['Do Something Quick'  => \&do_something_quick],
 		
-# to have deeper levels in the menu comment out the following
-# 4 lines and implement the appropriate function
-#		[ 'Deep' => [
-#				['Do something substantial'   => \&do_something_substantial],
-#			],
-#		],
+		# To have deeper levels in the menu comment out the following
+		# 4 lines and implement the appropriate function
+		#     [ 'Deep' => [
+		#         [ 'Do something substantial' => \&do_something_substantial ],
+		#     ],
+		# ],
 	);
 }
 
@@ -29,25 +32,30 @@ sub menu {
 #	my ($main) = @_;
 #}
 
-
 sub about {
 	my ($main) = @_;
 
-	my $path = File::Spec->catfile(Padre->ide->config_dir, 'plugins', 'Padre', 'Plugin', 'MY.pm');
-	my $about = Wx::AboutDialogInfo->new;
-	$about->SetName("MY Plugin");
-	$about->SetDescription(
-		"In order to make it easier to add small snippets of code\n" .
-		"to be executed in Padre, to write your own plugin\n" .
-		"We provide this special plugin called MY\n" .
-		"It is located in your configuration directory at\n" .
-		"$path\n" .
-		"\n" .
-		"Open it with any Padre and you'll see an explanation on how to add items"
+	my $path = File::Spec->catfile(
+		Padre->ide->config_dir,
+		qw{ plugins Padre Plugin MY.pm }
 	);
+	my $about = Wx::AboutDialogInfo->new;
+	$about->SetName(
+		Padre::Plugin::MY->menu_name;
+	);
+	$about->SetDescription( <<"END_MESSAGE" );
+The philosophy behind Padre is that every Perl programmer
+should be able to easily modify and improve their own editor.
+
+To help you get started, we've provided you with your own plugin.
+
+It is located in your configuration directory at:
+$path
+Open it with with Padre and you'll see an explanation on how to add items.
+END_MESSAGE
+
 	Wx::AboutBox( $about );
 	return;
 }
-
 
 1;
