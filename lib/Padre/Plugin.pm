923b43a0110f0e9bfb4f373bd2278578c3b537d1
Adam Kennedy <adamk@cpan.org>
2008-11-17 02:11:06 +0000

    Adding the first pass at a second-generation plugin API
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@1021 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ lib/Padre/Plugin.pm
@@ -0,0 +1,99 @@
+package Padre::Plugin;
+
+=pod
+
+=head1 NAME
+
+Padre::Plugin - Padre Plugin API 
+
+=head SYNOPSIS
+
+  package Padre::Plugin::Foo;
+  
+  use strict;
+  use base 'Padre::Plugin';
+  
+  # Declare the Padre classes we use and Padre version the code was written to
+  sub padre_interfaces {
+      'Padre::Document::Perl' => 0.16,
+      'Padre::Wx::MainWindow' => 0.16,
+      'Padre::DB'             => 0.16,
+  }
+  
+  # The plugin name to show in the Plugins menu
+  sub plugins_menu_label {
+  	  'Sample Plugin'
+  }
+  
+  # The command structure to show in the Plugins menu
+  sub plugins_menu_data {
+  	  ...
+  }
+  
+  1;
+
+=cut
+
+use 5.008;
+use strict;
+use warnings;
+use Scalar::Util ();
+use Padre::Wx    ();
+
+our $VERSION = '0.16';
+
+
+
+
+
+######################################################################
+# Default Constructor
+
+sub new {
+	my $class = shift;
+	my $self  = bless { @_ }, $class;
+
+	return $self;
+}
+
+# Default plugins menu label
+sub plugins_menu_label {
+	my $self  = shift;
+	my $label = Scalar::Util::blessed($self);
+	$label =~ s/^Padre::Plugin:://;
+	return $label;
+}
+
+# Default plugins menu data
+sub plugins_menu_data {
+	# Plugins returning no data will not
+	# be visible in the plugin menu.
+	return ();
+}
+
+# Generates plugin menu
+sub plugins_menu {
+	my $self   = shift;
+	my $parent = shift;
+	
+}
+
+sub plugins_menu_items {
+	my $self  = shift;
+	my $items = shift;
+	my ($self, $items) = @_;
+
+	my $menu = Wx::Menu->new;
+	foreach my $item ( @items ) {
+		if (ref $m->[1] eq 'ARRAY') {
+			my $submenu = $self->add_plugin_menu_items($m->[1]);
+			$menu->Append(-1, $m->[0], $submenu);
+		} else {
+			Wx::Event::EVT_MENU( $self->win, $menu->Append(-1, $m->[0]), $m->[1] );
+		}
+	}
+
+	return $menu;
+}
+
+1;
