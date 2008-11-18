55a225b5c017ed176c813c3d2600a24656132999
kaare <kaare@svngit.padre.perlide.org>
2008-11-18 20:24:00 +0000

    Perltidy and other nicifying the code.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@1075 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Wx/Dialog/Snippets.pm
+++ lib/Padre/Wx/Dialog/Snippets.pm
@@ -13,116 +13,102 @@ use Wx::Locale qw(:default);
 our $VERSION = '0.16';
 
 sub get_layout {
-	my ($config) = @_;
+    my ($config) = @_;
 
-    my $cats =  Padre::DB->find_snipclasses;
+    my $cats = Padre::DB->find_snipclasses;
     unshift @$cats, gettext('All');
     my $snippets = Padre::DB->find_snipnames;
 
-	my @layout = (
-		[
-			[ 'Wx::StaticText', undef,            gettext('Class:')],
-			[ 'Wx::Choice',     '_find_cat_',     $cats],
-		],
-		[
-			[ 'Wx::StaticText', undef,              gettext('Snippet:')],
-			[ 'Wx::Choice',   '_find_snippet_',     $snippets],
-		],
-		[
-			[],
-			[ 'Wx::Button',     '_insert_',    gettext('Insert')],
-			[ 'Wx::Button',     '_cancel_',    Wx::wxID_CANCEL],
-		],
-	);
-	return \@layout;
+    my @layout = (
+        [ [ 'Wx::StaticText', undef, gettext('Class:') ],   [ 'Wx::Choice', '_find_cat_',     $cats ], ],
+        [ [ 'Wx::StaticText', undef, gettext('Snippet:') ], [ 'Wx::Choice', '_find_snippet_', $snippets ], ],
+        [ [], [ 'Wx::Button', '_insert_', gettext('Insert') ], [ 'Wx::Button', '_cancel_', Wx::wxID_CANCEL ], ],
+    );
+    return \@layout;
 }
 
 sub dialog {
-	my ($class, $win, $args) = @_;
+    my ( $class, $win, $args ) = @_;
 
-	my $config = Padre->ide->config;
+    my $config = Padre->ide->config;
 
-	my $layout = get_layout($config);
-	my $dialog = Padre::Wx::Dialog->new(
-		parent => $win,
-		title  => gettext("Snippets"),
-		layout => $layout,
-		width  => [150, 200],
-	);
+    my $layout = get_layout($config);
+    my $dialog = Padre::Wx::Dialog->new(
+        parent => $win,
+        title  => gettext("Snippets"),
+        layout => $layout,
+        width  => [ 150, 200 ],
+    );
 
-	Wx::Event::EVT_CHOICE( $dialog, $dialog->{_widgets_}{_find_cat_}, \&find_category);
-	Wx::Event::EVT_BUTTON( $dialog, $dialog->{_widgets_}{_insert_},   \&get_snippet);
-	Wx::Event::EVT_BUTTON( $dialog, $dialog->{_widgets_}{_cancel_},   \&cancel_clicked);
+    Wx::Event::EVT_CHOICE( $dialog, $dialog->{_widgets_}{_find_cat_}, \&find_category );
+    Wx::Event::EVT_BUTTON( $dialog, $dialog->{_widgets_}{_insert_}, \&get_snippet );
+    Wx::Event::EVT_BUTTON( $dialog, $dialog->{_widgets_}{_cancel_}, \&cancel_clicked );
 
-	$dialog->{_widgets_}{_find_cat_}->SetFocus;
+    $dialog->{_widgets_}{_find_cat_}->SetFocus;
 
-	return $dialog;
+    return $dialog;
 }
 
 sub snippets {
-	my ($class, $main) = @_;
+    my ( $class, $main ) = @_;
 
-	my $dialog = $class->dialog($main, {});
-	$dialog->Show(1);
+    my $dialog = $class->dialog( $main, {} );
+    $dialog->Show(1);
 
-	return;
+    return;
+}
+
+sub _get_catno {
+    my ( $dialog ) = @_;
+
+    my $data     = $dialog->get_data or return;
+    my $catno    = $data->{_find_cat_} - 1;
+    return @{ Padre::DB->find_snipclasses }[$catno];
 }
 
 sub find_category {
-	my ($dialog, $event) = @_;
+    my ( $dialog, $event ) = @_;
 
-	my $data    = _get_data_from( $dialog ) or return;
-    my $catno   = $data->{_find_cat_} - 1;
-    my $cat     = @{ Padre::DB->find_snipclasses }[$catno];
+    my $cat = _get_catno($dialog);
     my $snippets = Padre::DB->find_snipnames($cat);
-    my $field = $dialog->{_widgets_}{_find_snippet_};
+    my $field    = $dialog->{_widgets_}{_find_snippet_};
     $field->Clear;
     $field->AppendItems($snippets);
     $field->SetSelection(0);
 
-	return;
+    return;
 }
 
 sub get_snippet_text {
-	my ($cat, $snipno) = @_;
+    my ( $cat, $snipno ) = @_;
 
     my $choices = Padre::DB->find_snippets($cat);
     return $choices->[$snipno];
 }
 
 sub get_snippet {
-	my ($dialog, $event) = @_;
+    my ( $dialog, $event ) = @_;
 
-	my $data   = _get_data_from( $dialog ) or return;
-    my $catno   = $data->{_find_cat_} - 1;
-    my $cat     = @{ Padre::DB->find_snipclasses }[$catno];
+    my $data   = $dialog->get_data or return;
+    my $cat = _get_catno($dialog);
     my $snipno = $data->{_find_snippet_};
-    my $text   = get_snippet_text($cat, $snipno);
+    my $text   = get_snippet_text( $cat, $snipno );
 
-	my $win = Padre->ide->wx->main_window;
+    my $win = Padre->ide->wx->main_window;
     my $id  = $win->{notebook}->GetSelection;
     $win->{notebook}->GetPage($id)->ReplaceSelection('');
     my $pos = $win->{notebook}->GetPage($id)->GetCurrentPos;
     $win->{notebook}->GetPage($id)->InsertText( $pos, $text );
-#    $win->{notebook}->GetPage($id)->GotoPos( $pos + $length - 1 );	return;
+
+    return;
 }
 
 sub cancel_clicked {
-	my ($dialog, $event) = @_;
+    my ( $dialog, $event ) = @_;
 
-	$dialog->Destroy;
+    $dialog->Destroy;
 
-	return;
-}
-
-sub _get_data_from {
-	my ( $dialog ) = @_;
-
-    my $data = $dialog->get_data;
-
-    #print Data::Dumper::Dumper $data;
-
-	return $data;
+    return;
 }
 
 1;
