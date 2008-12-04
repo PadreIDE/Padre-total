2ca3ecdbf8759a7b37b4f1a6de7eabfc55d237e6
hjansen <hjansen@svngit.padre.perlide.org>
2008-12-04 21:41:07 +0000

    Use AuiNotebook instead of traditional Notebook to have a more consistent layout and use less space
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@1651 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Wx/SyntaxChecker.pm
+++ lib/Padre/Wx/SyntaxChecker.pm
@@ -47,7 +47,13 @@ sub create_syntaxbar {
 	$syntaxbar->InsertColumn( 1, Wx::gettext('Type') );
 	$syntaxbar->InsertColumn( 2, Wx::gettext('Description') );
 
-	$mw->{gui}->{bottompane}->InsertPage( 1, $syntaxbar, Wx::gettext("Syntax Check"), 0 );
+	$mw->{gui}->{bottompane}->InsertPage(
+		1,
+		$syntaxbar,
+		Wx::gettext("Syntax Check"),
+		0,
+		Padre::Wx::tango( 'status', 'dialog-warning.png' )
+	);
 
 	Wx::Event::EVT_LIST_ITEM_ACTIVATED(
 		$mw,
