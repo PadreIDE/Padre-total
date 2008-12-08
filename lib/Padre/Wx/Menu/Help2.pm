5dba4ff1693e4f38213ce7e37bc2bbd06b45d282
Adam Kennedy <adamk@cpan.org>
2008-12-08 12:05:17 +0000

    Adding a second attempt at the Help menu.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@1730 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ lib/Padre/Wx/Menu/Help2.pm
@@ -0,0 +1,22 @@
+package Padre::Wx::Menu::Help2;
+
+# Second attempt to build an fully encapsulated help menu.
+# (As a test bed for doing it for the bigger ones)
+# Whoever ripped my last attempt apart and made it a trivial
+# container for functions instead of a real menu, please leave
+# this one alone for now. :(
+# Adam K
+
+use 5.008;
+use strict;
+use warnings;
+use base 'Padre::Wx::Submenu';
+
+our $VERSION = '0.20';
+
+sub new {
+	my $class = shift;
+	
+}
+
+1;
