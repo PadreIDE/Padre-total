3ce394c3b559fa6a60fab6105cada8fb6eafc632
Gabor Szabo <szabgab@gmail.com>
2008-12-16 21:34:13 +0000

    initial version of Perl6 plugin
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Padre-Plugin-Perl6@1979 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ lib/Padre/Plugin/Perl6.pm
@@ -0,0 +1,61 @@
+package Padre::Plugin::Perl6;
+use strict;
+use warnings;
+
+our $VERSION = '0.22';
+
+use Padre::Wx ();
+use base 'Padre::Plugin';
+
+=head1 NAME
+
+Padre::Plugin::Perl6 - Experimental Padre plugin for Perl6
+
+=head1 SYNOPSIS
+
+After installation when you run Padre there should be a menu option Plugins/Perl6.
+
+=head1 COPYRIGHT
+
+Copyright 2008 Gabor Szabo. L<http://www.szabgab.com/>
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl 5 itself.
+
+=cut
+
+sub padre_interfacesx {
+	'Padre::Plugin'         => 0.20,
+}
+
+
+sub menu_plugins_simple {
+	my $self = shift;
+	'Perl 6' => [
+		About => sub { $self->show_about },
+	];
+}
+
+
+sub show_about {
+	my ($main) = @_;
+
+	my $about = Wx::AboutDialogInfo->new;
+	$about->SetName("Padre::Plugin::Perl6");
+	$about->SetDescription(
+		"Experimental Perl6 syntax highlighting\n"
+	);
+	#$about->SetVersion($Padre::VERSION);
+	Wx::AboutBox( $about );
+	return;
+}
+
+
+1;
+
+# Copyright 2008 Gabor Szabo.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
