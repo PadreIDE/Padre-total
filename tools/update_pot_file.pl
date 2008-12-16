faa3e27b137a0e601835fd04623c82d975669042
Gabor Szabo <szabgab@gmail.com>
2008-12-16 18:58:44 +0000

    deal with utf8 files when updateing pot files (rindolf)
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@1968 18eddd70-d622-4293-bd05-fe65668e8bde

--- tools/update_pot_file.pl
+++ tools/update_pot_file.pl
@@ -21,7 +21,7 @@ print $fh map { "$_$/" } @pmfiles;
 close $fh;
 
 unlink $pot_file;
-system("xgettext -o $pot_file -f $pmfiles") == 0
+system("xgettext --from-code=utf-8 -o $pot_file -f $pmfiles") == 0
 	or die "xgettext exited with return code " . $? >> 8;
 
 # cleanup
