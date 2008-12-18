9632c905a686e0a89509116994bf7ba6d6cbac95
fayland <fayland@svngit.padre.perlide.org>
2008-12-18 10:35:16 +0000

    Padre::Plugin::XML
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@2029 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-XML/lib/Padre/Plugin/XML.pm
+++ Padre-Plugin-XML/lib/Padre/Plugin/XML.pm
@@ -3,96 +3,57 @@ package Padre::Plugin::XML;
 use warnings;
 use strict;
 
-=head1 NAME
-
-Padre::Plugin::XML - The great new Padre::Plugin::XML!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
 our $VERSION = '0.01';
 
+use base 'Padre::Plugin';
+use Wx ':everything';
 
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Padre::Plugin::XML;
-
-    my $foo = Padre::Plugin::XML->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
+sub padre_interfaces {
+	'Padre::Plugin' => '0.21',
 }
 
-=head2 function2
-
-=cut
-
-sub function2 {
+sub menu_plugins_simple {
+	'XML' => [
+		'Tidy XML', \&tidy_xml,
+	];
 }
+
+sub tidy_xml {
+	my ( $self ) = @_;
+	
+	my $src = $self->selected_text;
+	my $doc = $self->selected_document;
+	my $code = ( $src ) ? $src : $doc->text_get;
+	
+	return unless ( defined $code and length($code) );
+	
+	require XML::Tidy;
+	my $tidy_obj = XML::Tidy->new( xml => $code );
+	$tidy_obj->tidy();
+	
+	my $string = $tidy_obj->toString();
+	if ( $src ) {
+		my $editor = $self->selected_editor;
+	    $editor->ReplaceSelection( $string );
+	} else {
+		$doc->text_set( $string );
+	}
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Padre::Plugin::XML - L<Padre> and XML
+
+=head1 Tidy XML
+
+use L<XML::Tidy> to tidy XML
 
 =head1 AUTHOR
 
 Fayland Lam, C<< <fayland at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-padre-plugin-xml at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Padre-Plugin-XML>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Padre::Plugin::XML
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Padre-Plugin-XML>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Padre-Plugin-XML>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Padre-Plugin-XML>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Padre-Plugin-XML/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
 
 =head1 COPYRIGHT & LICENSE
 
@@ -101,7 +62,4 @@ Copyright 2008 Fayland Lam, all rights reserved.
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-
 =cut
-
-1; # End of Padre::Plugin::XML
