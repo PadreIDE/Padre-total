b649a7cdd3b0c4b084ee140d61e3432d34c33fe9
fayland <fayland@svngit.padre.perlide.org>
2008-12-18 10:25:48 +0000

    Padre::Plugin::CSS
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@2026 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-CSS/lib/Padre/Plugin/CSS.pm
+++ Padre-Plugin-CSS/lib/Padre/Plugin/CSS.pm
@@ -3,96 +3,77 @@ package Padre::Plugin::CSS;
 use warnings;
 use strict;
 
-=head1 NAME
-
-Padre::Plugin::CSS - The great new Padre::Plugin::CSS!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
 our $VERSION = '0.01';
 
+use base 'Padre::Plugin';
+use Wx ':everything';
 
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Padre::Plugin::CSS;
-
-    my $foo = Padre::Plugin::CSS->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
+sub padre_interfaces {
+	'Padre::Plugin' => '0.21',
 }
 
-=head2 function2
-
-=cut
-
-sub function2 {
+sub menu_plugins_simple {
+	'CSS' => [
+		'Validate CSS',  \&validate_css,
+	];
 }
+
+sub validate_css {
+	my ( $self ) = @_;
+	
+	my $doc  = $self->selected_document;
+	my $code = $doc->text_get;
+	
+	unless ( $code and length($code) ) {
+		Wx::MessageBox( 'No Code', 'Error', Wx::wxOK | Wx::wxCENTRE, $self );
+	}
+	
+	require WebService::Validator::CSS::W3C;
+	my $val = WebService::Validator::CSS::W3C->new();
+	my $ok  = $val->validate(string => $code);
+
+	if ($ok) {
+		if ( $val->is_valid ) {
+			_output( $self, "CSS is valid\n" );
+		} else {
+			my $error_text = "CSS is not valid\n";
+			$error_text .= "Errors:\n";
+			my @errors = $val->errors;
+			foreach my $err (@errors) {
+				my $message = $err->{message};
+				$message =~ s/(^\s+|\s+$)//isg;
+				$error_text .= " * $message ($err->{context}) at line $err->{line}\n";
+			}
+			_output( $self, $error_text );
+		}
+	} else {
+		my $error_text = sprintf("Failed to validate the code\n");
+        _output( $self, $error_text );
+	}
+}
+
+sub _output {
+	my ( $self, $text ) = @_;
+	
+	$self->show_output;
+	$self->{gui}->{output_panel}->clear;
+	$self->{gui}->{output_panel}->AppendText($text);
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Padre::Plugin::CSS - L<Padre> and CSS
+
+=head1 Validate CSS
+
+use L<WebService::Validator::CSS::W3C> to validate the CSS
 
 =head1 AUTHOR
 
 Fayland Lam, C<< <fayland at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-padre-plugin-css at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Padre-Plugin-CSS>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Padre::Plugin::CSS
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Padre-Plugin-CSS>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Padre-Plugin-CSS>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Padre-Plugin-CSS>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Padre-Plugin-CSS/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
 
 =head1 COPYRIGHT & LICENSE
 
@@ -101,7 +82,4 @@ Copyright 2008 Fayland Lam, all rights reserved.
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-
 =cut
-
-1; # End of Padre::Plugin::CSS
