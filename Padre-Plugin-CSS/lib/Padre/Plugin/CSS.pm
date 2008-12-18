ad3ed90b426b9000a03aeef77821888bb41e6083
fayland <fayland@svngit.padre.perlide.org>
2008-12-18 10:20:22 +0000

    start Padre-Plugin-CSS Padre-Plugin-HTML Padre-Plugin-XML
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@2024 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ Padre-Plugin-CSS/lib/Padre/Plugin/CSS.pm
@@ -0,0 +1,107 @@
+package Padre::Plugin::CSS;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Padre::Plugin::CSS - The great new Padre::Plugin::CSS!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Padre::Plugin::CSS;
+
+    my $foo = Padre::Plugin::CSS->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Fayland Lam, C<< <fayland at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-padre-plugin-css at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Padre-Plugin-CSS>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Padre::Plugin::CSS
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Padre-Plugin-CSS>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Padre-Plugin-CSS>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Padre-Plugin-CSS>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Padre-Plugin-CSS/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Fayland Lam, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Padre::Plugin::CSS
---
+++ Padre-Plugin-XML/lib/Padre/Plugin/XML.pm
@@ -0,0 +1,107 @@
+package Padre::Plugin::XML;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Padre::Plugin::XML - The great new Padre::Plugin::XML!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Padre::Plugin::XML;
+
+    my $foo = Padre::Plugin::XML->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Fayland Lam, C<< <fayland at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-padre-plugin-xml at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Padre-Plugin-XML>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Padre::Plugin::XML
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Padre-Plugin-XML>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Padre-Plugin-XML>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Padre-Plugin-XML>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Padre-Plugin-XML/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Fayland Lam, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Padre::Plugin::XML
