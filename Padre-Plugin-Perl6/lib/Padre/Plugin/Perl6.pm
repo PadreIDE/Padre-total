c91e077c0c4f1e37df67e9c4d45674edd38dc7c9
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2008-12-19 11:24:53 +0000

    [Padre::Plugin::Perl6] Added export full, simple and snippet HTMLs in Perl6 sub menu [Padre::Plugin::Perl6] Added dependency on CPAN Readonly 1.03
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@2100 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Perl6/lib/Padre/Plugin/Perl6.pm
+++ Padre-Plugin-Perl6/lib/Padre/Plugin/Perl6.pm
@@ -1,3 +1,7 @@
+# Copyright 2008 Gabor Szabo.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
 package Padre::Plugin::Perl6;
 
 use 5.010;
@@ -5,7 +9,7 @@ use strict;
 use warnings;
 use English;
 use Carp;
-use feature qw(say);
+use feature qw(say switch);
 use IO::File;
 use File::Temp;
 
@@ -13,9 +17,90 @@ our $VERSION = '0.22';
 
 use URI::file;
 use Syntax::Highlight::Perl6;
+use Readonly;
 
 use Padre::Wx ();
 use base 'Padre::Plugin';
+
+Readonly my $FULL_HTML    => 'full_html';
+Readonly my $SIMPLE_HTML  => 'simple_html';
+Readonly my $SNIPPET_HTML => 'snippet_html';
+
+sub padre_interfaces {
+	'Padre::Plugin'         => 0.20,
+}
+
+
+sub menu_plugins_simple {
+	my $self = shift;
+	'Perl 6' => [
+		'About' => sub { $self->show_about },
+		'Export Full HTML' => sub { $self->export_html($FULL_HTML); },
+		'Export Simple HTML' => sub { $self->export_html($SIMPLE_HTML); },
+		'Export Snippet HTML' => sub { $self->export_html($SNIPPET_HTML); },
+	];
+}
+
+sub registered_documents {
+	'application/x-perl6'    => 'Padre::Document::Perl6',
+}
+
+
+sub show_about {
+	my ($main) = @ARG;
+
+	my $about = Wx::AboutDialogInfo->new;
+	$about->SetName("Padre::Plugin::Perl6");
+	$about->SetDescription(
+		"Perl6 syntax highlighting that is based on Syntax::Highlight::Perl6\n"
+	);
+	Wx::AboutBox( $about );
+	return;
+}
+
+sub export_html {
+	my ($self, $type) = @ARG;
+	
+	my $text = Padre::Documents->current->text_get() // '';
+	
+  my $p = Syntax::Highlight::Perl6->new(
+    text => $text,
+		inline_resources => 1, 
+  );
+  
+  my $html;
+	eval {
+		given($type) {
+			when ($FULL_HTML) { $html = $p->full_html; }
+			when ($SIMPLE_HTML) { $html = $p->simple_html; }
+			when ($SNIPPET_HTML) { $html = $p->snippet_html; }
+			default {
+				croak "'$type' should full_html, simple_html or snippet_html";
+			}
+		}
+		1;
+	};
+	
+	if($EVAL_ERROR) {
+		say 'Parsing error, bye bye ->export_html';
+		return;
+	}
+
+	my $tmp = File::Temp->new(SUFFIX => '.html');
+	$tmp->unlink_on_destroy(0);
+	my $filename = $tmp->filename;
+	print $tmp $html;
+	close $tmp
+		or croak "Could not close $filename";
+
+	my $file_url = URI::file->new($filename);
+	Wx::LaunchDefaultBrowser($file_url);	
+}
+
+
+1;
+
+__END__
 
 =head1 NAME
 
@@ -33,77 +118,3 @@ Copyright 2008 Gabor Szabo. L<http://www.szabgab.com/>
 
 This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl 5 itself.
-
-=cut
-
-sub padre_interfaces {
-	'Padre::Plugin'         => 0.20,
-}
-
-
-sub menu_plugins_simple {
-	my $self = shift;
-	'Perl 6' => [
-		'About' => sub { $self->show_about },
-		'Export HTML' => sub { $self->export_html },
-	];
-}
-
-sub registered_documents {
-	'application/x-perl6'    => 'Padre::Document::Perl6',
-}
-
-
-sub show_about {
-	my ($main) = @_;
-
-	my $about = Wx::AboutDialogInfo->new;
-	$about->SetName("Padre::Plugin::Perl6");
-	$about->SetDescription(
-		"Perl6 syntax highlighting that is based on Syntax::Highlight::Perl6\n"
-	);
-	#$about->SetVersion($Padre::VERSION);
-	Wx::AboutBox( $about );
-	return;
-}
-
-
-sub export_html {
-	my ($main) = @_;
-	
-	my $text = Padre::Documents->current->text_get() // '';
-	
-  my $p = Syntax::Highlight::Perl6->new(
-    text => $text,
-  );
-  
-  my $snippet_html;
-	eval {
-		$snippet_html = $p->snippet_html;
-		1;
-	};
-	
-	if($EVAL_ERROR) {
-		say 'Parsing error, bye bye ->export_html';
-		return;
-	}
-
-	my $tmp = File::Temp->new(SUFFIX => '.html');
-	$tmp->unlink_on_destroy(0);
-	my $filename = $tmp->filename;
-	
-	print $tmp $snippet_html;
-	close $tmp
-		or croak "Could not close $filename";
-
-	my $uri = URI::file->new($filename);
-	Wx::LaunchDefaultBrowser($uri);	
-}
-
-
-1;
-
-# Copyright 2008 Gabor Szabo.
-# LICENSE
-# This program is free software; you can redistribute it and/or
-# modify it under the same terms as Perl 5 itself.
