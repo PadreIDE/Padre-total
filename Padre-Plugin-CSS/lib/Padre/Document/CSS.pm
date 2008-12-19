94acb535bd167f92d0513075078c501e54b97e79
fayland <fayland@svngit.padre.perlide.org>
2008-12-19 05:19:00 +0000

    add Padre/Document/CSS.pm
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@2081 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ Padre-Plugin-CSS/lib/Padre/Document/CSS.pm
@@ -0,0 +1,15 @@
+package Padre::Document::CSS;
+
+use 5.008;
+use strict;
+use warnings;
+use Carp            ();
+use Padre::Document ();
+use YAML::Tiny      ();
+
+our $VERSION = '0.22';
+our @ISA     = 'Padre::Document';
+
+sub comment_lines_str { return '//' }
+
+1;
