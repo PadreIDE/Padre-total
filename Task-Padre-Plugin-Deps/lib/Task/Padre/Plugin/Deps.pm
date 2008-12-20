d9eb701ebbd6bd48dc9513452f5358aadf212ca5
fayland <fayland@svngit.padre.perlide.org>
2008-12-20 11:28:38 +0000

    two Task release
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@2114 18eddd70-d622-4293-bd05-fe65668e8bde

--- Task-Padre-Plugin-Deps/lib/Task/Padre/Plugin/Deps.pm
+++ Task-Padre-Plugin-Deps/lib/Task/Padre/Plugin/Deps.pm
@@ -9,11 +9,11 @@ Task::Padre::Plugin::Deps - prereqs of Padre::Plugins
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
--- Task-Padre-Plugins/lib/Task/Padre/Plugins.pm
+++ Task-Padre-Plugins/lib/Task/Padre/Plugins.pm
@@ -9,11 +9,11 @@ Task::Padre::Plugins - Get many Plugins of Padre at once
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 
 =head1 SYNOPSIS
