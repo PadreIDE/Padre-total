8a0b3c664dfbcefa84e82418b92805d6a8e5b00e
fayland <fayland@svngit.padre.perlide.org>
2008-12-22 01:49:46 +0000

    from Module::Build to Module::Install
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@2144 18eddd70-d622-4293-bd05-fe65668e8bde

--- Task-Padre-Plugins/lib/Task/Padre/Plugins.pm
+++ Task-Padre-Plugins/lib/Task/Padre/Plugins.pm
@@ -9,11 +9,11 @@ Task::Padre::Plugins - Get many Plugins of Padre at once
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 
 =head1 SYNOPSIS
