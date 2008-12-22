5c43f0becf5ab41694ebd86d047adadc7571840a
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2008-12-22 07:19:47 +0000

    Code cleanup: Trim trailing spaces and tabs to space
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Padre-Plugin-Perl6@2153 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Plugin/Perl6.pm
+++ lib/Padre/Plugin/Perl6.pm
@@ -27,85 +27,85 @@ Readonly my $SIMPLE_HTML  => 'simple_html';
 Readonly my $SNIPPET_HTML => 'snippet_html';
 
 sub padre_interfaces {
-	return 'Padre::Plugin'         => 0.20,
+    return 'Padre::Plugin'         => 0.20,
 }
 
 
 sub menu_plugins_simple {
-	my $self = shift;
-	return 'Perl 6' => [
-		'Export Full HTML' => sub { $self->export_html($FULL_HTML); },
-		'Export Simple HTML' => sub { $self->export_html($SIMPLE_HTML); },
-		'Export Snippet HTML' => sub { $self->export_html($SNIPPET_HTML); },
-		'About' => sub { $self->show_about },
-	];
+    my $self = shift;
+    return 'Perl 6' => [
+        'Export Full HTML' => sub { $self->export_html($FULL_HTML); },
+        'Export Simple HTML' => sub { $self->export_html($SIMPLE_HTML); },
+        'Export Snippet HTML' => sub { $self->export_html($SNIPPET_HTML); },
+        'About' => sub { $self->show_about },
+    ];
 }
 
 sub registered_documents {
-	return 'application/x-perl6'    => 'Padre::Document::Perl6',
+    return 'application/x-perl6'    => 'Padre::Document::Perl6',
 }
 
 
 sub show_about {
-	my ($main) = @ARG;
+    my ($main) = @ARG;
 
-	my $about = Wx::AboutDialogInfo->new;
-	$about->SetName("Padre::Plugin::Perl6");
-	$about->SetDescription(
-		"Perl6 syntax highlighting that is based on\nSyntax::Highlight::Perl6\n"
-	);
-	Wx::AboutBox( $about );
-	return;
+    my $about = Wx::AboutDialogInfo->new;
+    $about->SetName("Padre::Plugin::Perl6");
+    $about->SetDescription(
+        "Perl6 syntax highlighting that is based on\nSyntax::Highlight::Perl6\n"
+    );
+    Wx::AboutBox( $about );
+    return;
 }
 
 sub export_html {
-	my ($self, $type) = @ARG;
+    my ($self, $type) = @ARG;
 
-	if(!defined Padre::Documents->current) {
-		return;
-	}
+    if(!defined Padre::Documents->current) {
+        return;
+    }
 
-	my $text = Padre::Documents->current->text_get() // '';
+    my $text = Padre::Documents->current->text_get() // '';
 
     my $p = Syntax::Highlight::Perl6->new(
         text => $text,
-        inline_resources => 1, 
+        inline_resources => 1,
     );
 
     my $html;
     eval {
-	    given($type) {
-			when ($FULL_HTML) { $html = $p->full_html; }
-			when ($SIMPLE_HTML) { $html = $p->simple_html; }
-			when ($SNIPPET_HTML) { $html = $p->snippet_html; }
-			default {
-				croak "'$type' should full_html, simple_html or snippet_html";
-			}
-		}
-		1;
-	};
+        given($type) {
+            when ($FULL_HTML) { $html = $p->full_html; }
+            when ($SIMPLE_HTML) { $html = $p->simple_html; }
+            when ($SNIPPET_HTML) { $html = $p->snippet_html; }
+            default {
+                croak "'$type' should full_html, simple_html or snippet_html";
+            }
+        }
+        1;
+    };
 
-	if($EVAL_ERROR) {
-		say 'Parsing error, bye bye ->export_html';
-		return;
-	}
+    if($EVAL_ERROR) {
+        say 'Parsing error, bye bye ->export_html';
+        return;
+    }
 
-	# create a temporary HTML file
-	my $tmp = File::Temp->new(SUFFIX => '.html');
-	$tmp->unlink_on_destroy(0);
-	my $filename = $tmp->filename;
-	print $tmp $html;
-	close $tmp
-		or croak "Could not close $filename";
+    # create a temporary HTML file
+    my $tmp = File::Temp->new(SUFFIX => '.html');
+    $tmp->unlink_on_destroy(0);
+    my $filename = $tmp->filename;
+    print $tmp $html;
+    close $tmp
+        or croak "Could not close $filename";
 
-	# try to open the HTML file
-	my $main   = Padre->ide->wx->main_window;
-	$main->setup_editor($filename);
+    # try to open the HTML file
+    my $main   = Padre->ide->wx->main_window;
+    $main->setup_editor($filename);
 
-	# launch the HTML file in your default browser
-	my $file_url = URI::file->new($filename);
-	Wx::LaunchDefaultBrowser($file_url);
-    
+    # launch the HTML file in your default browser
+    my $file_url = URI::file->new($filename);
+    Wx::LaunchDefaultBrowser($file_url);
+
     return;
 }
 
