32dbe1ff7ffba77d22f4d488542453399478bce2
Gabor Szabo <szabgab@gmail.com>
2009-01-02 12:27:35 +0000

    prepare for release Parse-ErrorString-Perl-0.10
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@2359 18eddd70-d622-4293-bd05-fe65668e8bde

--- Parse-ErrorString-Perl/lib/Parse/ErrorString/Perl.pm
+++ Parse-ErrorString-Perl/lib/Parse/ErrorString/Perl.pm
@@ -4,7 +4,7 @@ Parse::ErrorString::Perl - Parse error messages from the perl interpreter
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =head1 SYNOPSIS
 
@@ -229,7 +229,7 @@ sub stack {
 
 package Parse::ErrorString::Perl;
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 use Carp;
 use Pod::Find;
