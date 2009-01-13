5f47cd1f10256ea45daa065f79357a59adecffa4
Gabor Szabo <szabgab@gmail.com>
2009-01-13 20:50:34 +0000

    add comments on how to build parrot and rakudo
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@2795 18eddd70-d622-4293-bd05-fe65668e8bde

--- tools/include_parrot.pl
+++ tools/include_parrot.pl
@@ -8,10 +8,44 @@ use File::Spec;
 use File::Path qw(mkpath);
 use File::Copy qw(copy);
 
-my $src  = "c:/gabor/parrot";
+#my $src  = "c:/gabor/parrot";
+#my $dest = "c:/portable/parrot";
+
+# assuing I have Portable Strawberry installed in C:\
+# C:
+# cd \portable
+# svn export http://svn.perl.org/parrot/trunk parrot  (35492)
+# cd parrot
+# c:\portable\perl\bin\perl.exe Configure.pl
+# mingw32-make
+# cd languages\perl6
+# mingw32-make
+# cd ..
+# rename parrot x
+# copy the files from 
+
+my $src  = "c:/portable/x";
 my $dest = "c:/portable/parrot";
 
-foreach my $file (<DATA>) {
+my @windows_files = qw(
+	parrot.exe
+	libparrot.dll
+);
+my @linux_files = qw(
+	parrot
+	blib/lib/libparrot.so.0.8.2
+);
+
+my @files = qw(languages/perl6/perl6.pbc);
+if ($^O eq "MSWin32") {
+	push @files, @windows_files;
+} else {
+	push @files, @linux_files;
+}
+
+# runtime and all its subdirectories
+
+foreach my $file (@files) {
 	print $file;
 	chomp $file;
 	next if not $file;
@@ -22,7 +56,3 @@ foreach my $file (<DATA>) {
 	copy(File::Spec->catfile($src, $file), File::Spec->catdir($dest, $path));
 }
 
-__END__
-parrot.exe
-libparrot.dll
-languages/perl6/perl6.pbc
