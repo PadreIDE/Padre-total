ec77948b78937b44b4c82a77ca5de85a371b7bec
Gabor Szabo <szabgab@gmail.com>
2009-03-07 11:22:05 +0000

    SVN: add diff and commit support svn plugin
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@3191 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-SVN/lib/Padre/Plugin/SVN.pm
+++ Padre-Plugin-SVN/lib/Padre/Plugin/SVN.pm
@@ -13,7 +13,7 @@ use Padre::Util   ();
 #use File::Basename ();
 #use File::Spec;
 
-use VCI;
+#use VCI;
 
 our $VERSION = '0.01';
 our @ISA     = 'Padre::Plugin';
@@ -66,20 +66,19 @@ sub menu_plugins_simple {
 	my $self = shift;
 	return $self->plugin_name => [
 		'About'             => sub { $self->show_about },
-#		'Commit...' => [
-#			'File'     => sub { $self->svn_commit_file },
-#			'Project'  => sub { $self->svn_commit_project },
-#		],
+		'Commit...' => [
+			'File'     => sub { $self->svn_commit_file },
+			'Project'  => sub { $self->svn_commit_project },
+		],
 		'Status...' => [
 			'File'    => sub { $self->svn_status_of_file },
-#			'Dir'     => sub { $self->svn_status_of_dir },
-#			'Project' => sub { $self->svn_status_of_project },
+			'Project' => sub { $self->svn_status_of_project },
 		],
-#		'Diff...' => [
-#			'File'    => sub { $self->svn_diff_of_file },
-#			'Dir'     => sub { $self->svn_diff_of_dir },
-#			'Project' => sub { $self->svn_diff_of_project },
-#		],
+		'Diff...' => [
+			'File'    => sub { $self->svn_diff_of_file },
+			'Dir'     => sub { $self->svn_diff_of_dir },
+			'Project' => sub { $self->svn_diff_of_project },
+		],
 	];
 }
 
@@ -106,22 +105,10 @@ END_MESSAGE
 sub svn_status {
 	my ($self, $path) = @_;
 	my $main = Padre->ide->wx->main;
-
-	# TODO: connect to SVN repo without this workaround
-	my @info = qx{svn info $path};
-	if (not @info) {
-		$main->error("$path does not seem to be under SVN");
-		return;
-	}
-	my ($repo) = grep { $_ =~ /^Repository Root: / } @info;
-	$repo =~ s/^Repository Root:\s*//; 
-	$main->message($repo, "File");
-	my $repository = VCI->connect(type => 'Svn', repo => $repo);
-	print $repository;
-
+	my $status = qx{svn status $path};
+	$main->message($status, "$path");
 	return;
 }
-
 sub svn_status_of_file {
 	my ($self) = @_;
 	
@@ -131,6 +118,102 @@ sub svn_status_of_file {
 	$self->svn_status($filename);
 	return;
 }
+sub svn_status_of_project {
+	my ($self) = @_;
+	
+	my $main = Padre->ide->wx->main;
+	my $doc = $main->current->document;
+	my $filename = $doc->filename;
+	my $dir = Padre::Util::get_project_dir($filename);
+	$self->svn_status($dir);
+	return;
+}
+
+
+sub svn_diff {
+	my ($self, $path) = @_;
+	my $main = Padre->ide->wx->main;
+	my $status = qx{svn diff $path};
+	$main->message($status, "$path");
+	return;
+}
+sub svn_diff_of_file {
+	my ($self) = @_;
+	
+	my $main = Padre->ide->wx->main;
+	my $doc = $main->current->document;
+	my $filename = $doc->filename;
+	$self->svn_diff($filename);
+	return;
+}
+
+sub svn_diff_of_project {
+	my ($self) = @_;
+	
+	my $main = Padre->ide->wx->main;
+	my $doc = $main->current->document;
+	my $filename = $doc->filename;
+	my $dir = Padre::Util::get_project_dir($filename);
+	$self->svn_diff($dir);
+	return;
+}
+
+sub svn_commit {
+	my ($self, $path) = @_;
+	
+	my $main = Padre->ide->wx->main;
+	my $message = $main->prompt("SVN Commit of $path", "Please type in your message", "MY_SVN_COMMIT");
+	if ($message) {
+		$main->message( $message, 'Filename' );
+		system qq(svn commit $path -m"$message");
+	}
+
+	return;	
+}
+
+sub svn_commit_file {
+	my ($self) = @_;
+	
+	my $main = Padre->ide->wx->main;
+	my $doc = $main->current->document;
+	my $filename = $doc->filename;
+	$self->svn_commit($filename);
+	return;
+}
+
+sub svn_commit_project {
+	my ($self) = @_;
+	
+	my $main = Padre->ide->wx->main;
+	my $doc = $main->current->document;
+	my $filename = $doc->filename;
+	my $dir = Padre::Util::get_project_dir($filename);
+	$self->svn_commit($dir);
+	return;
+}
+
+
+#sub vci {
+#	my ($self, $path) = @_;
+#	my $main = Padre->ide->wx->main;
+#	# TODO: connect to SVN repo without this workaround
+#	my @info = qx{svn info $path};
+#	if (not @info) {
+#		$main->error("$path does not seem to be under SVN");
+#		return;
+#	}
+#	chomp @info;
+#	my ($repo) = grep { $_ =~ /^Repository Root: / } @info;
+#	$repo =~ s/^Repository Root:\s*//; 
+#	$main->message("'$repo'", "File");
+#	my $repository = VCI->connect(type => 'Svn', repo => $repo);
+#	print "$repository\n";
+#	print "---\n";
+#	print $repository->projects, "\n";
+#
+#	return;
+#}
+#
 
 1;
 
