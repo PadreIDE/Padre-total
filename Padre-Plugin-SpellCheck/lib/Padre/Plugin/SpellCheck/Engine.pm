0c821265cfc311fffc4183b07dadd426336edd39
jquelin <jquelin@svngit.padre.perlide.org>
2009-04-20 14:33:29 +0000

    creation of module that will hold the spellcheck logic
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@4009 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ Padre-Plugin-SpellCheck/lib/Padre/Plugin/SpellCheck/Engine.pm
@@ -0,0 +1,59 @@
+#
+# This file is part of Padre::Plugin::SpellCheck.
+# Copyright (c) 2009 Jerome Quelin, all rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+#
+#
+
+package Padre::Plugin::SpellCheck::Engine;
+
+use warnings;
+use strict;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Padre::Plugin::SpellCheck::Engine - spell engine for plugin
+
+
+
+=head1 DESCRIPTION
+
+This plugins allows one to checking her text spelling within Padre. It
+is using C<Text::Aspell> underneath, so check this module's pod for more
+information.
+
+Of course, you need to have the aspell binary and dictionnary installed.
+
+
+
+=head1 PUBLIC METHODS
+
+
+
+=head1 SEE ALSO
+
+For all related information (bug reporting, source code repository,
+etc.), refer to L<Padre::Plugin::SpellCheck>.
+
+
+
+=head1 AUTHOR
+
+Jerome Quelin, C<< <jquelin@cpan.org> >>
+
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (c) 2009 Jerome Quelin, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
