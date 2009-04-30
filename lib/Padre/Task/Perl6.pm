8fd42ce8ed676d23c8a175bc769041d10588eba8
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-04-30 03:33:00 +0000

    Updated Perl6 plugin to version 0.35
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Padre-Plugin-Perl6@4245 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Task/Perl6.pm
+++ lib/Padre/Task/Perl6.pm
@@ -12,7 +12,7 @@ use File::Basename;
 use File::Spec;
 use Cwd;
  
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 our $thread_running = 0;
 
 # This is run in the main thread before being handed
--- lib/Padre/Task/SyntaxChecker/Perl6.pm
+++ lib/Padre/Task/SyntaxChecker/Perl6.pm
@@ -3,7 +3,7 @@ package Padre::Task::SyntaxChecker::Perl6;
 use strict;
 use warnings;
 
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 
 use base 'Padre::Task::SyntaxChecker';
 
