38400a26455fd0eba7060dcf49b1d6baa8adf8ff
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-05-04 22:48:05 +0000

    A preliminary attempt at fixing wperl ticket:302 along with proper warnings/errors parsing.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@4274 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Perl6/lib/Padre/Task/p6tokens.pl
+++ Padre-Plugin-Perl6/lib/Padre/Task/p6tokens.pl
@@ -5,24 +5,32 @@
 use strict;
 use warnings;
 
-use STD;
-use Syntax::Highlight::Perl6;
-use Storable qw( nfreeze );
+# parse arguments
+my $num_args = $#ARGV + 1;
+if($num_args != 1) {
+    die "p6tokens.pl needs one file.\n";
+}
 
-# read input from STDIN
-my $text = '';
-my $line;
-binmode STDIN;
-while($line = <STDIN>) {
-    $text .= $line;
+# Load file into a scalar without File::Slurp (see perlfaq5)
+my $filename = $ARGV[0];
+my $text;
+{
+	# slurp the input file
+	open IN, $filename or die "Could not open $filename for reading";
+	local $/ = undef;   #enable localized slurp mode
+	$text = <IN>;
+	close IN or die "Could not close $filename";
 }
 
 # create a syntax highlighter and serialize its tokens to STDOUT
+require STD;
+require Syntax::Highlight::Perl6;
 my $p = Syntax::Highlight::Perl6->new(
     text => $text,
 );
 my @tokens = $p->tokens;
-my $output = nfreeze(\@tokens);
+require Storable;
+my $output = Storable::nfreeze(\@tokens);
 binmode STDOUT;
 print $output;
 
