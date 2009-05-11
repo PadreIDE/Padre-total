e2e005cfc091435ca0ece4264e5d613599458b3b
jquelin <jquelin@svngit.padre.perlide.org>
2009-05-11 18:56:00 +0000

    v0.2.1
    - adding missing prereq
    
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@4541 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Nopaste/lib/Padre/Plugin/Nopaste.pm
+++ Padre-Plugin-Nopaste/lib/Padre/Plugin/Nopaste.pm
@@ -18,7 +18,7 @@ use Module::Util          qw{ find_installed };
 
 use parent qw{ Padre::Plugin Padre::Task };
 
-our $VERSION = '0.2.0';
+our $VERSION = '0.2.1';
 
 
 # -- padre plugin api, refer to Padre::Plugin
