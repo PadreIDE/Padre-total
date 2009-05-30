a8ad4afc53e09ea58d811c74703d95543f7eaa21
dam <dam@svngit.padre.perlide.org>
2009-05-30 09:01:18 +0000

    add links to plugin modules to ease navigation on search.cpan.org
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@5001 18eddd70-d622-4293-bd05-fe65668e8bde

--- Task-Padre-Plugins/lib/Task/Padre/Plugins.pm
+++ Task-Padre-Plugins/lib/Task/Padre/Plugins.pm
@@ -24,121 +24,181 @@ If not, they run with the whole text from selected document.
 
 L<Acme::PlayCode> Plugin for L<Padre>
 
+See L<Padre::Plugin::AcmePlayCode>
+
 =head2 Padre::Plugin::Alarm
 
 Alarm Clock in Padre
+
+See L<Padre::Plugin::Alarm>
 
 =head2 Padre::Plugin::Autoformat
 
 reformat your text within Padre
 
+See L<Padre::Plugin::Autoformat>
+
 =head2 Padre::Plugin::CSS
 
 L<Padre> and CSS
+
+See L<Padre::Plugin::CSS>
 
 =head2 Padre::Plugin::Catalyst
 
 Simple Catalyst helper interface for Padre
 
+See L<Padre::Plugin::Catalyst>
+
 =head2 Padre::Plugin::CommandLine
 
 vi and emacs in Padre ?
+
+See L<Padre::Plugin::CommandLine>
 
 =head2 Padre::Plugin::DataWalker
 
 Simple Perl data structure browser Padre
 
+See L<Padre::Plugin::DataWalker>
+
 =head2 Padre::Plugin::Debugger
 
 Debug Perl code from Padre editor
+
+See L<Padre::Plugin::Debugger>
 
 =head2 Padre::Plugin::Ecliptic
 
 Padre plugin that provides Eclipse killer features
 
+See L<Padre::Plugin::Ecliptic>
+
 =head2 Padre::Plugin::Encode
 
 convert file to different encoding in Padre
+
+See L<Padre::Plugin::Encode>
 
 =head2 Padre::Plugin::Encrypt
 
 encrypt/decrypt file in Padre
 
+See L<Padre::Plugin::Encrypt>
+
 =head2 Padre::Plugin::Git
 
 Simple Git interface for Padre
+
+See L<Padre::Plugin::Git>
 
 =head2 Padre::Plugin::HTML
 
 L<Padre> and HTML
 
+See L<Padre::Plugin::HTML>
+
 =head2 Padre::Plugin::HTMLExport
 
 export highlighted HTML in Padre
+
+See L<Padre::Plugin::HTMLExport>
 
 =head2 Padre::Plugin::InstallPARDist
 
 Installation of .par archives into the system
 
+See L<Padre::Plugin::InstallPARDist>
+
 =head2 Padre::Plugin::JavaScript
 
 L<Padre> and JavaScript
+
+See L<Padre::Plugin::JavaScript>
 
 =head2 Padre::Plugin::Mojolicious
 
 Simple Mojolicious helper interface for Padre
 
+See L<Padre::Plugin::Mojolicious>
+
 =head2 Padre::Plugin::Nopaste
 
 send code on a nopaste website from padre
+
+See L<Padre::Plugin::Nopaste>
 
 =head2 Padre::Plugin::PAR
 
 PAR generation from Padre
 
+See L<Padre::Plugin::PAR>
+
 =head2 Padre::Plugin::PSI
 
 Experimental Padre plugin written in Perl 6
+
+See L<Padre::Plugin::PSI>
 
 =head2 Padre::Plugin::Parrot
 
 Experimental Padre plugin that runs on Parrot
 
+See L<Padre::Plugin::Parrot>
+
 =head2 Padre::Plugin::Perl6
 
 Padre plugin for Perl6
+
+See L<Padre::Plugin::Perl6>
 
 =head2 Padre::Plugin::PerlCritic
 
 Analyze perl files with Perl::Critic
 
+See L<Padre::Plugin::PerlCritic>
+
 =head2 Padre::Plugin::PerlTidy
 
 Format perl files using Perl::Tidy
+
+See L<Padre::Plugin::PerlTidy>
 
 =head2 Padre::Plugin::SVK
 
 Simple SVK interface for Padre
 
+See L<Padre::Plugin::SVK>
+
 =head2 Padre::Plugin::SVN
 
 Simple SVN interface for Padre
+
+See L<Padre::Plugin::SVN>
 
 =head2 Padre::Plugin::SpellCheck
 
 check spelling in Padre
 
+See L<Padre::Plugin::SpellCheck>
+
 =head2 Padre::Plugin::Vi
 
 vi keyboard for Padre
+
+See L<Padre::Plugin::Vi>
 
 =head2 Padre::Plugin::ViewInBrowser
 
 view selected doc in browser for L<Padre>
 
+See L<Padre::Plugin::ViewInBrowser>
+
 =head2 Padre::Plugin::XML
 
 L<Padre> and XML
+
+See L<Padre::Plugin::XML>
 
 =head1 AUTHOR
 
