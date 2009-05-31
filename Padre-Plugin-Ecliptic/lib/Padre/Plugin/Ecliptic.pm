127abd8a0ef7271b51bea8d06df65ba7326948ac
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-05-31 04:36:10 +0000

    [Ecliptic] Preparing for 0.06 CPAN release.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@5050 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Ecliptic/lib/Padre/Plugin/Ecliptic.pm
+++ Padre-Plugin-Ecliptic/lib/Padre/Plugin/Ecliptic.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 # package exports and version
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 our @EXPORT_OK = ();
 
 # module imports
--- Padre-Plugin-Ecliptic/lib/Padre/Plugin/Ecliptic/QuickMenuAccessDialog.pm
+++ Padre-Plugin-Ecliptic/lib/Padre/Plugin/Ecliptic/QuickMenuAccessDialog.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 
 # package exports and version
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 our @EXPORT_OK = ();
 
 # module imports
