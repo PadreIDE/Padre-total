f28310745bdeeba984680fc35da263bb719ca037
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-06-10 08:34:55 +0000

    [Ecliptic] More POD documentation
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@5138 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Ecliptic/lib/Padre/Plugin/Ecliptic.pm
+++ Padre-Plugin-Ecliptic/lib/Padre/Plugin/Ecliptic.pm
@@ -99,7 +99,7 @@ sub menu_plugins {
 		sub { $self->_show_quick_module_access_dialog(); },
 	);
 
-	# "Open in explorer" action
+	# "Open in Explorer" action
 	Wx::Event::EVT_MENU(
 		$main_window,
 		$self->{menu}->Append( -1, _T("Open in Explorer\tCtrl-6"), ),
@@ -262,20 +262,31 @@ when you press the OK button.
 You can simply ignore CVS, .svn and .git folders using a simple checkbox 
 (enhancement over Eclipse).
 
-=head2 'Quick Menu Access' (Shortcut: Ctrl-3)
+=head2 List Key Bindings (Shortcut: Ctrl-Shift-L)
+
+This opens a dialog with a yellow list of current Padre actions/shortcuts. When you hit the OK 
+button, the selected Padre action will be performed.
+
+=head2 Quick Menu Access (Shortcut: Ctrl-3)
 
 This opens a dialog where you can search for menu labels. When you hit the OK 
 button, the menu item will be selected.
 
-=head2 'Quick Outline Access' (Shortcut: Ctrl-4)
+=head2 Quick Outline Access (Shortcut: Ctrl-4)
 
 This opens a dialog where you can search for outline tree. When you hit the OK 
 button, the outline element in the outline tree will be selected.
 
-=head2 'Quick Module Access' (Shortcut: Ctrl-5)
+=head2 Quick Module Access (Shortcut: Ctrl-5)
 
 This opens a dialog where you can search for a CPAN module. When you hit the OK 
 button, the selected module will be displayed in Padre's POD browser.
+
+=head2 Open in Explorer (Shortcut: Ctrl-6)
+
+For the current saved Padre document, open the platform's file manager and tries to select it if possible.
+On win32, opens the containing folder and selects the file in explorer. On *inux KDE/GNOME, 
+opens the containing folder for it.
 
 =head2 'About'
 
