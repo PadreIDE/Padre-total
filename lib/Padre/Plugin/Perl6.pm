d48b1a4544fa5fae150e83ae9d7a22ad5c6e4d18
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-06-21 11:07:30 +0000

    [Perl 6] Updated version for the 0.44 CPAN release. Release theme: Perl 6 Quick Fixes and support for Ecliptic!
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Padre-Plugin-Perl6@5431 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Plugin/Perl6.pm
+++ lib/Padre/Plugin/Perl6.pm
@@ -8,7 +8,7 @@ use Padre::Wx   ();
 use base 'Padre::Plugin';
 
 # exports and version
-our $VERSION   = '0.43';
+our $VERSION   = '0.44';
 our @EXPORT_OK = qw(plugin_config);
 
 # constants for html exporting
--- lib/Padre/Plugin/Perl6/Perl6OutlineTask.pm
+++ lib/Padre/Plugin/Perl6/Perl6OutlineTask.pm
@@ -3,7 +3,7 @@ package Padre::Plugin::Perl6::Perl6OutlineTask;
 use strict;
 use warnings;
 
-our $VERSION = '0.43';
+our $VERSION = '0.44';
 
 use base 'Padre::Task::Outline';
 
