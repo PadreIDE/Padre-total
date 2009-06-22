a368e20e5f1f52d47af85ca96a287a58f15ecb1c
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-06-22 12:19:53 +0000

    [Perl 6] Implemented "Use [N..*] for indefinite range" quick fix
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@5466 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Perl6/lib/Padre/Plugin/Perl6/Perl6Document.pm
+++ Padre-Plugin-Perl6/lib/Padre/Plugin/Perl6/Perl6Document.pm
@@ -384,7 +384,24 @@ sub _find_quick_fix {
 					},
 				};
 				
-			}
+			} elsif($issue_msg =~ /^Please use \.\.\* for indefinite range/) {
+
+				push @items, {
+					text     => Wx::gettext("Use [N..*] for indefinite range"),
+					listener => sub { 
+						#Replace first '[1..]' with '[1..*]' in the current line
+						my $line_start = $editor->PositionFromLine( $current_line_no );
+						my $line_end   = $editor->GetLineEndPosition( $current_line_no );
+						my $line_text  = $editor->GetTextRange($line_start, $line_end);
+						$line_text =~ s/\[\s*(.+?)\.\.\s*\]/\[$1..*\]/;
+						$editor->SetSelection( $line_start, $line_end );
+						$editor->ReplaceSelection( $line_text );
+					},
+				};
+			
+			} 
+			
+			
 
 			if($comment_error_action) {
 				push @items, {
