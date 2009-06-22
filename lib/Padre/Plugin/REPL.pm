4dba185d31107186db57b3a2b6a84da3519a9288
Ryan Niebur <rsn@cpan.org>
2009-06-22 04:56:33 +0000

    add a tab to the bottom notebook for the REPL, and add an input text box, a button, and a output box to it. I need to figure out how to make these size correctly.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Padre-Plugin-REPL@5441 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Plugin/REPL.pm
+++ lib/Padre/Plugin/REPL.pm
@@ -1,3 +1,48 @@
+package Padre::Plugin::REPL::Panel;
+
+use Padre::Wx;
+use Padre::Util qw/_T/;
+use base 'Wx::Panel';
+
+sub new {
+	my $class      = shift;
+	my $main       = shift;
+	my $self       = $class->SUPER::new( Padre::Current->main->bottom );
+	my $box        = Wx::BoxSizer->new(Wx::wxVERTICAL);
+	my $bottom_box = Wx::BoxSizer->new(Wx::wxHORIZONTAL);
+	my $output     = Wx::TextCtrl->new(
+		$self,
+		-1,
+		"",
+		Wx::wxDefaultPosition,
+		Wx::wxDefaultSize,
+		Wx::wxTE_READONLY
+			| Wx::wxTE_MULTILINE
+			| Wx::wxTE_DONTWRAP
+			| Wx::wxNO_FULL_REPAINT_ON_RESIZE,
+	);
+	$box->Add($output);
+	my $input = Wx::TextCtrl->new(
+		$self,
+		-1,
+		"",
+		Wx::wxDefaultPosition,
+		Wx::wxDefaultSize
+	);
+	my $button = Wx::Button->new( $self, -1, _T("Evaluate") );
+	Wx::Event::EVT_BUTTON( $self, $button, \&Padre::Plugin::REPL::evaluate );
+	$bottom_box->Add($input);
+	$bottom_box->Add($button);
+	$box->Add($bottom_box);
+	$self->SetSizer($box);
+	Padre::Current->main->bottom->show($self);
+	return ( $input, $output );
+}
+
+sub gettext_label {
+	return "REPL";
+}
+
 package Padre::Plugin::REPL;
 
 use warnings;
@@ -13,16 +58,22 @@ use Padre::Util qw/_T/;
 use Devel::REPL;
 use Capture::Tiny qw/capture_merged/;
 use Devel::REPL::Script;
+use Class::Unload;
 
 our $VERSION = '0.01';
 
 our $repl;
+our ( $input, $output );
+
+sub make_panel {
+	( $input, $output ) = Padre::Plugin::REPL::Panel->new();
+}
 
 sub padre_interfaces {
 	return 'Padre::Plugin' => '0.26';
 }
 
-sub menu_plugins_simple {
+sub menu_plugins_simple_DISABLED {
 	return "REPL" => [
 		( _T('Evaluate something') . "\tCtrl+e" ) => \&dialog,
 	];
@@ -32,6 +83,10 @@ sub plugin_enable {
 	_init_repl();
 }
 
+sub plugin_disable {
+	Class::Unload->unload('Padre::Plugin::REPL::Frame');
+}
+
 sub dialog {
 	my $code = Wx::GetTextFromUser("What do you want to evaluate?");
 	my $res  = _eval_repl($code);
@@ -39,8 +94,16 @@ sub dialog {
 	Padre::Current->main->show_output(1);
 }
 
+sub evaluate {
+	my $code = $input->GetValue();
+	my $res  = _eval_repl($code);
+	$output->AppendText("# $code\n$res\n");
+	$input->SetValue("");
+}
+
 sub _init_repl {
 	return if ( defined($repl) );
+	make_panel();
 	my $temp = Devel::REPL::Script->new();
 	$repl = $temp->_repl();
 	$temp->load_profile( $temp->profile );
