891437069ce6d5c258917504a8728be11c627d9a
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-06-22 14:46:49 +0000

    [Perl 6] Implemented "Use ~= instead of .=" quick fix.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Padre-Plugin-Perl6@5471 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Plugin/Perl6/Perl6Document.pm
+++ lib/Padre/Plugin/Perl6/Perl6Document.pm
@@ -449,6 +449,25 @@ sub _find_quick_fix {
 					},
 				};
 			
+			} elsif($issue_msg =~ /^\s*Possible obsolete use of \.\= as append operator/i) {
+
+				# Fixes the following:
+				# $string .= "a";
+				# into:
+				# $string ~= "a";
+				push @items, {
+					text     => Wx::gettext("Use ~= instead of .="),
+					listener => sub { 
+						#Replace first '=.' with '=~' in the current line
+						my $line_start = $editor->PositionFromLine( $current_line_no );
+						my $line_end   = $editor->GetLineEndPosition( $current_line_no );
+						my $line_text  = $editor->GetTextRange($line_start, $line_end);
+						$line_text =~ s/\.\=/~=/;
+						$editor->SetSelection( $line_start, $line_end );
+						$editor->ReplaceSelection( $line_text );
+					},
+				};
+			
 			}
 			
 			if(not $comment_error_added) {
