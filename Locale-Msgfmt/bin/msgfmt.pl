29f0a7a89f4e8ab190bee1c000c6183d6c426f34
Ryan Niebur <rsn@cpan.org>
2009-06-27 00:28:08 +0000

    make the interface a bit nicer
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@5618 18eddd70-d622-4293-bd05-fe65668e8bde

--- Locale-Msgfmt/bin/msgfmt.pl
+++ Locale-Msgfmt/bin/msgfmt.pl
@@ -9,9 +9,5 @@ use warnings;
 my($opt_o, $opt_f);
 GetOptions("output-file|o=s" => \$opt_o, "use-fuzzy|f" => \$opt_f);
 my $in = shift;
-if(!(defined($in) && defined($opt_o))) {
-  print "usage: $0 [-f] -o output.mo input.po\n";
-  exit(1);
-}
 
 msgfmt({in => $in, out => $opt_o, fuzzy => $opt_f});
