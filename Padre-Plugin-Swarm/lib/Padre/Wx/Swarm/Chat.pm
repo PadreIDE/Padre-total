45e13c764f3e6af0f1a70473fcd542d9964d602f
submersible_toaster <submersible_toaster@svngit.padre.perlide.org>
2009-06-27 08:08:23 +0000

    w00t! - swarm chat 0.1 functional.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@5658 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Swarm/lib/Padre/Wx/Swarm/Chat.pm
+++ Padre-Plugin-Swarm/lib/Padre/Wx/Swarm/Chat.pm
@@ -7,20 +7,50 @@ use Params::Util qw{_INSTANCE};
 use Padre::Wx ();
 
 our $VERSION = '0.37';
-our @ISA     = 'Wx::TextCtrl';
+our @ISA     = 'Wx::Panel';
 
 #our $EVT_Chat = Wx::NewEventType();
-
+use Class::XSAccessor
+	accessors => {
+		textinput => 'textinput',
+		chatframe => 'chatframe',
+	};
+	
 sub new {
 	my $class = shift;
 	my $main  = shift;
 	my $self = $class->SUPER::new(
 		$main->bottom,-1,
-		'',
+		#'',
 		Wx::wxDefaultPosition,
 		Wx::wxDefaultSize,
 		Wx::wxLC_REPORT | Wx::wxLC_SINGLE_SEL
 	);
+	
+	my $sizer = Wx::BoxSizer->new(Wx::wxVERTICAL);
+	
+	my $text = Wx::TextCtrl->new($self,-1,'',
+		Wx::wxDefaultPosition,
+		Wx::wxDefaultSize,
+		Wx::wxTE_PROCESS_ENTER
+	);
+	my $chat = Wx::TextCtrl->new($self,-1,'',
+		Wx::wxDefaultPosition,
+		Wx::wxDefaultSize,
+		Wx::wxTE_READONLY|Wx::wxTE_MULTILINE|Wx::wxNO_FULL_REPAINT_ON_RESIZE
+	);
+	$sizer->Add($chat,1, Wx::wxGROW );
+	$sizer->Add($text,0, Wx::wxGROW );
+	
+	$self->textinput( $text );
+	$self->chatframe( $chat );
+	$self->SetSizer($sizer);
+	
+	Wx::Event::EVT_TEXT_ENTER(
+                $self, $text,
+                \&on_text_enter
+        );
+
 	
 	return $self;
 }
@@ -101,7 +131,7 @@ sub poll_service {
 		my $output = sprintf( "%s@[%s] :%s\n",
 			$user, $ip, $content
 		);
-		$self->AppendText( $output );
+		$self->chatframe->AppendText( $output );
 	}
 }
 
@@ -111,6 +141,14 @@ sub tell_service {
 	my $args = shift;
 	my $service = $self->service;
 	$service->chat( $body );
+	
+}
+
+sub on_text_enter {
+	my ($self,$event) = @_;
+	my $message = $self->textinput->GetValue;
+	$self->tell_service( $message );
+	$self->textinput->SetValue('');
 	
 }
 1;
