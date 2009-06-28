30800ca7c380485adbd079bfd099e3cc9c83532f
submersible_toaster <submersible_toaster@svngit.padre.perlide.org>
2009-06-28 13:39:31 +0000

    joins and bridges padre_muc@conference.jabber.org to local padre multicast swarm
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@5681 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Swarm/jabber.pl
+++ Padre-Plugin-Swarm/jabber.pl
@@ -9,19 +9,34 @@ use Net::Jabber::Client;
 use JSON::XS;
 use Padre::Swarm::Transport::Multicast;
 
+
+use constant MUC => 'padre_muc@conference.jabber.org';
+use constant MUCPASS => 'szabgab';
+
+my $resource = 'Padre-Swarm-' . $$;
+
 my $padre = Padre::Swarm::Transport::Multicast->new;
-$padre->subscribe_channel( 12000 , 1 );
+$padre->subscribe_channel( 12000 , 1);
 $padre->start;
 
 
 my ($user,$pass) = @ARGV;
 my ($username,$host) = split /@/, $user;
 
-my $xc = Net::Jabber::Client->new();
+my $jid = $user . '/' . $resource;
+
+my $mucjid;
+
+my $xc = Net::Jabber::Client->new(debug=>2);
+my $Debug = new Net::XMPP::Debug();
+$Debug->Init(level=>1,file=>"stdout");
+#$xc->{DEBUG} = $Debug;
+
 $xc->SetCallBacks(
 	onconnect=>\&connected,
 	ondisconnect=> \&disconnected,
 	onauth => \&authenticated,
+	onprocess=>\&checkpadre,
 	);
 	
 $xc->SetMessageCallBacks(
@@ -32,18 +47,24 @@ $xc->SetMessageCallBacks(
 ); 
 
 
+
+warn "execute";
 $xc->Execute(
 	hostname=>$host,
         username=>$username,
         password=>$pass,
-        resource=>"Padre-Swarm-$$",
-        mode => 'nonblock',
+        resource=>$resource,
+	processtimeout=>0.5,
 );
-#
+warn "execute completed";
 
-while (defined $xc->Process(5) ) {
-	warn "loop";
-}
+
+#$xc->Connect( hostname => $host );
+#$xc->AuthSend( username => $username, password=>$pass,
+#	resource => 'padre-swarm-$$',
+#);
+
+
 
 
 sub connected {
@@ -51,31 +72,55 @@ sub connected {
 }
 
 sub chat {
-	my ($jid,$msg) = @_;
-	warn "CHAT handed $jid ";
-	warn $msg->GetBody;
+	my ($id,$msg) = @_;
 	
-	
-
-warn "Tell padre " ,
+	return if (
+		$msg->GetSubject eq $jid );
+		
+warn "Tell padre from $jid ", $msg->GetSubject;
 	$padre->tell_channel( 12000 ,
 		JSON::XS::encode_json(
 			{ user=>$msg->GetFrom, 
 			message=>$msg->GetBody,
-			subject=>$msg->GetSubject,
-			timestamp=>$msg->GetTimeStamp}
+			subject=>$jid,
+			thread=>$msg->GetThread,
+			timestamp=>$msg->GetTimeStamp,
+			}
 		)
 	)
 }
 
+sub checkpadre {
+	if (my @ready = $padre->poll(0)) {
+		my ($port,$client,$payload) = $padre->receive_from( 12000 );
+		my $message = JSON::XS::decode_json( $payload );
+
+			warn Dumper $message;
+		# Dont loop :)
+		return if ( exists $message->{subject}
+			&& $message->{subject} eq $jid );
+                
+		my $jid = $xc->MessageSend(
+			to=> MUC ,
+			from => $mucjid,
+                        subject=>$jid ,
+                        thread=>$jid,
+                        body => $message->{message},
+			type => 'groupchat',
+                );
+                warn "Relayed PADRE chat as $jid";
+	}
+	
+}
 
 sub authenticated {
 	warn 'authenticated';
 	warn 'Joining MUC' ,
-	$xc->MUCJoin(
-		room => 'padre_muc',
-		password => 'szabgab',
-		server => 'conference.jabber.org',
+	my ($room,$server) = split /@/ , MUC;;
+	$mucjid = $xc->MUCJoin(
+		room => $room,
+		password => MUCPASS,
+		server => $server,
 		nick => 'padre-swarm-xmpp-test',
 	);
   
