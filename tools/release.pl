dd3391b893f5550e6f1e93c9d75a7070254d9e1b
Ryan Niebur <rsn@cpan.org>
2009-06-29 01:10:33 +0000

    use Env::Sanctify instead of "local $ENV{DISPLAY} = undef" to unset DISPLAY, it works better (setting it to undef still leaves it in the environment, just set to nothing. the correct way would normally be "delete $ENV{DISPLAY}"). and ran perltidy.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@5702 18eddd70-d622-4293-bd05-fe65668e8bde

--- tools/release.pl
+++ tools/release.pl
@@ -5,45 +5,48 @@ use warnings;
 # check if there are versions in every module and if they are in the same
 # allow updating version numbers to one specific version.
 
+use Env::Sanctify;
 use autodie qw(:default system);
+
 # needs IPC::System::Simple
 
 use Cwd            ();
 use File::Basename ();
-use File::Copy     qw(copy);
-use File::Find     qw(find);
-use File::Slurp    qw(read_file write_file);
-use File::Temp     ();
+use File::Copy qw(copy);
+use File::Find qw(find);
+use File::Slurp qw(read_file write_file);
+use File::Temp ();
 use FindBin;
 
 use lib "$FindBin::Bin/../Padre";
 use privlib::Tools;
 
-my $SVN     = "http://svn.perlide.org/padre";
-my $TAGS    = "http://svn.perlide.org/padre/tags";
-my $error   = 0;
+my $SVN   = "http://svn.perlide.org/padre";
+my $TAGS  = "http://svn.perlide.org/padre/tags";
+my $error = 0;
 
-my ($rev, $version, $tag) = @ARGV;
+my ( $rev, $version, $tag ) = @ARGV;
 die "Usage: $0 REV VERSION [--tag]\n"
-	if not $version or $version !~ /^\d\.\d\.?\d$/ or $rev !~ /^r?\d+$/;
+	if not $version
+		or $version !~ /^\d\.\d\.?\d$/
+		or $rev !~ /^r?\d+$/;
 
 my $start_dir = Cwd::cwd();
 
-my @svn_info = ($^O ne 'MSWin32') ? qx{LC_ALL=C svn info} : qx{svn info};
+my @svn_info = ( $^O ne 'MSWin32' ) ? qx{LC_ALL=C svn info} : qx{svn info};
 my ($URL) = grep {/^URL:\s*/} @svn_info;
 die "no url" if not $URL;
 chomp $URL;
 $URL =~ s/^URL:\s*//;
-
 
 my $name;
 my $ver;
 
 # URL can be .../trunk/Name     or ../trunk/Name-More-Name
 # or    .../branches/Name or .../barnches/Name-0.12  or the others
-if ($URL =~ m{$SVN/(trunk|branches)/([^/]+)$}) {
+if ( $URL =~ m{$SVN/(trunk|branches)/([^/]+)$} ) {
 	$name = $2;
-	if ($name =~ /^([\w-]+)-(\d+\.\d+(_\d+)?)$/) {
+	if ( $name =~ /^([\w-]+)-(\d+\.\d+(_\d+)?)$/ ) {
 		$name = $1;
 		$ver  = $2;
 	}
@@ -54,7 +57,7 @@ die "No name" if not $name;
 
 print "name: $name\n";
 print "ver: $ver\n" if $ver;
-if ($ver and $ver ne $version) {
+if ( $ver and $ver ne $version ) {
 	die "Invalid version $ver - $version\n";
 }
 
@@ -66,11 +69,11 @@ _system("svn export --quiet -r$rev $URL src");
 chdir 'src';
 
 my $locale_path;
-if (-d 'share/locale') {
+if ( -d 'share/locale' ) {
 	$locale_path = Cwd::cwd();
 } else {
-	(my $path = $name) =~ s{-}{/}g;
-	if (-d "lib/$path/share/locale") {
+	( my $path = $name ) =~ s{-}{/}g;
+	if ( -d "lib/$path/share/locale" ) {
 		$locale_path = Cwd::cwd() . "/lib/$path";
 	}
 }
@@ -81,14 +84,14 @@ if ($locale_path) {
 }
 
 #print "Setting VERSION $version\n";
-find(\&check_version, 'lib');
+find( \&check_version, 'lib' );
 die if $error;
 
-my $make         = $^O eq 'freebsd' ? 'HARNESS_DEBUG=1 gmake' : 'make';
-my $makefile_pl  = "Makefile.PL";
-if(-f "Build.PL") {
+my $make = $^O eq 'freebsd' ? 'HARNESS_DEBUG=1 gmake' : 'make';
+my $makefile_pl = "Makefile.PL";
+if ( -f "Build.PL" ) {
 	$makefile_pl = "Build.PL";
-	$make = ($^O ne 'MSWin32') ? "./Build" : "Build.bat";
+	$make = ( $^O ne 'MSWin32' ) ? "./Build" : "Build.bat";
 }
 _system("$^X $makefile_pl");
 _system("$make");
@@ -96,35 +99,35 @@ _system("$make manifest");
 _system("$make test");
 _system("$make disttest");
 
-if ($^O ne 'MSWin32') {
+if ( $^O ne 'MSWin32' ) {
 	print "Turn off DISPLAY\n";
-	local $ENV{DISPLAY} = undef;
+	my $sanctify = Env::Sanctify->sanctify( sanctify => ['DISPLAY'] );
 	_system("$make disttest");
+	$sanctify->restore();
 }
 
 _system("$make dist");
-copy("$name-$version.tar.gz", $start_dir) or die $!;
+copy( "$name-$version.tar.gz", $start_dir ) or die $!;
 if ($tag) {
 	_system("svn cp -r$rev $URL $TAGS/$name-$version -m'tag $name-$version'");
 }
 chdir $start_dir;
 
-
 sub check_version {
-    return if $File::Find::name =~ /\.svn/;
-    return if $_ !~ /\.pm/;
-    my @data = read_file($_);
-    if (my ($line) = grep {$_ =~ /^our \$VERSION\s*=\s*'\d+\.\d\.?\d';/ } @data ) {
-		if ($line !~ /^(our \$VERSION\s*=\s*)'$version';/ ) {
+	return if $File::Find::name =~ /\.svn/;
+	return if $_ !~ /\.pm/;
+	my @data = read_file($_);
+	if ( my ($line) = grep { $_ =~ /^our \$VERSION\s*=\s*'\d+\.\d\.?\d';/ } @data ) {
+		if ( $line !~ /^(our \$VERSION\s*=\s*)'$version';/ ) {
 			chomp $line;
 			warn "Invalid VERSION in $File::Find::name  ($line)\n";
 			$error++;
 		}
-    } else {
-       warn "No VERSION in $File::Find::name\n";
-       $error++;
-    }
-    return;
+	} else {
+		warn "No VERSION in $File::Find::name\n";
+		$error++;
+	}
+	return;
 }
 
 sub _system {
