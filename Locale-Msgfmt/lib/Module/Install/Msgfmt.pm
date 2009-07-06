770f9b1f81299081a4689fe62a89ef44fad3020b
Ryan Niebur <rsn@cpan.org>
2009-07-06 04:45:03 +0000

    - add Module::Install::Msgfmt. just replace install_share with         install_share_with_mofiles in your Makefile.PL.       - add remove option to remove the source file(s) and all .pot         files if a directory is being processed       - add verbose option to print the files that were converted
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@5853 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ Locale-Msgfmt/lib/Module/Install/Msgfmt.pm
@@ -0,0 +1,48 @@
+package Module::Install::Msgfmt;
+
+use strict;
+use File::Spec;
+use Module::Install::Base ();
+use Module::Install::Share;
+
+use vars qw{$VERSION @ISA $ISCORE};
+$VERSION = '0.01';
+@ISA     = 'Module::Install::Base';
+
+sub install_share_with_mofiles {
+  my @orig = (@_);
+  my $self = shift;
+  my $class     = ref($self);
+  my $inc_class = join('::', @{$self->_top}{qw(prefix name)});
+  my $dir  = @_ ? pop   : 'share';
+  my $type = @_ ? shift : 'dist';
+  my $module = @_ ? shift : '';
+  $self->build_requires( 'Locale::Msgfmt' => '0.09' );
+  install_share(@orig);
+  my $distname = "";
+  if ( $type eq 'dist' ) {
+    $distname = $self->name;
+  } else {
+    $distname = Module::Install::_CLASS($module);
+    $distname =~ s/::/-/g;
+  }
+  my $path = File::Spec->catfile('auto', 'share', $type, $distname);
+  $self->postamble(<<"END_MAKEFILE");
+config ::
+\t\$(NOECHO) \$(PERL) "-M$inc_class" -e "do_msgfmt(q(\$(INST_LIB)), q($path))"
+
+END_MAKEFILE
+}
+
+# blib/lib/auto/share/dist/Padre/locale/he.po
+sub do_msgfmt {
+  my $self = shift;
+  my $lib = shift;
+  my $sharepath = shift;
+  my $fullpath = File::Spec->catfile($lib, $sharepath, 'locale');
+  if(! -d $fullpath) {
+    die("$fullpath isn't a directory");
+  }
+  require Locale::Msgfmt;
+  Locale::Msgfmt::msgfmt({in => $fullpath, verbose => 1, remove => 1});
+}
