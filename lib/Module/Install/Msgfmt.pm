2cc41533247ea273d5a6a872564d1932763346b4
Ryan Niebur <rsn@cpan.org>
2009-07-06 20:56:30 +0000

    preparing to upload
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Locale-Msgfmt@5885 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Module/Install/Msgfmt.pm
+++ lib/Module/Install/Msgfmt.pm
@@ -5,7 +5,7 @@ use File::Spec;
 use Module::Install::Base ();
 use Module::Install::Share;
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 our @ISA     = 'Module::Install::Base';
 
 sub install_share_with_mofiles {
