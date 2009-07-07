d9e6dae7aa618a9adec3049f5f7df232ee469dcf
Ryan Niebur <rsn@cpan.org>
2009-07-07 05:56:48 +0000

    update version number
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Locale-Msgfmt@5892 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Locale/Msgfmt/Utils.pm
+++ lib/Locale/Msgfmt/Utils.pm
@@ -3,7 +3,7 @@ package Locale::Msgfmt::Utils;
 use strict;
 use warnings;
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 sub character {
 	return map { pack "N*", $_ } @_;
--- lib/Locale/Msgfmt/po.pm
+++ lib/Locale/Msgfmt/po.pm
@@ -5,7 +5,7 @@ use Locale::Msgfmt::Utils;
 use strict;
 use warnings;
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 sub new {
 	my $class = shift;
