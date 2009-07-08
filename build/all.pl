9a7c63b9c9555d916faf4492c702d689904368f5
Gabor Szabo <szabgab@gmail.com>
2009-07-08 20:14:51 +0000

    move content of developers.html page to individual files and generate the page
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/branches/template_site@5944 18eddd70-d622-4293-bd05-fe65668e8bde

--- build/all.pl
+++ build/all.pl
@@ -7,6 +7,7 @@ use File::Copy qw(copy);
 use File::Find::Rule;
 use File::Basename qw(dirname basename);
 use YAML::Tiny qw(LoadFile);
+use Data::Dumper qw(Dumper);
 
 my ($source_dir, $output_dir) = @ARGV;
 die "No output dir  Usage: $0 SOURCE_DIR OUTPUT_DIR\n" if not $output_dir or not -d $output_dir;
@@ -31,6 +32,9 @@ my $config = {
 my $tt = Template->new($config);
 my $stash = LoadFile("$source_dir/data/stash.yml");
 
+$stash->{developers} = read_developers($stash->{developers});
+#print Dumper $stash->{developers};
+
 # for now - only a flat directory processed w/ template.
 my $pages_dir = "$source_dir/tt/pages";
 my $page_handle;
@@ -44,4 +48,37 @@ while ( my $file = readdir( $page_handle ) ) {
 	$tt->process($template , $stash, $file )
 		|| die $tt->error();
 }
+
+
+# TODO: add some error checking and data validation (correct sections? correct fields ?)
+sub read_developers {
+	my $list = shift;
+
+	my @developers;
+	foreach my $f (@$list) {
+		my $file = "$source_dir/data/developers/$f";
+		open my $fh, '<', $file or die "Could not open ($file) $!";
+		my $section;
+		my %data;
+		while (my $line = <$fh>) {
+			if ($line =~ /^\[([^\]]+)\]/) {
+				$section = $1;
+				next;
+			}
+			next if not $section;
+			if ($section eq 'data') {
+				if ($line =~ /\S/) {
+					chomp $line;
+					my ($k, $v) = split /=/, $line, 2;
+					$data{$k} = $v;
+				}
+			} else {
+				$data{$section} .= $line;
+			}
+		}
+		push @developers, \%data;
+	}
+	return \@developers;
+}
+
 
