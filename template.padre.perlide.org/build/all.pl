a60f32190ddac03502f9404bd0f5a3ddaa08ca75
Gabor Szabo <szabgab@gmail.com>
2009-07-09 14:39:28 +0000

    move branch of template site to trunk
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@5987 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ template.padre.perlide.org/build/all.pl
@@ -0,0 +1,88 @@
+#!perl
+use strict;
+use warnings;
+use Template;
+use File::Spec;
+use File::Copy qw(copy);
+use File::Find::Rule;
+use File::Basename qw(dirname basename);
+use YAML::Tiny qw(LoadFile);
+use Data::Dumper qw(Dumper);
+
+my ($source_dir, $output_dir) = @ARGV;
+die "No output dir  Usage: $0 SOURCE_DIR OUTPUT_DIR\n" if not $output_dir or not -d $output_dir;
+
+# copy static files
+for my $file (File::Find::Rule->relative->file()->in("$source_dir/documentroot")) {
+	next if $file =~ m{.svn};
+	my $dir = "$output_dir/" . dirname($file);
+	mkdir ($dir);
+	my $to = "$dir/" . basename($file);
+	copy("$source_dir/documentroot/$file", $to);
+	chmod 0644, $to;
+}
+
+my $config = {
+		INCLUDE_PATH => "$source_dir/tt",  # or list ref
+		POST_CHOMP   => 1,               # cleanup whitespace 
+		EVAL_PERL    => 0,               # evaluate Perl code blocks
+		OUTPUT_PATH   => $output_dir,
+};
+
+my $tt = Template->new($config);
+my $stash = LoadFile("$source_dir/data/stash.yml");
+
+$stash->{developers}  = read_people($stash->{developers},  'developers');
+$stash->{translators} = read_people($stash->{translators}, 'translators');
+#print Dumper $stash->{developers};
+#print Dumper $stash->{translators};
+
+# for now - only a flat directory processed w/ template.
+my $pages_dir = "$source_dir/tt/pages";
+my $page_handle;
+opendir( $page_handle, $pages_dir) 
+	|| die "Failed to opendir '$pages_dir' , $!";
+while ( my $file = readdir( $page_handle ) ) {
+	next unless $file =~ /\.(html)$/; 
+	
+	my $template = File::Spec->catfile( 'pages', $file );
+	# OUTPUT_PATH is appended to $file by TT
+	$tt->process($template , $stash, $file )
+		|| die $tt->error();
+}
+
+
+# TODO: add some error checking and data validation (correct sections? correct fields ?)
+sub read_people {
+	my $list = shift;
+    my $dir  = shift;
+
+	my @developers;
+	foreach my $f (@$list) {
+		my $file = "$source_dir/data/$dir/$f.ini";
+		open my $fh, '<', $file or die "Could not open ($file) $!";
+		my $section;
+		my %data;
+		$data{nickname} = $f;
+		while (my $line = <$fh>) {
+			if ($line =~ /^\[([^\]]+)\]/) {
+				$section = $1;
+				next;
+			}
+			next if not $section;
+			if ($section eq 'data') {
+				if ($line =~ /\S/) {
+					chomp $line;
+					my ($k, $v) = split /=/, $line, 2;
+					$data{$k} = $v;
+				}
+			} else {
+				$data{$section} .= $line;
+			}
+		}
+		push @developers, \%data;
+	}
+	return \@developers;
+}
+
+
---
+++ template.padre.perlide.org/documentroot/static/irc.js
@@ -0,0 +1,128 @@
+var channels = {
+   general:      {title:"General Perl Help",             host:"irc.freenode.net", channel:"perl"},
+   win32:        {title:"Win32 specific",                host:"irc.perl.org",     channel:"win32"},
+   padre:        {title:"Padre, the Perl IDE",           host:"irc.perl.org",     channel:"padre"},
+   catalyst:     {title:"Catalyst",                      host:"irc.perl.org",     channel:"catalyst"},
+   dbix_class:   {title:"DBIx::Class",                   host:"irc.perl.org",     channel:"dbix-class"},
+   email:        {title:"Perl Email Project",            host:"irc.perl.org",     channel:"email"},
+   parrot:       {title:"Parrot",                        host:"irc.perl.org",     channel:"parrot"},
+   perl_qa:      {title:"Perl Quality Assurance",        host:"irc.perl.org",     channel:"perl-qa"},
+   locale_fr:    {title:"Perl in French",                host:"irc.perl.org",     channel:"perlfr"},
+   locale_it:    {title:"Perl in Italian",               host:"irc.freenode.net", channel:"perl.it"},
+   locale_jp:    {title:"Perl in Japanese",              host:"irc.freenode.net", channel:"jpa-perl"},
+   locale_kr:    {title:"Perl in Korean",                host:"irc.freenode.net", channel:"perl-kr"},
+   locale_pl:    {title:"Perl in Polish",                host:"irc.perl.org",     channel:"perl.pl"},
+   locale_pt_br: {title:"Perl in Portuguese",            host:"irc.freenode.net", channel:"perl.br"},
+   locale_zh_tw: {title:"Perl in Taiwan (Traditional Chinese)", host:"irc.freenode.net", channel:"perl.tw"},
+   poe:          {title:"POE",                           host:"irc.perl.org",     channel:"poe"},
+   rt:           {title:"RT",                            host:"irc.perl.org",     channel:"rt"},
+   perl_help:    {title:"Perl Help",                     host:"irc.perl.org",     channel:"perl-help"},
+   wxperl:       {title:"Wx Perl",                       host:"irc.perl.org",     channel:"wxperl"},
+   perl6:        {title:"Perl 6 development",            host:"irc.freenode.net", channel:"perl6"},
+};
+var default_channel = "general";
+
+function get_values() {
+    // getting the parameters from the URL
+    var loc = window.location.toString().indexOf('?');
+    if (loc > 0) {
+        var val = window.location.toString().substr(loc + 1);
+        var pairs = val.split("&");
+        var data = Array;
+        for (var i = 0; i < pairs.length; i++) {
+	    var pair = pairs[i].split("=");
+	    data[pair[0]] = pair[1];
+        }
+        return data;
+    } else {
+        return [];
+    }
+}
+
+function on_click() {
+
+    var name;
+    var selected_index = document.forms.n.channel.selectedIndex;
+    name = document.forms.n.channel[selected_index].value;
+
+    setCookie( "nickname" , document.forms.n.nickname.value , 7 );
+    var url;
+    if (channels[name].host == 'irc.freenode.net') {
+	url =  'http://webchat.freenode.net/?';
+	url += 'channels=' + channels[name].channel;
+	url += '&nick=' + document.forms.n.nickname.value;
+    } else {
+	url = 'http://widget.mibbit.com/?autoConnect=true';
+	url += '&server=' + channels[name].host;
+	url += '&channel=%23' + channels[name].channel;
+	url += '&nick=' + document.forms.n.nickname.value;
+    }
+    //alert(url);
+    window.location = url;
+}
+
+function setup_page() {
+    var values = get_values();
+    if (values["nickname"]) {
+	document.forms.n.nickname.value = values["nickname"];
+	setCookie("nickname", values["nickname"]);
+    } else if (getCookie("nickname")) {
+	    document.forms.n.nickname.value = getCookie("nickname");
+    } else {
+	var nick = Math.floor(Math.random()*10000);
+	document.forms.n.nickname.value = "user_" + nick;
+    }
+    var ch = default_channel;
+    if (values["channel"]) {
+	if (channels[ values["channel"] ]) {
+	    ch = values["channel"];
+	}
+    }
+
+    var html = '<select name="channel">';
+    for (var i in channels) {
+	
+	// '<tr><td><input type="radio" name="channel" value="' + i + '"';
+	html    += '<option value="' + i + '">'  + channels[i].title + '</option>';
+	
+	
+    }
+    html    += '</select>';
+    document.write(html);
+}
+
+
+
+function getCookie(c_name)
+{
+if (document.cookie.length>0)
+  {
+  c_start=document.cookie.indexOf(c_name + "=");
+  if (c_start!=-1)
+    {
+    c_start=c_start + c_name.length+1;
+    c_end=document.cookie.indexOf(";",c_start);
+    if (c_end==-1) c_end=document.cookie.length;
+    return unescape(document.cookie.substring(c_start,c_end));
+    }
+  }
+return "";
+}
+
+function setCookie(c_name,value,expiredays)
+{
+var exdate=new Date();
+exdate.setDate(exdate.getDate()+expiredays);
+document.cookie=c_name+ "=" +escape(value)+
+((expiredays==null) ? "" : ";expires="+exdate.toGMTString());
+}
+
+function checkCookie()
+{
+var username = getCookie('nickname');
+if (username!=null && username!="")
+  {
+    document.getElementById("irc_nickname").value = username;
+  }
+
+}
