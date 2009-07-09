26d66299fe6bb7de0e309d51ac646eeb7f3c5ca8
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-07-09 03:12:23 +0000

    [Perl 6] Added Perl 6 Help Generator script (for safe keeping)
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@5954 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ Padre-Plugin-Perl6/parse_p6_help.pl
@@ -0,0 +1,53 @@
+use strict;
+use warnings;
+
+#autoflush STDOUT
+$| = 1;
+
+#
+# Convert html back to text by removing html tags and converting
+# html entities.
+#
+sub _to_text {
+	my $text = shift;
+	$text =~ s/<em>(.+?)<\/em>/$1/g;
+	$text =~ s/<.+?>//g;
+	$text =~ s/&amp;/&/g;
+	$text =~ s/&lt;/</g;
+	$text =~ s/&gt;/>/g;
+	$text =~ s/&quot;/"/g;
+	$text =~ s/&nbsp;/ /g;
+	return $text;
+}
+
+#
+# Converts Perl 6 table index help to POD after getting it from the web.
+#
+sub _perl6_table_index_to_pod {
+	my $url = shift;
+	
+	print "Loading $url\n";
+	require LWP::UserAgent;
+	require HTTP::Request;
+	my $ua = LWP::UserAgent->new;
+	my $req = HTTP::Request->new(GET => $url);
+	my $res = $ua->request($req);
+	if(not $res->is_success) {
+		die $res->status_line, "\n";
+	}
+	
+	my $pod = '';
+	my @lines = split /\n/, $res->content;
+	foreach my $line (@lines) {
+		chomp $line;
+		if($line =~ /<li><strong>(.+?)<\/strong>(.+?)<\/li>/) {
+			my ($item, $item_description)= (_to_text($1), _to_text($2) );
+			$pod .=  "=head1 $item\n\n$item_description\n\n";
+		}
+	}
+	
+	return $pod;
+}
+
+my $pod = _perl6_table_index_to_pod 'http://www.perlfoundation.org/perl6/index.cgi?perl_table_index';
+print $pod;
