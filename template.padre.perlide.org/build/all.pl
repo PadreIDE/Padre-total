a8c17d11cf6592cdb644e849fdba74f1da35f3be
daxim <daxim@svngit.padre.perlide.org>
2009-07-10 13:38:00 +0000

    HTML fixes
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@6015 18eddd70-d622-4293-bd05-fe65668e8bde

--- template.padre.perlide.org/build/all.pl
+++ template.padre.perlide.org/build/all.pl
@@ -8,6 +8,7 @@ use File::Find::Rule;
 use File::Basename qw(dirname basename);
 use YAML::Tiny qw(LoadFile);
 use Data::Dumper qw(Dumper);
+use Text::Unaccent::PurePerl qw(unac_string);
 
 my ($source_dir, $output_dir) = @ARGV;
 die "No output dir  Usage: $0 SOURCE_DIR OUTPUT_DIR\n" if not $output_dir or not -d $output_dir;
@@ -24,9 +25,16 @@ for my $file (File::Find::Rule->relative->file()->in("$source_dir/documentroot")
 
 my $config = {
 		INCLUDE_PATH => "$source_dir/tt",  # or list ref
-		POST_CHOMP   => 1,               # cleanup whitespace 
+		POST_CHOMP   => 1,               # cleanup whitespace
 		EVAL_PERL    => 0,               # evaluate Perl code blocks
 		OUTPUT_PATH   => $output_dir,
+        FILTERS => {
+            'id_attr' => sub {
+                my ($text) = @_;
+                $text =~ s/ /_/g;
+                return unac_string $text;
+              }
+          },
 };
 
 my $tt = Template->new($config);
@@ -40,11 +48,11 @@ $stash->{translators} = read_people($stash->{translators}, 'translators');
 # for now - only a flat directory processed w/ template.
 my $pages_dir = "$source_dir/tt/pages";
 my $page_handle;
-opendir( $page_handle, $pages_dir) 
+opendir( $page_handle, $pages_dir)
 	|| die "Failed to opendir '$pages_dir' , $!";
 while ( my $file = readdir( $page_handle ) ) {
-	next unless $file =~ /\.(html)$/; 
-	
+	next unless $file =~ /\.(html)$/;
+
 	my $template = File::Spec->catfile( 'pages', $file );
 	# OUTPUT_PATH is appended to $file by TT
 	$tt->process($template , $stash, $file )
@@ -60,7 +68,7 @@ sub read_people {
 	my @developers;
 	foreach my $f (@$list) {
 		my $file = "$source_dir/data/$dir/$f.ini";
-		open my $fh, '<', $file or die "Could not open ($file) $!";
+		open my $fh, '<:utf8', $file or die "Could not open ($file) $!";
 		my $section;
 		my %data;
 		$data{nickname} = $f;
