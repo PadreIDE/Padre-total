287730a08f142be467df9b761fc7b5114931827f
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-07-16 09:10:56 +0000

    [Perl 6] Bumped version to 0.51 for eminent CPAN release
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Padre-Plugin-Perl6@6117 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Plugin/Perl6.pm
+++ lib/Padre/Plugin/Perl6.pm
@@ -8,7 +8,7 @@ use Padre::Wx   ();
 use base 'Padre::Plugin';
 
 # exports and version
-our $VERSION   = '0.50';
+our $VERSION   = '0.51';
 our @EXPORT_OK = qw(plugin_config);
 
 # constants for html exporting
--- lib/Padre/Plugin/Perl6/Perl6OutlineTask.pm
+++ lib/Padre/Plugin/Perl6/Perl6OutlineTask.pm
@@ -3,7 +3,7 @@ package Padre::Plugin::Perl6::Perl6OutlineTask;
 use strict;
 use warnings;
 
-our $VERSION = '0.50';
+our $VERSION = '0.51';
 
 use base 'Padre::Task::Outline';
 
