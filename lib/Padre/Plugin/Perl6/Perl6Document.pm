c9902d3f90c85afe2d27ebe0e93b3df5ec67f6d3
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-07-18 06:43:04 +0000

    [Perl 6] Copyright nightmare fixes on all files
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Padre-Plugin-Perl6@6181 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Plugin/Perl6/Perl6Document.pm
+++ lib/Padre/Plugin/Perl6/Perl6Document.pm
@@ -901,7 +901,18 @@ sub get_outline {
 
 1;
 
-# Copyright 2008 Gabor Szabo.
-# LICENSE
-# This program is free software; you can redistribute it and/or
-# modify it under the same terms as Perl 5 itself.
+__END__
+
+=head1 AUTHOR
+
+Ahmad M. Zawawi, C<< <ahmad.zawawi at gmail.com> >>
+
+Gabor Szabo L<http://szabgab.com/>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2008-2009 Ahmad M. Zawawi, C<< <ahmad.zawawi at gmail.com> >> and 
+Gabor Szabo. L<http://szabgab.com/>
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl 5 itself.
