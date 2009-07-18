5b3631a821b7862298e1b84a6fbebd8274197dae
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-07-18 04:57:44 +0000

    [Perl6-Refactor] Initial module structure initialized... Nothing special yet.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@6169 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ Perl6-Refactor/lib/Perl6/Refactor.pm
@@ -0,0 +1,106 @@
+package Perl6::Refactor;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Perl6::Refactor - The great new Perl6::Refactor!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Perl6::Refactor;
+
+    my $foo = Perl6::Refactor->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Ahmad M. Zawawi, C<< <ahmad.zawawi at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-perl6-refactor at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Perl6-Refactor>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Perl6::Refactor
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Perl6-Refactor>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Perl6-Refactor>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Perl6-Refactor>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Perl6-Refactor/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Ahmad M. Zawawi, all rights reserved.
+
+This program is released under the following license: artistic_2
+
+
+=cut
+
+1; # End of Perl6::Refactor
