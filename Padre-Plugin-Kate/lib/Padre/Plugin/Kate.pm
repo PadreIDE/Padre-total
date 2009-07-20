80ae42cf4e9eee857fb5b82b59c524c86135d128
Gabor Szabo <szabgab@gmail.com>
2009-07-20 20:40:50 +0000

    prepare the Kate integration with the new highlighting configuration
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@6332 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Kate/lib/Padre/Plugin/Kate.pm
+++ Padre-Plugin-Kate/lib/Padre/Plugin/Kate.pm
@@ -6,6 +6,7 @@ use 5.008;
 our $VERSION = '0.01';
 
 use Padre::Wx ();
+use Padre::Current;
 
 use base 'Padre::Plugin';
 
@@ -35,6 +36,7 @@ sub plugin_name {
 	'Kate';
 }
 
+
 sub menu_plugins_simple {
 	my $self = shift;
 	return $self->plugin_name => [
@@ -42,9 +44,42 @@ sub menu_plugins_simple {
 	];
 }
 
-sub registered_documents {
-	#'text/x-abc' => 'Padre::Plugin::Kate::ABC', 
-	'application/x-php' => 'Padre::Plugin::Kate::PHP',
+sub provided_highlighters { 
+	return (
+		['Padre::Plugin::Kate', 'Kate', 'Using Syntax::Highlight::Engine::Kate based on the Kate editor'],
+	);
+}
+
+sub highlighting_mime_types {
+	return (
+		'Padre::Plugin::Kate' => ['application/x-php'],
+	);
+}
+
+# TODO shall we create a module for each mime-type and register it as a highlighter
+# or is our dispatching ok?
+# Shall we create a module called Pudre::Plugin::Kate::Colorize that will do the dispatching ?
+my %d = (
+	'application/x-php' => 'PHP',
+);
+sub colorize {
+	my $self = shift;
+	my $doc = Padre::Current->document;
+	my $mime_type = $doc->get_mimetype;
+	
+	if ( $d{$mime_type} ) {
+		my $module = 'Padre::Plugin::Kate::' . $d{$mime_type};
+print "M '$module'\n";
+		eval "use $module";
+		if ($@) {
+			warn $@;
+			return;
+		}
+		$module->colorize(@_);
+	} else {
+		warn("Invalid mime-type ($mime_type) passed to the Kate highlighter");
+	}
+	return;
 }
 
 sub about {
