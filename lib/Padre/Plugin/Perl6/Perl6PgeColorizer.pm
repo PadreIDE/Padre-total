79994c4d018eb97ce7b289a1e7f7911a23567594
Gabor Szabo <szabgab@gmail.com>
2009-07-20 16:42:06 +0000

    functions that list syntax highlighters supported by the plugin
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Padre-Plugin-Perl6@6316 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ lib/Padre/Plugin/Perl6/Perl6PgeColorizer.pm
@@ -0,0 +1,26 @@
+package Padre::Plugin::Perl6::Perl6PgeColorizer;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.53';
+
+sub colorize {
+}
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Ahmad M. Zawawi C<< <ahmad.zawawi at gmail.com> >>
+
+Gabor Szabo L<http://szabgab.com/>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2008-2009 Padre Developers as in Perl6.pm
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl 5 itself.
