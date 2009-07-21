53175ef95a5007f0417fc4d57fc73f560a80567a
Gabor Szabo <szabgab@gmail.com>
2009-07-21 13:35:21 +0000

    [Kate] remove the PHP special file and include the code in the main module
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@6368 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Kate/lib/Padre/Plugin/Kate.pm
+++ Padre-Plugin-Kate/lib/Padre/Plugin/Kate.pm
@@ -59,28 +59,71 @@ sub highlighting_mime_types {
 # TODO shall we create a module for each mime-type and register it as a highlighter
 # or is our dispatching ok?
 # Shall we create a module called Pudre::Plugin::Kate::Colorize that will do the dispatching ?
+# now this is the mapping to the Kate highlighter engine
 my %d = (
-	'application/x-php' => 'PHP',
+	'application/x-php' => 'Perl',
 );
+use Syntax::Highlight::Engine::Kate::All;
+use Syntax::Highlight::Engine::Kate;
+
 sub colorize {
-	my $self = shift;
+	my ( $self, $first ) = @_;
+
 	my $doc = Padre::Current->document;
 	my $mime_type = $doc->get_mimetype;
-	
-	if ( $d{$mime_type} ) {
-		my $module = 'Padre::Plugin::Kate::' . $d{$mime_type};
-#print "M '$module'\n";
-		eval "use $module";
-		if ($@) {
-			warn $@;
-			return;
-		}
-		$module->colorize(@_);
-	} else {
+	if ( not $d{$mime_type} ) {
 		warn("Invalid mime-type ($mime_type) passed to the Kate highlighter");
+		return;
+	}
+
+	# TODO we might need not remove all the color, just from a certain section
+	# TODO reuse the $first passed to the method
+	$doc->remove_color;
+
+	my $editor = $doc->editor;
+	my $text   = $doc->text_get;
+
+	my $kate = Syntax::Highlight::Engine::Kate->new(
+		language => $d{$mime_type},
+	);
+
+	# returns a list of pairs: string, type
+	my @tokens = $kate->highlight($text);
+	my %COLOR = (
+		Normal => 0,
+		Operator => 1,
+		String => 2,
+		Function => 3,
+		DataType => 4,
+		Variable => 5,
+		Float => 6,
+		Keyword => 7,
+		Char => 8,
+		Comment => 9,
+	);
+
+	my $start = 0;
+	my $end   = 0;
+	while (@tokens) {
+		my $string = shift @tokens;
+		my $type   = shift @tokens;
+		#$type ||= 'Normal';
+		#print "'$string'    '$type'\n";
+		my $color = $COLOR{$type};
+		if (not defined $color) {
+			warn "Missing color definition for type '$type'\n";
+			$color = 0;
+		}
+		my $length = length($string);
+		#$end += $length;
+		$editor->StartStyling( $start, $color );
+		$editor->SetStyling( $length, $color );
+		#$start = $end;
+		$start += $length;
 	}
 	return;
 }
+
 
 sub about {
 	my ($main) = @_;
