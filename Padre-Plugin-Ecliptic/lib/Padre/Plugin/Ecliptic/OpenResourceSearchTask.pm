481e1caa4cef77834e35e06c863570ea1ec40fa1
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-07-29 02:38:49 +0000

    [Ecliptic] More cosmetic changes to search task
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@6748 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Ecliptic/lib/Padre/Plugin/Ecliptic/OpenResourceSearchTask.pm
+++ Padre-Plugin-Ecliptic/lib/Padre/Plugin/Ecliptic/OpenResourceSearchTask.pm
@@ -9,47 +9,41 @@ use Padre::Constant ();
 our $VERSION        = '0.15';
 our $thread_running = 0;
 
+#
 # This is run in the main thread before being handed
 # off to a worker (background) thread. The Wx GUI can be
 # polled for information here.
-# If you don't need it, just inherit this default no-op.
+#
 sub prepare {
 	my $self = shift;
 
-	# it is not running yet.
-	$self->{broken} = 0;
-
 	# assign a place in the work queue
 	if ($thread_running) {
-
 		# single thread instance at a time please. aborting...
-		$self->{broken} = 1;
 		return "break";
 	}
 	$thread_running = 1;
 	return 1;
 }
 
-sub is_broken {
+#
+# Task thread subroutine
+#
+sub run {
 	my $self = shift;
-	return $self->{broken};
+
+	return 1;
 }
 
+#
 # This is run in the main thread after the task is done.
 # It can update the GUI and do cleanup.
-# You don't have to implement this if you don't need it.
+#
 sub finish {
 	my ($self, $main) = @_;
 
 	# finished here
 	$thread_running = 0;
-
-	return 1;
-}
-
-# Task thread subroutine
-sub run {
-	my $self = shift;
 
 	return 1;
 }
