e03876a26b2256f30f294e4d3271ac0cb0b96090
Adam Kennedy <adamk@cpan.org>
2009-08-01 10:54:03 +0000

    Created the skeleton
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@6824 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ Padre-Plugin-Media/lib/Padre/Plugin/Media.pm
@@ -0,0 +1,79 @@
+package Padre::Plugin::Media;
+
+use 5.008;
+use strict;
+use warnings;
+use Padre::Config ();
+use Padre::Wx     ();
+use Padre::Plugin ();
+
+our $VERSION = '0.25';
+our @ISA     = 'Padre::Plugin';
+
+
+
+
+
+#####################################################################
+# Padre::Plugin Methods
+
+sub padre_interfaces {
+	'Padre::Plugin' => 0.42
+}
+
+sub plugin_name {
+	'Media Plugin (Experimental)';
+}
+
+sub menu_plugins_simple {
+	my $self = shift;
+	return $self->plugin_name => [
+		'About' => sub { $self->show_about },
+		# 'Another Menu Entry' => sub { $self->about },
+		# 'A Sub-Menu...' => [
+		#     'Sub-Menu Entry' => sub { $self->about },
+		# ],
+	];
+}
+
+
+
+
+
+#####################################################################
+# Custom Methods
+
+sub show_about {
+	my $self = shift;
+
+	# Locate this plugin
+	my $path = File::Spec->catfile(
+		Padre::Config->default_dir,
+		qw{ plugins Padre Plugin Media.pm }
+	);
+
+	# Generate the About dialog
+	my $about = Wx::AboutDialogInfo->new;
+	$about->SetName("My Plugin");
+	$about->SetDescription( <<"END_MESSAGE" );
+The philosophy behind Padre is that every Perl programmer
+should be able to easily modify and improve their own editor.
+
+To help you get started, we've provided you with your own plugin.
+
+It is located in your configuration directory at:
+$path
+Open it with with Padre and you'll see an explanation on how to add items.
+END_MESSAGE
+
+	# Show the About dialog
+	Wx::AboutBox( $about );
+
+	return;
+}
+
+1;
+# Copyright 2008 Gabor Szabo.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
