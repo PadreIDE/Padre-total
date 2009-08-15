4a4155cf032f89525c40e3c422bfb79ec5a4fb28
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-08-15 07:43:55 +0000

    [Perl 6] Depends on Padre 0.43 to make use of plugin_directory_share
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@7204 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Perl6/lib/Padre/Plugin/Perl6.pm
+++ Padre-Plugin-Perl6/lib/Padre/Plugin/Perl6.pm
@@ -29,11 +29,6 @@ sub plugin_config {
 	return $config;
 }
 
-# private subroutine to return the current share directory location
-sub _sharedir {
-	return Cwd::realpath( File::Spec->join( File::Basename::dirname(__FILE__), 'Perl6/share' ) );
-}
-
 # Returns the plugin name to Padre
 sub plugin_name {
 	return Wx::gettext("Perl 6");
@@ -41,11 +36,12 @@ sub plugin_name {
 
 # directory where to find the translations
 sub plugin_locale_directory {
-	return File::Spec->catdir( _sharedir(), 'locale' );
+	my $self = shift;
+	return File::Spec->catdir( $self->plugin_directory_share(), 'locale' );
 }
 
 sub padre_interfaces {
-	'Padre::Plugin' => 0.41,;
+	'Padre::Plugin' => 0.43,;
 }
 
 # plugin's real icon object
@@ -60,9 +56,10 @@ sub logo_icon {
 
 # plugin bitmap
 sub plugin_icon {
+	my $self = shift;
 
 	# find resource path
-	my $iconpath = File::Spec->catfile( _sharedir(), 'icons', 'camelia.png' );
+	my $iconpath = File::Spec->catfile( $self->plugin_directory_share(), 'icons', 'camelia.png' );
 
 	# create and return icon
 	return Wx::Bitmap->new( $iconpath, Wx::wxBITMAP_TYPE_PNG );
@@ -346,7 +343,7 @@ sub _create_from_template {
 	$self->main->on_new;
 
 	my $editor = $self->current->editor or return;
-	my $file = File::Spec->catdir( _sharedir(), "templates/$template.$extension" );
+	my $file = File::Spec->catdir( $self->plugin_directory_share(), "templates/$template.$extension" );
 	$editor->insert_from_file($file);
 
 	my $document = $editor->{Document};
@@ -367,7 +364,7 @@ sub show_preferences {
 }
 
 sub show_about {
-	my $main = shift;
+	my $self = shift;
 
 	require Syntax::Highlight::Perl6;
 	require App::Grok;
@@ -385,7 +382,7 @@ sub show_about {
 	$about->SetVersion($VERSION);
 
 	# create and return the camelia icon
-	my $camelia_path = File::Spec->catfile( _sharedir(), 'icons', 'camelia-big.png' );
+	my $camelia_path = File::Spec->catfile( $self->plugin_directory_share(), 'icons', 'camelia-big.png' );
 	my $camelia_bmp = Wx::Bitmap->new( $camelia_path, Wx::wxBITMAP_TYPE_PNG );
 	my $camelia_icon = Wx::Icon->new();
 	$camelia_icon->CopyFromBitmap($camelia_bmp);
