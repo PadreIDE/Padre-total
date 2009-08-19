0e4cfcd653c3983e080906d56623693a4080da02
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-08-19 08:04:11 +0000

    [Perl 6] Fixed Copyright on Perl6HelpProvider.pm
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@7387 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Perl6/lib/Padre/Plugin/Perl6/Perl6HelpProvider.pm
+++ Padre-Plugin-Perl6/lib/Padre/Plugin/Perl6/Perl6HelpProvider.pm
@@ -63,7 +63,15 @@ Padre::Plugin::Perl6::PerlHelpProvider - Perl 6 Help Provider
 
 Perl 6 Help index is built here and rendered.
 
-# Copyright 2008-2009 The Padre development team as listed in Padre.pm.
-# LICENSE
-# This program is free software; you can redistribute it and/or
-# modify it under the same terms as Perl 5 itself.
+=head1 AUTHOR
+
+Ahmad M. Zawawi C<< <ahmad.zawawi at gmail.com> >>
+
+Gabor Szabo L<http://szabgab.com/>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2008-2009 Padre Developers as in Perl6.pm
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl 5 itself.
