9e08cac0620002d612762cf8d706fd9aceedcf09
daxim <daxim@svngit.padre.perlide.org>
2009-08-19 17:10:53 +0000

    replace manual arguments handling with getopt, add documentation
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@7392 18eddd70-d622-4293-bd05-fe65668e8bde

--- template.padre.perlide.org/build/all.pl
+++ template.padre.perlide.org/build/all.pl
@@ -9,25 +9,28 @@ use File::Basename qw(dirname basename);
 use YAML::Tiny qw(LoadFile);
 use Data::Dumper qw(Dumper);
 use Text::Unaccent::PurePerl qw(unac_string);
+use Pod::Usage::CommandLine qw(GetOptions pod2usage);
 
-my ($source_dir, $output_dir) = @ARGV;
-die "No output dir  Usage: $0 SOURCE_DIR OUTPUT_DIR\n" if not $output_dir or not -d $output_dir;
+my %opt;
+GetOptions(\%opt, 's|source-directory=s', 'o|output-directory=s') or pod2usage(2);
+pod2usage(2) unless $opt{o} and $opt{s};
+die "directory $opt{o} does not exist\n" unless -d $opt{o};
 
 # copy static files
-for my $file (File::Find::Rule->relative->file()->in("$source_dir/documentroot")) {
+for my $file (File::Find::Rule->relative->file()->in("$opt{s}/documentroot")) {
 	next if $file =~ m{.svn};
-	my $dir = "$output_dir/" . dirname($file);
+	my $dir = "$opt{o}/" . dirname($file);
 	mkdir ($dir);
 	my $to = "$dir/" . basename($file);
-	copy("$source_dir/documentroot/$file", $to);
+	copy("$opt{s}/documentroot/$file", $to);
 	chmod 0644, $to;
 }
 
 my $config = {
-		INCLUDE_PATH => "$source_dir/tt",  # or list ref
+		INCLUDE_PATH => "$opt{s}/tt",  # or list ref
 		POST_CHOMP   => 1,               # cleanup whitespace
 		EVAL_PERL    => 0,               # evaluate Perl code blocks
-		OUTPUT_PATH   => $output_dir,
+		OUTPUT_PATH   => $opt{o},
         FILTERS => {
             'id_attr' => sub {
                 my ($text) = @_;
@@ -39,7 +42,7 @@ my $config = {
 };
 
 my $tt = Template->new($config);
-my ($stash) = LoadFile("$source_dir/data/stash.yml"); # older version of YAML::Tiny return list ??
+my ($stash) = LoadFile("$opt{s}/data/stash.yml"); # older version of YAML::Tiny return list ??
 
 $stash->{developers}  = read_people($stash->{developers},  'developers');
 $stash->{translators} = read_people($stash->{translators}, 'translators');
@@ -47,7 +50,7 @@ $stash->{translators} = read_people($stash->{translators}, 'translators');
 #print Dumper $stash->{translators};
 
 # for now - only a flat directory processed w/ template.
-my $pages_dir = "$source_dir/tt/pages";
+my $pages_dir = "$opt{s}/tt/pages";
 my $page_handle;
 opendir( $page_handle, $pages_dir)
 	|| die "Failed to opendir '$pages_dir' , $!";
@@ -70,7 +73,7 @@ sub read_people {
 
 	my @developers;
 	foreach my $f (@$list) {
-		my $file = "$source_dir/data/$dir/$f.ini";
+		my $file = "$opt{s}/data/$dir/$f.ini";
 		open my $fh, '<:utf8', $file or die "Could not open ($file) $!";
 		my $section;
 		my %data;
@@ -96,4 +99,56 @@ sub read_people {
 	return \@developers;
 }
 
+__END__
 
+=head1 NAME
+
+all.pl - Build the web site at padre.perlide.org
+
+=head1 SYNOPSIS
+
+    all.pl \
+      -s ~/svn/padre/template.padre.perlide.org \
+      -o ~/public_html/padre.perlide.org
+
+Options:
+
+    --source-directory      base directory where templates etc. are located
+    --output-directory      base directory where built web site files go
+    --help                  brief help message
+    --man                   full documentation
+
+Options can be shortened according to L<Getopt::Long/Case and abbreviations>.
+
+=head1 OPTIONS
+
+=over
+
+=item --source-directory
+
+Mandatory. Specifies the name of the base directory where the web site
+templates, static files and source data are located.
+
+This is the checked out local working copy coming from the SVN path
+L<http://svn.perlide.org/padre/trunk/template.padre.perlide.org>.
+
+=item --output-directory
+
+Mandatory. Specifies the name of the directory where the web site files
+are output after the build.
+
+This directory already needs to exists.
+
+=item --help
+
+Print a brief help message and exits.
+
+=item --man
+
+Prints the manual page and exits.
+
+=back
+
+=head1 DESCRIPTION
+
+C<all.pl> builds the files for the web site at L<http://padre.perlide.org>.
