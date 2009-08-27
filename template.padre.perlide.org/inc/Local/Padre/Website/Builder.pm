d924bd79299b5ac7bf4b84220d21c92b91dd38bf
daxim <daxim@svngit.padre.perlide.org>
2009-08-27 15:12:06 +0000

    modernise Build system, add tests
    * Tests for HTML validity and spelling.
    
    * Template locations in subdirectories are now (not only a flat directory).
    
    * Removed the chmod 0644 of the target directory because it interfered.
      No idea why it was there in the first place.
    
    * Added a stub for an install target.
    
    * Module deps changed (excluding modules in core):
      -File::Find::Rule
      -Pod::Usage::CommandLine
      +Capture::Tiny
      +File::Next
      +File::Which
      +Path::Class
    
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@7619 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ template.padre.perlide.org/inc/Local/Padre/Website/Builder.pm
@@ -0,0 +1,197 @@
+package Local::Padre::Website::Builder;
+use 5.010;
+use utf8;
+use strict;
+use warnings FATAL => 'all';
+use parent 'Module::Build';
+use File::Copy qw(copy);
+use File::Next qw();
+use HTML::TreeBuilder::XPath qw();
+use List::Util qw(reduce);
+use LWP::Simple qw(get);
+use Path::Class qw(dir file);
+use Template qw();
+use Text::Unaccent::PurePerl qw(unac_string);
+use Time::Piece qw(localtime);
+use YAML::Tiny qw(LoadFile);
+use autodie qw(:all copy);
+
+{
+    our @nerfed_methods =    # not applicable for building a website
+      map {"ACTION_$_"}
+      qw(code dist distcheck distclean distdir distmeta distsign disttest docs
+      html manifest manpages pardist ppd ppmdist skipcheck testcover testpod
+      testpodcoverage versioninstall);
+    eval "sub $_ {}" for @nerfed_methods;
+
+    sub known_actions {
+        my ($self) = @_;
+        my %actions;
+        no strict 'refs';
+        foreach my $class ($self->super_classes) {
+            foreach (keys %{$class . '::'}) {
+                next
+                  if $_ ~~ @nerfed_methods;   # so ACTION_help works as intended
+                $actions{$1}++ if /^ACTION_(\w+)/;
+            }
+        }
+        return wantarray ? sort keys %actions : \%actions;
+    }
+}
+
+sub dist_name    {return 'Padre website'}
+sub dist_version {return localtime->ymd}
+
+sub ACTION_build {
+    my ($self) = @_;
+    $self->depends_on(qw(copy_static_files process_templates));
+    say 'You can run ./Build test now.';
+}
+
+sub ACTION_copy_static_files {
+    my ($self) = @_;
+    my $documentroot = dir($self->base_dir, 'documentroot');
+
+    my $iter = File::Next::files({
+            descend_filter => sub {$_ ne '.svn'}
+        },
+        $documentroot
+    );
+
+    while (defined(my $fullpath = $iter->())) {
+        my $target_dir = dir($self->destdir,
+            file($fullpath)->relative($documentroot)->parent);
+        $target_dir->mkpath;
+        copy $fullpath, $target_dir;
+    }
+}
+
+sub ACTION_process_templates {
+    my ($self) = @_;
+
+    my $include_path = dir($self->base_dir, 'tt');
+    my $tt = Template->new({
+            INCLUDE_PATH => $include_path,
+            POST_CHOMP   => 1,                # cleanup whitespace
+            EVAL_PERL    => 0,                # evaluate Perl code blocks
+            OUTPUT_PATH  => $self->destdir,
+            FILTERS      => {
+                'id_attr' => sub {
+                    my ($text)    = @_;
+                    my ($rewrite) = unac_string $text;    # list context pls!
+                    $rewrite =~ s/\s+/_/g;
+                    $rewrite;
+                },
+            },
+        });
+
+    # older version of YAML::Tiny return list ??
+    my ($stash) = LoadFile file($self->base_dir, qw(data stash.yml));
+
+    $stash->{screenshots_xml} = '<div/>';    # FIXME $self->_scrape_screenshots;
+    $stash->{build_date} = $self->dist_version;
+    $stash->{developers}
+      = $self->_read_people($stash->{developers}, 'developers');
+    $stash->{translators}
+      = $self->_read_people($stash->{translators}, 'translators');
+
+    # recursive templates structure now supported
+    {
+        my $templates_dir = dir($include_path, 'pages');
+        my $iter = File::Next::files({
+                file_filter => sub {/\.html \z/msx}
+            },
+            $templates_dir
+        );
+        while (defined(my $template_file = $iter->())) {
+            $tt->process(
+                file($template_file)->relative($include_path)->stringify,
+                $stash,
+                file($template_file)->relative($templates_dir)->stringify,
+                {binmode => ':utf8'},
+            ) or die $tt->error;
+        }
+    }
+}
+
+sub ACTION_install {
+    my ($self) = @_;
+    say './Build install does nothing yet. Fix me!';
+
+    # system qw(rsync -a), $self->destdir, 'padre.perlide.org:';
+    # or something like that.
+}
+
+sub _scrape_screenshots {
+
+    # scrape the wiki page to keep screenshots in sync
+    my $screenshots_xml;
+    unless ($screenshots_xml
+        = get 'http://padre.perlide.org/trac/wiki/Screenshots') {
+        die "could not get the screenshots wiki page\n";
+    }
+    my $tree = HTML::TreeBuilder::XPath->new_from_content($screenshots_xml);
+    my @result_nodes;
+    my %seen_version;
+    for my $node (
+        $tree->findnodes('/html/body/div[3]/div[2]/div/*')->get_nodelist) {
+        next if 'h1' eq $node->tag;    # skip page header
+        if ('h2' eq $node->tag) {
+            my ($version) = [$node->content_list]->[0] =~ /(\d+\.\d+)/;
+            if ($version && !exists $seen_version{$version}) {
+                $node->attr('id', "v$version");    # must start with letter
+                $seen_version{$version} = undef;
+            } else {
+                $node->attr('id', undef);
+            }
+            push @result_nodes, $node;
+            next;
+        }
+        if ('p' eq $node->tag) {
+            if (ref [$node->content_list]->[0]) {    # has children
+                my ($img) = [$node->content_list]->[0]->content_list; # /p/a/img
+                $img->attr('title', undef);
+                $img->attr('alt',   '');
+                push @result_nodes, $img;
+            } else {    # has only text
+                push @result_nodes, $node;    # /p
+            }
+        }
+    }
+    no warnings 'once';    # stupid Perl forgets about $a $b exemption
+    return reduce {$a . $b} map {$_->as_XML} @result_nodes;    # XML string
+}
+
+# TODO: add some error checking and data validation (correct sections? correct fields ?)
+sub _read_people {
+    my ($self, $list, $dir) = @_;
+
+    my @developers;
+    foreach my $f (@$list) {
+        my $file = file($self->base_dir, 'data', $dir, "$f.ini");
+        open my $fh, '<:utf8', $file;
+        my $section;
+        my %data;
+        $data{nickname} = $f;
+        while (my $line = <$fh>) {
+            if ($line =~ /^\[([^\]]+)\]/) {
+                $section = $1;
+                next;
+            }
+            next if not $section;
+            if ($section eq 'data') {
+                if ($line =~ /\S/) {
+                    chomp $line;
+                    my ($k, $v) = split /=/, $line, 2;
+                    $data{$k} = $v;
+                }
+            } else {
+                $data{$section} .= $line;
+            }
+        }
+        push @developers, \%data;
+    }
+    return \@developers;
+}
+
+1;
---
+++ template.padre.perlide.org/t/validate.t
@@ -0,0 +1,43 @@
+#!perl
+use 5.010;
+use utf8;
+use strict;
+use warnings FATAL => 'all';
+use autodie qw(:all);
+use Capture::Tiny qw(capture);
+use File::Next qw();
+use File::Which qw(which);
+use Test::More;
+
+# Skip means sweep bugs under the rug.
+# I want this test to be actually run.
+BAIL_OUT 'xmllint (part of the libxml2 package) not installed.'
+  unless which 'xmllint';
+
+my $destdir;
+{
+    my $runtime_params_file = '_build/runtime_params';
+    my $runtime_params      = do $runtime_params_file;
+    die "Could not load $runtime_params_file. Run Build.PL first.\n"
+      unless $runtime_params;
+    $destdir = $runtime_params->{destdir};
+}
+
+my $iter = File::Next::files({
+        file_filter => sub {/\.html \z/msx},
+        sort_files  => 1,
+    },
+    $destdir
+);
+
+my $file_counter;
+while (defined(my $html_file = $iter->())) {
+    $file_counter++;
+    my (undef, $stderr) = capture {
+        system qw(xmllint --html --noout), $html_file;
+    };
+    ok !$stderr, "$html_file validates";
+    diag $stderr if $stderr;
+}
+
+done_testing($file_counter);
