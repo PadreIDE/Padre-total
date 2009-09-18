4c8b9f12ec223e36115a14ed8951b4ef9e8c36f3
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-09-18 09:50:49 +0000

    [Perl 6] An upgrade task suddenly appears here.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@8030 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ Padre-Plugin-Perl6/lib/Padre/Plugin/Perl6/UpgradeTask.pm
@@ -0,0 +1,65 @@
+package Padre::Plugin::Perl6::UpgradeTask;
+
+use 5.008;
+use strict;
+use warnings;
+use Padre::Task ();
+use Padre::Wx   ();
+
+our $VERSION = '0.46';
+our @ISA     = 'Padre::Task';
+
+# set up a new event type
+our $SAY_HELLO_EVENT : shared = Wx::NewEventType();
+
+sub prepare {
+
+	# Set up the event handler
+	Wx::Event::EVT_COMMAND(
+		Padre->ide->wx->main,
+		-1,
+		$SAY_HELLO_EVENT,
+		\&on_say_hello,
+	);
+
+	return;
+}
+
+# The event handler
+sub on_say_hello {
+	my ( $main, $event ) = @_;
+	@_ = (); # hack to avoid "Scalars leaked"
+
+	# Write a message to the beginning of the document
+	my $editor = $main->current->editor;
+	return if not defined $editor;
+	$editor->InsertText( 0, $event->GetData );
+}
+
+sub run {
+	my $self = shift;
+
+	# post two events for fun
+	$self->post_event( $SAY_HELLO_EVENT, "Hello from thread!\n" );
+	sleep 1;
+	$self->post_event( $SAY_HELLO_EVENT, "Hello again!\n" );
+
+	return 1;
+}
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Ahmad M. Zawawi C<< <ahmad.zawawi at gmail.com> >>
+
+Gabor Szabo L<http://szabgab.com/>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2008-2009 Padre Developers as in Perl6.pm
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl 5 itself.
