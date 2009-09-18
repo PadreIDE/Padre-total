0c6f4aaa8a55cd2cbbb60aec582e9fbef14bf2d6
Sebastian Willing <sewi@cpan.org>
2009-09-18 13:02:31 +0000

    A small script which tests if kill 0,$PID works. Please run it and report back the OS (printed by the script) and the result (success or error message).
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/branches/auto_update_test@8044 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ process_test.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+$|=1;
+
+use strict;
+use warnings;
+
+print "\nPadre test script as preparation for auto-update\n\n".
+      "This script will not do any updates itself, just do some OS tests which don't harm.\n\n";
+
+$SIG{CHLD} = sub { wait; };
+
+my $PID = fork; # Create a process for testing
+if ( ! $PID) {
+ sleep 1;
+ exit;
+}
+
+print "My PID id $$, my child id $PID\n\n";
+
+print "Running on $^O\n".
+      "Child should be there... ";
+if (kill(0,$PID)) {
+ print "is is\n";
+} else {
+ print "no\n";
+ die 'Unable to find child at first attempt';
+}
+
+print "Child should still be there... ";
+if (kill(0,$PID)) {
+ print "is is\n";
+} else {
+ print "no\n";
+ die 'kill0 seems to kill child - bad';
+}
+
+sleep 2;
+print "Child should be gone... ";
+if ( ! kill(0,$PID)) {
+ print "is is\n";
+} else {
+ print "no\n";
+ die 'Child seems to be off but detection failed';
+}
