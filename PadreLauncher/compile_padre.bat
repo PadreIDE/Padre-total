bc5d87405f97db0b18a00e36c22bf35c5372c343
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-09-22 10:29:59 +0000

    [PadreLauncher] Minimal win32 Padre launcher at your service... we may need now to integrate [PadreLauncher] the build process to Padre...
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@8222 18eddd70-d622-4293-bd05-fe65668e8bde

--- PadreLauncher/compile_padre.bat
+++ PadreLauncher/compile_padre.bat
@@ -1,7 +1,7 @@
 @echo off
 rem Creates Padre launcher C code
-del padre.exe
-del padre-rc.o
+del padre.exe if exists
+del padre-rc.o if exists
 windres padre-rc.rc padre-rc.o
 gcc -O2 -mwindows padre.c padre-rc.o -o padre.exe
 strip padre.exe
