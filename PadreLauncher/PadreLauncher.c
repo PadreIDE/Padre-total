9911686ea3d9202d608423983834bdf252964ada
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-09-22 10:11:22 +0000

    [PadreLauncher] Code cleanup... only the things we really need is here.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@8219 18eddd70-d622-4293-bd05-fe65668e8bde

--- PadreLauncher/PadreLauncher.c
+++ PadreLauncher/PadreLauncher.c
@@ -1,65 +1,20 @@
- 
 /**
- * Padre's Win32 Launcher Evil Experiment in C
+ * Padre minimal Win32 Executable Launcher
+ * @author Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
  */
 #include <windows.h>
  
 /**
- *
+ * When called by windows, we simply launch Padre from here
  */
-LRESULT CALLBACK WndProc(
-	HWND   hWnd,
-	UINT   msg,
-	WPARAM wParam,
-	LPARAM lParam ) {
- 
-	switch( msg ) {
-	case WM_PAINT: {
-		PAINTSTRUCT ps;
-		HDC hDC = BeginPaint( hWnd, &ps );
-		TextOut(hDC, 10, 10, "ADP GmbH", 8 );
-		EndPaint( hWnd, &ps );
-	}
-	break;
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, 
+	LPSTR lpCmdLine, int nCmdShow) 
+{
 
-	case WM_DESTROY:
-	  PostQuitMessage(0);
-	break;
-
-	default:
-	  return DefWindowProc( hWnd, msg, wParam, lParam);
-	} 
-	return 0;
-}
- 
-/**
- *
- */
-int WINAPI WinMain( 
-	HINSTANCE hInstance, 
-	HINSTANCE hPrevInstance, 
-	LPSTR lpCmdLine, 
-	int nCmdShow) {
- 
-	WNDCLASSEX wce;
-
-	wce.cbSize        = sizeof(wce);
-	wce.style         = CS_VREDRAW | CS_HREDRAW; 
-	wce.lpfnWndProc   = (WNDPROC) WndProc; 
-	wce.cbClsExtra    = 0; 
-	wce.cbWndExtra    = 0; 
-	wce.hInstance     = hInstance; 
-	wce.hIcon         = LoadIcon((HINSTANCE) NULL, IDI_APPLICATION); 
-	wce.hCursor       = LoadCursor((HINSTANCE) NULL, IDC_ARROW); 
-	wce.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH); 
-	wce.lpszMenuName  = 0;
-	wce.lpszClassName = "ADPWinClass",
-	wce.hIconSm       = 0;
-
-	if (!RegisterClassEx(&wce)) return 0; 
-
+	// To use ShellExecute
 	LoadLibrary("shell32");
 
+	// Open Padre now..
 	HINSTANCE instance;
 	ShellExecute(
 		NULL,
@@ -69,35 +24,13 @@ int WINAPI WinMain(
 		NULL,
 		SW_SHOWMAXIMIZED
 	);
-	// HWND hWnd = CreateWindowEx(
-	// 0,                      // Ex Styles
-	// "ADPWinClass",
-	// "ADP GmbH",
-	 // WS_OVERLAPPEDWINDOW,
-	 // CW_USEDEFAULT,  // x
-	 // CW_USEDEFAULT,  // y
-	 // CW_USEDEFAULT,  // Height
-	 // CW_USEDEFAULT,  // Width
-	 // NULL,           // Parent Window
-	 // NULL,           // Menu, or windows id if child
-	 // hInstance,      // 
-	 // NULL            // Pointer to window specific data
-	// );
-
-	// ShowWindow( hWnd, nCmdShow );
-
-	// MSG msg;
-	// int r;
-	// while ((r = GetMessage(&msg, NULL, 0, 0 )) != 0) { 
-		// if (r == -1) {
-			// // Error!
-		// }
-		// else {
-			// TranslateMessage(&msg); 
-			// DispatchMessage(&msg); 
-		// }
-	// } 
 
 	// The application's return value
 	return 0;
 }
+/**
+# Copyright 2008-2009 The Padre development team as listed in Padre.pm.
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
+*/
