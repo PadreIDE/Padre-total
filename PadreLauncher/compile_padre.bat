37311c9e7e6c362cda38689166222776b4436ba6
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-09-22 10:26:46 +0000

    [PadreLauncher] Added Padre's win32 ICOn to be linked up but It is not working atm
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@8221 18eddd70-d622-4293-bd05-fe65668e8bde

--- PadreLauncher/compile_padre.bat
+++ PadreLauncher/compile_padre.bat
@@ -1,5 +1,7 @@
 @echo off
 rem Creates Padre launcher C code
 del padre.exe
-gcc -Os -mwindows padre.c -o padre.exe
+del padre-rc.o
+windres padre-rc.rc padre-rc.o
+gcc -O2 -mwindows padre.c padre-rc.o -o padre.exe
 strip padre.exe
