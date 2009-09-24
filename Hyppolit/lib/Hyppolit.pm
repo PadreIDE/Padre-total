af4565bf1fc93c128ead2648c9303738349c13e6
getty <getty@svngit.padre.perlide.org>
2009-09-24 12:02:00 +0000

    Updating Hyppolit to Adam Bot Framework
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@8278 18eddd70-d622-4293-bd05-fe65668e8bde

--- Hyppolit/lib/Hyppolit.pm
+++ Hyppolit/lib/Hyppolit.pm
@@ -1,193 +1,194 @@
 package Hyppolit;
-use strict;
+use Moses;
 use warnings FATAL => 'all';
 use 5.008005;
 
+#
 # TODO
-# initial welcome message to any new user
+#
+# welcome message to any new user
 # logging
-# accept some commands:
-#   trust nick   DONE
+# daemonize
+# svn commit messages (via svn db)
+# pastebot integration
+# keep history? (comment by Getty: what you mean?)
+# "adult"word check
+#
+
+#
+# DONE
+#
+# karma nick (nick++ / nick--)
+# add trusted users (trust nick)
 #     only allow trusting nick that is currently logged in?
 #     allow setting trust for anyone with +o ? (or as it is now only already trusted nicks?)
-#   karma nick
-#   nick++
-#   nick--
-# daemonize it
-# svn commit messages
+# svn commit messages (via file)
 # trac changes messages
-# pastebot integration
-# word is explanation
-# word is also explanation
-# word?
-# TODO: keep history?
+# reacting on public ticket number or changeset (r1 or #1)
+# calcbot functionality (word is / word is also / word?)
+# logging irc (via POE::Component::IRC::Plugin::Logger)
+#
 
 # When it gets OP bit it should go over all the current nicks and add ops to the trusted people
 # oh actually I think it can just rty to add ops to every trusted person and it will give only to those who
 # have no OP yet
 # Also check that it can give op when someone changs alias to a trusted one
 
+our $VERSION = '0.06';
 
+use MooseX::Storage;
 
-our $VERSION = '0.05';
-
-use base 'Exporter';
-
-use POE;
-use POE::Component::IRC::State;
-use POE::Component::IRC::Plugin::AutoJoin;
 use POE::Component::IRC::Plugin::Logger;
 use POE::Component::IRC::Plugin::FollowTail;
 use DBI;
 
 use Data::Dumper;
 
-use YAML::Syck qw(LoadFile DumpFile);
-my $svnlook = '/usr/bin/svnlook';
+with Storage(
+	format => 'YAML',
+	io => 'AtomicFile',
+);
 
-my @methods = qw(
-	_start irc_join irc_public irc_msg
-	irc_tail_input irc_tail_error irc_tail_reset
-	trac_check
-	);
-our @EXPORT = @methods;
-my $config;
-my $config_file;
+#####################################################################
 
-sub save_config {
-	DumpFile($config_file, $config);
-}
+has svnlook => ( is => 'rw', isa => 'Maybe[Str]', default => sub { '/usr/bin/svnlook' } );
+has logdir => ( is => 'rw', isa => 'Maybe[Str]' );
+has svninputfile => ( is => 'rw', isa => 'Maybe[Str]' );
+has tracdb => ( is => 'rw', isa => 'Maybe[Str]' );
+has tracurl => ( is => 'rw', isa => 'Maybe[Str]', default => sub { 'http://padre.perlide.org/trac/' } );
+has repo => ( is => 'rw', isa => 'Maybe[Str]' );
 
-sub run {
-	my $class = shift;
-	$config_file = shift;
-	
-	die "Missing channels. Usage $0 config_file.yml\n" if not $config_file;
-	$config = LoadFile($config_file);
+#####################################################################
 
-	die "No server defined\n" if not $config->{server};
-	die "No nick defined\n" if not $config->{nick};
-	
-	if (not $config->{channels} or 
-		not ref $config->{channels} or
-		ref($config->{channels}) ne 'ARRAY' or
-		not @{ $config->{channels} }) {
-		die "No channels defined\n";
-	}
+has last_trac_check => ( is => 'rw', isa => 'Int', default => sub { 0 } );
+has trusted => ( is => 'rw', isa => 'HashRef', default => sub { {} } );
+has karma => ( is => 'rw', isa => 'HashRef', default => sub { {} } );
+has calc => ( is => 'rw', isa => 'HashRef', default => sub { {} } );
 
-	print Dumper $config;
+#####################################################################
 
-	POE::Session->create(
-		package_states => [
-			main => \@methods
-		]
-	);
+has tracdbh => (
+	is => 'rw', isa => 'DBI::db', lazy => 1,
+	traits => ['DoNotSerialize'],
+	default => sub {
+		my $self = shift;
+		if ($self->tracdb) {
+			DBI->connect("dbi:SQLite:dbname=".$self->tracdb,"","");
+		}
+	},
+);
 
-	$poe_kernel->run();
-}
+# dont store specific MooseX Role attributes
+has [ '+_irc', '+configfile', '+logger', '+plugins', '+ARGV', '+extra_argv', '+use_logger_singleton' ] => (
+	traits => ['DoNotSerialize'],
+);
 
-# so that it is accessable outside of the PoCo::IRC
-my $irc;
-my $dbh;
+around 'pack' => sub {
+	my ($next, $self) = @_;
+	my $hash = $self->$next(@_);
+	delete $hash->{__CLASS__};
+	return $hash;
+};
 
-sub _start {
-	$irc = POE::Component::IRC::State->spawn(
-		Nick   => $config->{nick},
-		Server => $config->{server},
-	);
+####################################################################
 
-	# TODO: AutoJoin does not seem to rejoin after it was kicked out.
-
-	$irc->plugin_add('AutoJoin', POE::Component::IRC::Plugin::AutoJoin->new(
-		Channels => $config->{channels},
-	));
-	if ($config->{logdir}) {
-		$irc->plugin_add('Logger', POE::Component::IRC::Plugin::Logger->new(
-			Path         => $config->{logdir},
+sub custom_plugins {
+	my $self = shift;
+	my %custom_plugins;
+	if ($self->logdir) {
+		$custom_plugins{'Logger'} = POE::Component::IRC::Plugin::Logger->new(
+			Path         => $self->logdir,
 			Private      => 0,
 			Public       => 1,
 # Restricted => 0,   #did not help
 			Sort_by_date => 1,
-		));
-		system "chmod -R 755 $config->{logdir}"; # TODO move to a better place
+		);
+		system "chmod -R 755 ".$self->logdir; # TODO move to a better place
 	}
-	if ($config->{inputfile}) {
-		$irc->plugin_add( 'FollowTail' => POE::Component::IRC::Plugin::FollowTail->new( 
-			filename => $config->{inputfile},
-		));
+	if ($self->svninputfile) {
+		$custom_plugins{'FollowTail'} = POE::Component::IRC::Plugin::FollowTail->new( 
+			filename => $self->svninputfile,
+		);
 	}
-
-	$irc->yield(register => 'join');
-	#$irc->yield(register => 'all');
-	$irc->yield('connect');
-
-	if ($config->{tracdb}) {
-		$dbh = DBI->connect("dbi:SQLite:dbname=".$config->{tracdb},"","");
-		$_[ KERNEL ]->delay(trac_check => 5);
-	}
-
+	return \%custom_plugins;
 }
 
-sub irc_public {
+sub save {
+	my ($self) = @_;
+	$self->store( $self->configfile->stringify );
+}
+
+sub BUILD {
+	my $self = shift;
+
+	if ($self->tracdb) {
+		POE::Kernel->delay(trac_check => 5);
+	}
+}
+
+event irc_public => sub {
+	my $self = $_[OBJECT];
 	my $nick = (split /!/, $_[ARG0])[0];
 	my $channel = $_[ARG1];
-# now unnecessary
-#	my $irc = $_[SENDER]->get_heap();
 
 	my $text = $_[ARG2];
 	
-	#print "Nick $nick on channel @$channel said the following: '$text'\n";
-	if ($text =~ /^\s*  $config->{nick} \s* [,:]? \s* trust  \s+  (.*)/x ) {
-		#print "trust '$1'\n";
-		if ( $config->{trusted}{$nick} ) {
+	$self->debug("Nick $nick on channel @$channel said the following: '$text'");
+
+	my $nickname = $self->irc->nick_name;
+
+	# check for trust
+	if ($text =~ /^\s*  $nickname \s* [,:]? \s* trust  \s+  (.*)/x ) {
+		$self->debug("trust '$1'");
+		if ( $self->trusted->{$nick} ) {
 			foreach my $n (split /\s*[ ,]\s*/, $1) {
-				if ( $config->{trusted}{$n} ) {
-					$irc->yield(privmsg => $channel, "$n was already trusted");
+				if ( $self->trusted->{$n} ) {
+					$self->privmsg( $channel => "$n was already trusted");
 				} else {
-					$config->{trusted}{$n} = 1;
-					$irc->yield(privmsg => $channel, "Consider $n trusted");
+					$self->trusted->{$n} = 1;
+					$self->privmsg( $channel => "Consider $n trusted");
 				}
-				set_op($irc, $channel, $n);
+				$self->set_op($channel, $n);
 			}
-			save_config();
+			$self->save;
 		}
 	}
 	
-	if ($text =~ /^\s*  $config->{nick} \s* [,:]? \s* (\S+)  \s+ is \s+ also \s+ (.*)/x ) {
+	# calcbot functionality
+	if ($text =~ /^\s*  $nickname \s* [,:]? \s* (\S+)  \s+ is \s+ also \s+ (.*)/x ) {
 		my $word = $1;
-		$config->{is}{$word} .= " and also $2";
-		save_config();
-		$irc->yield(privmsg => $channel, "$word is now $config->{is}{$word}");
-	} elsif ($text =~ /^\s*  $config->{nick} \s* [,:]? \s* (\S+)  \s+ is \s+ (.*)/x ) {
+		$self->calc->{$word} .= " and also $2";
+		$self->privmsg( $channel => "$word is now ".$self->calc->{$word} );
+		$self->save;
+	} elsif ($text =~ /^\s*  $nickname \s* [,:]? \s* (\S+)  \s+ is \s+ (.*)/x ) {
 		my $word = $1;
-		my $was = $config->{is}{$word} || 'unknown';
-		$config->{is}{$word} = $2;
-		save_config();
-		$irc->yield(privmsg => $channel, "$word was $was");
-		$irc->yield(privmsg => $channel, "$word is now $config->{is}{$word}");
+		my $was = $self->calc->{$word} || 'unknown';
+		$self->calc->{$word} = $2;
+		$self->save;
+		$self->privmsg( $channel => "$word was $was" );
+		$self->privmsg( $channel => "$word is now $self->calc->{$word}" );
 	} elsif ($text =~ /^\s*  (\S+)\?  \s*$/x ) {
-		if ($1 eq $config->{nick}) {
-			$irc->yield(privmsg => $channel, "$config->{nick} is a bot currently running version $VERSION");
-			$irc->yield(privmsg => $channel, "My master is szabgab.");
-		} elsif ($config->{is}{$1}) {
-			$irc->yield(privmsg => $channel, "$1 is $config->{is}{$1}");
+		if ($1 eq $nickname ) {
+			$self->privmsg( $channel => "$nickname is a bot currently running version $VERSION. My master is szabgab." );
+		} elsif ($self->calc->{$1}) {
+			$self->privmsg( $channel => "$1 is $self->calc->{$1}" );
 		} else {
-			#$irc->yield(privmsg => $channel, "I don't know what $1 is");
+			#$self->privmsg( $channel => "I don't know what $1 is" );
 		}
 	}
 
 	# regexp need adjusting, i'm bad at it ;)...
 	if ($text =~ /\#(\d+)/x) {
 		if ($1+0 > 0) {
-			my $text = trac_ticket_text($1);
-			$irc->yield(privmsg => $channel, $text ) if $text;
+			my $text = $self->trac_ticket_text($1);
+			$self->privmsg( $channel => $text ) if $text;
 		}
 	}
 
 	# regexp need adjusting, i'm bad at it ;)...
 	if ($text =~ /r(\d+)/x) {
 		# no check at all... TODO
-		$irc->yield(privmsg => $channel, trac_changeset_text($1)) if $1+0 > 0;
+		$self->privmsg( $channel => $self->trac_changeset_text($1)) if $1+0 > 0;
 	}
 
 	# TODO karma only users who are around ?
@@ -195,158 +196,155 @@ sub irc_public {
 	if ($text =~ /(\S+)(\+\+|--)/) {
 		my ($nick, $karma) = ($1, $2);
 		if ($karma eq '++') {
-			$config->{karma}{$nick}++;
+			$self->karma->{$nick}++;
 		} else {
-			$config->{karma}{$nick}--;
+			$self->karma->{$nick}--;
 		}
-		save_config();
+		$self->save;
 	}
+
 	if ($text =~ /^\s* karma \s+ (\S+) \s*$/x) {
-		my $karma = $config->{karma}{$1} || 0;
-		$irc->yield(privmsg => $channel, "Karma of $1 is $karma");
+		my $karma = $self->karma->{$1} || 0;
+		$self->privmsg( $channel => "Karma of $1 is $karma");
 	}
-}
+};
 
-
-sub irc_msg {
+event irc_msg => sub {
+	my $self = $_[OBJECT];
 	my $nick = (split /!/, $_[ARG0])[0];
-	#my $channel = $_[ARG1];
-# now unnecessary
-#	my $irc = $_[SENDER]->get_heap();
-
 	my $text = $_[ARG2];
 
-	#print "Nick $nick said to me '$text'\n";
-}
+	$self->debug("Nick $nick said to me '$text'");
+};
 
-sub irc_join {
+event irc_join => sub {
+	my $self = $_[OBJECT];
 	my $nick = (split /!/, $_[ARG0])[0];
 	my $channel = $_[ARG1];
-# now unnecessary
-#	my $irc = $_[SENDER]->get_heap();
 
 	# only send the message if we were the one joining
-	if ($nick eq $irc->nick_name()) {
-		#$irc->yield(privmsg => $channel, "Hi everybody! I am the local bot ($VERSION)");
+	if ($nick eq $self->nickname) {
+		#$self->privmsg( $channel => "Hi everybody! I am $self->nickname, your bot butler ($VERSION)" );
 	}
 
 	# TODO for now it is on every channel
 	# but it should work with some database
 	
-	if ( $config->{trusted}{$nick} ) {
-		set_op($irc, $channel, $nick);
+	if ( $self->trusted->{$nick} ) {
+		$self->set_op($channel, $nick);
 	}
-}
+};
 
 sub set_op {
-	my ($irc, $channel, $nick) = @_;
+	my ($self, $channel, $nick) = @_;
 	if (ref $channel and ref($channel) eq 'ARRAY') {
 		($channel) = @$channel;
 	}
-	print "Giving op to '$nick' on '$channel' ($irc)\n";
-	$irc->yield(mode => $channel => "+o $nick");
-# its already at another place, should be removed here?
-#	system "chmod -R 755 $config->{logdir}"; # TODO move to a better place
+	$self->debug("Giving op to '$nick' on '$channel'");
+	$self->mode( $channel => "+o $nick" );
 }
 
-sub _default {
-	my $nick = (split /!/, $_[ARG0])[0];
-	print "Default: $nick ", scalar(@_), "\n";
-}
+# dont get sense ;) help
+#sub _default {
+#	my $nick = (split /!/, $_[ARG0])[0];
+#	$self->debug("Default: $nick ", scalar(@_), "\n");
+#}
 
-sub irc_all {
+event irc_all => sub {
 	my $nick = (split /!/, $_[ARG0])[0];
 	print "All: $nick ", scalar(@_), "\n";
-}
+};
 
-
-sub irc_tail_input {
-	my ($kernel, $sender, $filename, $input) = @_[KERNEL, SENDER, ARG0, ARG1];
-	my $repo = $config->{repo};
-	return if not $repo;
+event irc_tail_input => sub {
+	my ($self, $kernel, $sender, $filename, $input) = @_[OBJECT, KERNEL, SENDER, ARG0, ARG1];
+	return if not $self->repo;
 	if ($input =~ /^SVN (\d+)$/) {
 		my $id = $1;
-		my $author = qx{$svnlook author $repo -r $id};
+		my $author = qx{$self->svnlook author $self->repo -r $id};
 		chomp $author;
-		$config->{karma}{$author}++;
-		my $log    = qx{$svnlook log $repo -r $id};
-		my @dirs   = qx{$svnlook dirs-changed $repo -r $id};
+		$self->karma->{$author}++;
+		my $log    = qx{$self->svnlook log $self->repo -r $id};
+		my @dirs   = qx{$self->svnlook dirs-changed $self->repo -r $id};
 		chomp @dirs;
-		my $msg    = "svn: r$id | $author++ | http://padre.perlide.org/trac/changeset/$id\n";
-		$kernel->post( $sender, 'privmsg', $_, $msg ) for @{ $config->{channels} };
+		my $msg    = "svn: r$id | $author++ | ".$self->tracurl."changeset/$id\n";
+		$self->privmsg( $_ => $msg ) for @{ $self->channels };
 		foreach my $line (split /\n/, $log) {
-			$kernel->post( $sender, 'privmsg', $_, "     $line" ) for @{ $config->{channels} };
+			$self->privmsg( $_ => "     $line" ) for @{ $self->get_channels };
 		}
 		my $dirs = join " ", @dirs;
-		$kernel->post( $sender, 'privmsg', $_, "     $dirs" ) for @{ $config->{channels} };
-		
+		$self->privmsg( $_, "     $dirs" ) for @{ $self->get_channels };
 	}
 	# TODO report error ?
 	# $kernel->post( $sender, 'privmsg', $_, "$config->{inputfile} $input" ) for @{ $config->{channels} };
 	return;
-}
+};
 
-sub irc_tail_error {
-	my ($kernel, $sender, $filename, $errnum, $errstring)
-		= @_[KERNEL, SENDER, ARG0 .. ARG2];
-	$kernel->post( $sender, 'privmsg', $_, "SVN ERROR: $errnum $errstring" ) for @{ $config->{channels} };
-# now unnecessary
-#	my $irc = $sender->get_heap();
-	$irc->plugin_del( 'FollowTail' );
-	return;
-}
+# TODO - maybe unnecessary if switched to SVN::Client
+#sub irc_tail_error {
+#	my ($kernel, $sender, $filename, $errnum, $errstring)
+#		= @_[KERNEL, SENDER, ARG0 .. ARG2];
+#	$kernel->post( $sender, 'privmsg', $_, "SVN ERROR: $errnum $errstring" ) for @{ $config->{channels} };
+#	$irc->plugin_del( 'FollowTail' );
+#	return;
+#}
 
-sub irc_tail_reset {
-	my ($kernel, $sender, $filename) = @_[KERNEL, SENDER, ARG0];
+# TODO - maybe unnecessary if switched to SVN::Client
+#sub irc_tail_reset {
+#	my ($kernel, $sender, $filename) = @_[KERNEL, SENDER, ARG0];
 #	$kernel->post( $sender, 'privmsg', $_, "$config->{inputfile} RESET EVENT" ) for @{ $config->{channels} };
-	return;
-}
+#	return;
+#}
 
 sub trac_changeset_text {
+	my $self = shift;
+	return if !$self->tracurl;
 	my $changeset_id = shift;
-	return "Changeset #".$changeset_id." http://padre.perlide.org/trac/changeset/".$changeset_id;
+	return "Changeset #".$changeset_id." ".$self->tracurl."changeset/".$changeset_id;
 }
 
 sub trac_ticket_text {
-	return if !$config->{tracdb};
+	my $self = shift;
+	return if !$self->tracurl;
+	return if !$self->tracdb;
 	my $ticket_id = shift;
-	my $ticket = $dbh->selectrow_hashref("
+	my $ticket = $self->tracdbh->selectrow_hashref("
 		select * from ticket
 			where id = ?
 	", {}, $ticket_id);
 	return if !$ticket;
-	my $ticket_comment = $dbh->selectrow_hashref("
+	my $ticket_comment = $self->tracdbh->selectrow_hashref("
 		select oldvalue from ticket_change
 			where ticket = ? and field = 'comment'
 			order by time desc
 	", {}, $ticket_id);
-	my $url = "http://padre.perlide.org/trac/ticket/".$ticket_id;
+	my $url = $self->tracurl."ticket/".$ticket_id;
 	$url .= "#comment:".$ticket_comment->{oldvalue} if $ticket_comment and $ticket_comment->{oldvalue};
 	return "#".$ticket_id.": ".$ticket->{summary}." (".$ticket->{status}." ".$ticket->{type}.") [ ".$url." ]";
 }
 
-sub trac_check {
+event trac_check => sub {
+	my $self = $_[OBJECT];
 	my $trac_check_time = time;
-	my $last_trac_check = $config->{last_trac_check};
+	my $last_trac_check = $self->last_trac_check;
 
-	my $tickets = $dbh->selectall_hashref("
+	my $tickets = $self->tracdbh->selectall_hashref("
 		select id from ticket
 			where changetime > ? and changetime <= ?
 			order by changetime asc
 	", "id", {}, $last_trac_check, $trac_check_time);
 
 	for my $ticket_id (keys %{$tickets}) {
-		my $text = trac_ticket_text($ticket_id);
+		my $text = $self->trac_ticket_text($ticket_id);
 		if ($text) {
-			$irc->yield( privmsg => $_, $text) for @{ $config->{channels} };
+			$self->privmsg( $_ => $text ) for @{ $self->channels };
 		}
 	}
 
-	$config->{last_trac_check} = $trac_check_time;
-	save_config;
-	$_[KERNEL]->delay(trac_check => 30);
+	$self->last_trac_check($trac_check_time);
+	$self->save;
+	POE::Kernel->delay(trac_check => 15);
 	return;
-}
+};
 
 1;
 
