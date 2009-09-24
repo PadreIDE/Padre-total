dc151b102c70d0136d8875fb7cc8dfe9cda3e1ef
getty <getty@svngit.padre.perlide.org>
2009-09-24 22:34:35 +0000

    more cleaning up on Hyppolit. added profanity check. "save point" commit before going plugins
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@8321 18eddd70-d622-4293-bd05-fe65668e8bde

--- Hyppolit/lib/Hyppolit.pm
+++ Hyppolit/lib/Hyppolit.pm
@@ -12,7 +12,6 @@ use 5.008005;
 # svn commit messages (via svn db)
 # pastebot integration
 # keep history? (comment by Getty: what you mean?)
-# "adult"word check
 #
 
 #
@@ -27,6 +26,7 @@ use 5.008005;
 # reacting on public ticket number or changeset (r1 or #1)
 # calcbot functionality (word is / word is also / word?)
 # logging irc (via POE::Component::IRC::Plugin::Logger)
+# profanity check
 #
 
 # When it gets OP bit it should go over all the current nicks and add ops to the trusted people
@@ -41,6 +41,7 @@ use MooseX::Storage;
 use POE::Component::IRC::Plugin::Logger;
 use POE::Component::IRC::Plugin::FollowTail;
 use DBI;
+use Regexp::Common qw( pattern profanity );
 
 use Data::Dumper;
 
@@ -50,6 +51,16 @@ with Storage(
 );
 
 #####################################################################
+#
+# regexp patterns
+#
+
+
+
+#####################################################################
+#
+# configuration attributes
+#
 
 has svnlook => ( is => 'rw', isa => 'Maybe[Str]', default => sub { '/usr/bin/svnlook' } );
 has logdir => ( is => 'rw', isa => 'Maybe[Str]' );
@@ -59,6 +70,9 @@ has tracurl => ( is => 'rw', isa => 'Maybe[Str]', default => sub { 'http://padre
 has repo => ( is => 'rw', isa => 'Maybe[Str]' );
 
 #####################################################################
+#
+# saved attributes
+#
 
 has last_trac_check => ( is => 'rw', isa => 'Int', default => sub { 0 } );
 has trusted => ( is => 'rw', isa => 'HashRef', default => sub { {} } );
@@ -66,15 +80,16 @@ has karma => ( is => 'rw', isa => 'HashRef', default => sub { {} } );
 has calc => ( is => 'rw', isa => 'HashRef', default => sub { {} } );
 
 #####################################################################
+#
+# internal attributes and special magic
+#
 
 has tracdbh => (
 	is => 'rw', isa => 'DBI::db', lazy => 1,
 	traits => ['DoNotSerialize'],
 	default => sub {
 		my $self = shift;
-		if ($self->tracdb) {
-			DBI->connect("dbi:SQLite:dbname=".$self->tracdb,"","");
-		}
+		DBI->connect("dbi:SQLite:dbname=".$self->tracdb,"","");
 	},
 );
 
@@ -83,6 +98,7 @@ has [ '+_irc', '+configfile', '+logger', '+plugins', '+ARGV', '+extra_argv', '+u
 	traits => ['DoNotSerialize'],
 );
 
+# remove the __CLASS__ attribute from the serialized object (i love sugar)
 around 'pack' => sub {
 	my ($next, $self) = @_;
 	my $hash = $self->$next(@_);
@@ -96,136 +112,31 @@ our %Channel_Text = (
 );
 
 ####################################################################
-
-sub custom_plugins {
-	my $self = shift;
-	my %custom_plugins;
-	if ($self->logdir) {
-		$custom_plugins{'Logger'} = POE::Component::IRC::Plugin::Logger->new(
-			Path         => $self->logdir,
-			Private      => 0,
-			Public       => 1,
-# Restricted => 0,   #did not help
-			Sort_by_date => 1,
-		);
-		system "chmod -R 755 ".$self->logdir; # TODO move to a better place
-	}
-	if ($self->svninputfile) {
-		$custom_plugins{'FollowTail'} = POE::Component::IRC::Plugin::FollowTail->new( 
-			filename => $self->svninputfile,
-		);
-	}
-	return \%custom_plugins;
-}
-
-sub save {
-	my ($self) = @_;
-	$self->store( $self->configfile->stringify );
-}
-
-sub BUILD {
-	my $self = shift;
-
-	if ($self->tracdb) {
-		POE::Kernel->delay(trac_check => 5);
-	}
-}
+#
+# events
+#
 
 event irc_public => sub {
-	my $self = $_[OBJECT];
-	my $nick = (split /!/, $_[ARG0])[0];
-	my $channel = $_[ARG1];
-
-	my $text = $_[ARG2];
+	my ( $self, $ircsender, $channel, $text ) = @_[ OBJECT, ARG0, ARG1, ARG2 ];
+	my $nick = (split /!/, $ircsender)[0];
 	
 	$self->debug("Nick $nick on channel @$channel said the following: '$text'");
 
-	my $nickname = $self->irc->nick_name;
-
-	# check for trust
-	if ($text =~ /^\s*  $nickname \s* [,:]? \s* trust  \s+  (.*)/x ) {
-		$self->debug("trust '$1'");
-		if ( $self->trusted->{$nick} ) {
-			foreach my $n (split /\s*[ ,]\s*/, $1) {
-				if ( $self->trusted->{$n} ) {
-					$self->privmsg( $channel => "$n was already trusted");
-				} else {
-					$self->trusted->{$n} = 1;
-					$self->privmsg( $channel => "Consider $n trusted");
-				}
-				$self->set_op($channel, $n);
-			}
-			$self->save;
-		}
-	}
-	
-	# calcbot functionality
-	if ($text =~ /^\s*  $nickname \s* [,:]? \s* (\S+)  \s+ is \s+ also \s+ (.*)/x ) {
-		my $word = $1;
-		$self->calc->{$word} .= " and also $2";
-		$self->privmsg( $channel => "$word is now ".$self->calc->{$word} );
-		$self->save;
-	} elsif ($text =~ /^\s*  $nickname \s* [,:]? \s* (\S+)  \s+ is \s+ (.*)/x ) {
-		my $word = $1;
-		my $was = $self->calc->{$word} || 'unknown';
-		$self->calc->{$word} = $2;
-		$self->save;
-		$self->privmsg( $channel => "$word was $was" );
-		$self->privmsg( $channel => "$word is now $self->calc->{$word}" );
-	} elsif ($text =~ /^\s*  (\S+)\?  \s*$/x ) {
-		if ($1 eq $nickname ) {
-			$self->privmsg( $channel => "$nickname is a bot currently running version $VERSION. My master is szabgab." );
-		} elsif ($self->calc->{$1}) {
-			$self->privmsg( $channel => "$1 is $self->calc->{$1}" );
-		} else {
-			#$self->privmsg( $channel => "I don't know what $1 is" );
-		}
-	}
-
-	# regexp need adjusting, i'm bad at it ;)...
-	if ($text =~ /\#(\d+)/x) {
-		if ($1+0 > 0) {
-			my $text = $self->trac_ticket_text($1);
-			$self->privmsg( $channel => $text ) if $text;
-		}
-	}
-
-	# regexp need adjusting, i'm bad at it ;)...
-	if ($text =~ /r(\d+)/x) {
-		# no check at all... TODO
-		$self->privmsg( $channel => $self->trac_changeset_text($1)) if $1+0 > 0;
-	}
-
-	# TODO karma only users who are around ?
-	# record karma
-	if ($text =~ /(\S+)(\+\+|--)/) {
-		my ($nick, $karma) = ($1, $2);
-		if ($karma eq '++') {
-			$self->karma->{$nick}++;
-		} else {
-			$self->karma->{$nick}--;
-		}
-		$self->save;
-	}
-
-	if ($text =~ /^\s* karma \s+ (\S+) \s*$/x) {
-		my $karma = $self->karma->{$1} || 0;
-		$self->privmsg( $channel => "Karma of $1 is $karma");
-	}
+	$self->parsetext($nick, $channel, $text);
 };
 
 event irc_msg => sub {
-	my $self = $_[OBJECT];
-	my $nick = (split /!/, $_[ARG0])[0];
-	my $text = $_[ARG2];
+	my ( $self, $ircsender, $targets, $text ) = @_[ OBJECT, ARG0, ARG1, ARG2 ];
+	my $nick = (split /!/, $ircsender)[0];
 
 	$self->debug("Nick $nick said to me '$text'");
+
+	$self->parsetext($nick, $nick, $text);
 };
 
 event irc_join => sub {
-	my $self = $_[OBJECT];
-	my $nick = (split /!/, $_[ARG0])[0];
-	my $channel = $_[ARG1];
+	my ( $self, $ircsender, $channel ) = @_[ OBJECT, ARG0, ARG1 ];
+	my $nick = (split /!/, $ircsender)[0];
 
 	# only send the message if we were the one joining
 	if ($nick eq $self->nickname) {
@@ -239,15 +150,6 @@ event irc_join => sub {
 		$self->set_op($channel, $nick);
 	}
 };
-
-sub set_op {
-	my ($self, $channel, $nick) = @_;
-	if (ref $channel and ref($channel) eq 'ARRAY') {
-		($channel) = @$channel;
-	}
-	$self->debug("Giving op to '$nick' on '$channel'");
-	$self->mode( $channel => "+o $nick" );
-}
 
 # dont get sense ;) help
 #sub _default {
@@ -300,6 +202,176 @@ event irc_tail_input => sub {
 #	return;
 #}
 
+event trac_check => sub {
+	my $self = $_[OBJECT];
+	my $trac_check_time = time;
+	my $last_trac_check = $self->last_trac_check;
+
+	my $tickets = $self->tracdbh->selectall_hashref("
+		select id from ticket
+			where changetime > ? and changetime <= ?
+			order by changetime asc
+	", "id", {}, $last_trac_check, $trac_check_time);
+
+	for my $ticket_id (keys %{$tickets}) {
+		my $text = $self->trac_ticket_text($ticket_id);
+		if ($text) {
+			$self->privmsg( $_ => $text ) for @{ $self->channels };
+		}
+	}
+
+	$self->last_trac_check($trac_check_time);
+	$self->save;
+	POE::Kernel->delay(trac_check => 15);
+	return;
+};
+
+####################################################################
+#
+# class methods
+#
+
+sub START {
+	my $self = shift;
+	POE::Kernel->delay(trac_check => 5) if ($self->tracdb);
+}
+
+sub parsetext {
+	my ( $self, $nick, $from, $text ) = @_;
+	my $botnick = $self->irc->nick_name;
+
+	#
+	# check for trust
+	#
+	if ($text =~ /^\s*  $botnick \s* [,:]? \s* trust  \s+  (.*)/x ) {
+		$self->debug("trust '$1'");
+		if ( $self->trusted->{$nick} ) {
+			foreach my $n (split /\s*[ ,]\s*/, $1) {
+				if ( $self->trusted->{$n} ) {
+					$self->privmsg( $from => "$n was already trusted");
+				} else {
+					$self->trusted->{$n} = 1;
+					$self->privmsg( $from => "Consider $n trusted");
+				}
+				$self->set_op($from, $n) if /^\#/x;
+			}
+			$self->save;
+		}
+	}
+
+	#
+	# TODO karma only users who are around ? record karma
+	#
+	if ($text =~ /(\S+)(\+\+|--)/) {
+		if ($from =~ /^\#/x) {
+			$self->privmsg( $from => "I dont give karma in private." );
+		} else {
+			my ($karmanick, $karma) = ($1, $2);
+			if ($karma eq '++') {
+				$self->karma->{$karmanick}++;
+			} else {
+				$self->karma->{$karmanick}--;
+			}
+			$self->save;
+		}
+	} elsif ($text =~ /^\s* karma \s+ (\S+) \s*$/x) {
+		my $karma = $self->karma->{$1} || 0;
+		$self->privmsg( $from => "Karma of $1 is $karma");
+	}
+
+	#
+	# bad language check
+	#
+	if ($text =~ /$RE{profanity}/) {
+		if ($nick eq 'Getty') {
+			$self->privmsg( $from => "Getty: not again, please..." );
+		} else {
+			$self->privmsg( $from => $nick.
+				": This channel is a public support channel for people from every age. Please consider a language also suitable for kids."
+			);
+		}
+	}
+
+	#
+	# calcbot functionality
+	#
+	if ($text =~ /^\s*  $botnick \s* [,:]? \s* (\S+)  \s+ is \s+ also \s+ (.*)/x ) {
+		my $word = $1;
+		$self->calc->{$word} .= " and also $2";
+		$self->privmsg( $from => "$word is now ".$self->calc->{$word} );
+		$self->save;
+	} elsif ($text =~ /^\s*  $botnick \s* [,:]? \s* (\S+)  \s+ is \s+ (.*)/x ) {
+		my $word = $1;
+		my $was = $self->calc->{$word} || 'unknown';
+		$self->calc->{$word} = $2;
+		$self->save;
+		$self->privmsg( $from => "$word was $was" );
+		$self->privmsg( $from => "$word is now $self->calc->{$word}" );
+	} elsif ($text =~ /^\s*  (\S+)\?  \s*$/x ) {
+		if ($1 eq $botnick ) {
+			$self->privmsg( $from => "$botnick is a bot currently running version $VERSION. My master is szabgab." );
+		} elsif ($self->calc->{$1}) {
+			$self->privmsg( $from => "$1 is $self->calc->{$1}" );
+		} else {
+			#$self->privmsg( $from => "I don't know what $1 is" );
+		}
+	}
+
+	#
+	# regexp need adjusting, i'm bad at it ;)...
+	#
+	if ($text =~ /(?<!\\)\#(\d+)/) {
+		if ($1+0 > 0) {
+			my $tickettext = $self->trac_ticket_text($1);
+			$self->privmsg( $from => $tickettext ) if $tickettext;
+		}
+	}
+	
+	#
+	# regexp need adjusting, i'm bad at it ;)...
+	#
+	if ($text =~ /\br(\d+)/x) {
+		# no check at all... TODO
+		$self->privmsg( $from => $self->trac_changeset_text($1)) if $1+0 > 0;
+	}
+
+}
+
+sub set_op {
+	my ($self, $channel, $nick) = @_;
+	if (ref $channel and ref($channel) eq 'ARRAY') {
+		($channel) = @$channel;
+	}
+	$self->debug("Giving op to '$nick' on '$channel'");
+	$self->mode( $channel => "+o $nick" );
+}
+
+sub custom_plugins {
+	my $self = shift;
+	my %custom_plugins;
+	if ($self->logdir) {
+		$custom_plugins{'Logger'} = POE::Component::IRC::Plugin::Logger->new(
+			Path         => $self->logdir,
+			Private      => 0,
+			Public       => 1,
+# Restricted => 0,   #did not help
+			Sort_by_date => 1,
+		);
+		system "chmod -R 755 ".$self->logdir; # TODO move to a better place
+	}
+	if ($self->svninputfile) {
+		$custom_plugins{'FollowTail'} = POE::Component::IRC::Plugin::FollowTail->new( 
+			filename => $self->svninputfile,
+		);
+	}
+	return \%custom_plugins;
+}
+
+sub save {
+	my $self = shift;
+	$self->store( $self->configfile->stringify );
+}
+
 sub trac_changeset_text {
 	my $self = shift;
 	return if !$self->tracurl;
@@ -327,30 +399,4 @@ sub trac_ticket_text {
 	return "#".$ticket_id.": ".$ticket->{summary}." (".$ticket->{status}." ".$ticket->{type}.") [ ".$url." ]";
 }
 
-event trac_check => sub {
-	my $self = $_[OBJECT];
-	my $trac_check_time = time;
-	my $last_trac_check = $self->last_trac_check;
-
-	my $tickets = $self->tracdbh->selectall_hashref("
-		select id from ticket
-			where changetime > ? and changetime <= ?
-			order by changetime asc
-	", "id", {}, $last_trac_check, $trac_check_time);
-
-	for my $ticket_id (keys %{$tickets}) {
-		my $text = $self->trac_ticket_text($ticket_id);
-		if ($text) {
-			$self->privmsg( $_ => $text ) for @{ $self->channels };
-		}
-	}
-
-	$self->last_trac_check($trac_check_time);
-	$self->save;
-	POE::Kernel->delay(trac_check => 15);
-	return;
-};
-
 1;
-
-
