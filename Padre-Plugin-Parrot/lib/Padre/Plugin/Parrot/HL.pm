c04580c336250d9dc5daab20662e0902c737433b
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-10-03 03:40:46 +0000

    [Parrot] Fixed a compilation error
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@8493 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Parrot/lib/Padre/Plugin/Parrot/HL.pm
+++ Padre-Plugin-Parrot/lib/Padre/Plugin/Parrot/HL.pm
@@ -2,6 +2,8 @@ package Padre::Plugin::Parrot::HL;
 use strict;
 use warnings;
 
+use Padre::Wx  ();
+
 our $VERSION = '0.26';
 
 # colorize timer to make sure that colorize tasks are scheduled properly...
