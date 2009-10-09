ff13a08d892256a3becccff53c9ebfdb568eae2d
Sebastian Willing <sewi@cpan.org>
2009-10-09 14:46:39 +0000

    Clean up files
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@8706 18eddd70-d622-4293-bd05-fe65668e8bde

--- PadreLauncher/compile_padre.bat
+++ PadreLauncher/compile_padre.bat
@@ -1,7 +1,6 @@
 @echo off
 rem Creates Padre launcher executable
-del padre.exe if exists
-del padre-rc.o if exists
+if exist padre.exe del padre.exe
+if exist padre-rc.o del padre-rc.o
 windres padre-rc.rc padre-rc.o
-gcc -Os -mwindows padre.c padre-rc.o -o padre.exe
-strip padre.exe
+gcc -Wall -Os -mwin32 -mwindows -Wl,-s %* padre.c padre-rc.o -o padre.exe
