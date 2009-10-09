1bf742a08251101285b087bff1a315296a3752d5
Sebastian Willing <sewi@cpan.org>
2009-10-09 20:27:21 +0000

    Third version is finally working
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@8719 18eddd70-d622-4293-bd05-fe65668e8bde

--- tools/actions_list.pl
+++ tools/actions_list.pl
@@ -7,53 +7,107 @@ actions_list.pl - Create a list of Padre Actions
 
 =head1 SYNOPSIS
 
-	actions_list.pl --text
-	actions_list.pl --html
+	actions_list.pl --text actions.dump
+	actions_list.pl --html actions.dump
 
 =head1 DESCRIPTION
 
 This script creates a list of all actions supported by Padre
 and outputs it in text or HTML-format. The default is text format.
 
+=head1 USAGE
+
+Set the environment variable PADRE_EXPORT_ACTIONS to 1 and then
+run Padre.
+
+	PADRE_EXPORT_ACTIONS=1 ./dev.pl
+
+It will create a actions.dump - file in your config dir
+(usually ~/.padre on Linux). Just pass this file to actions_list.pl
+
 =cut
 
 use 5.006;
 use strict;
 use warnings;
-use PPI;
 
-our $VERSION = '0.01';
+our $VERSION = '0.03';
 
-our %actions;
+package Local::Output::Text;
 
-# Override the Padre::Action to capture created actions:
-
-package Padre::Action;
-
-sub new {
-	my $class = shift;
-	my %data = @_;
-	print "Add\n";
-	$main::actions{$data{name}} = \%data;
+sub start {
+	my $self = shift;
 }
 
-package main;
+sub action {
+	my $self = shift;
+	my $action = shift;
+	print "***** ".$action->{name}." *****\n";
+	print 'Default shortcut: '.$action->{shortcut}."\n" if defined($action->{shortcut});
+	print $action->{comment}."\n" if defined($action->{comment});
+	print "\n";
+}
 
-# We don't want any Action module to load the real Action.pm:
+sub finish {
+	my $self = shift;
+}
 
-opendir my $mod_dir,'lib/Padre/Action' or die 'Error opening Action-dir';
-for my $Mod (readdir($mod_dir)) {
-	$Mod =~ /\.pm$/ or next;
-	print "Load $Mod\n";
-	my $Document = PPI::Document->new('lib/Padre/Action/'.$Mod);
-	if ( ! defined($Document)) {
-		warn 'Error while parsing '.$Mod;
+
+
+
+package Local::Output::HTML;
+
+sub start {
+	my $self = shift;
+	print <<_EOT_;
+<html><body><table border=1>
+<th><td>Action</td><td>Default<br>shortcut</td><td>Comment</td></th>
+_EOT_
+}
+
+sub action {
+	my $self = shift;
+	my $action = shift;
+	print '<tr>'.
+		'<td>'.$action->{name}.'</td>'.
+		'<td>'.(defined($action->{shortcut}) ? $action->{shortcut} : '').'</td>'.
+		'<td>'.(defined($action->{comment}) ? $action->{comment} : '').'</td>'.
+		"</tr>\n";
+}
+
+sub finish {
+	my $self = shift;
+	print '</table></body></html>';
+}
+
+
+
+
+my %actions;
+our $VAR1;
+
+my $Formatter = 'Local::Output::Text';
+
+for (@ARGV) {
+	if ($_ eq '--text') {
+		$Formatter = 'Local::Output::Text';
+		next;
+	} elsif ($_ eq '--html') {
+		$Formatter = 'Local::Output::HTML';
 		next;
 	}
-	my $New_Sub = (@{$Document->find(sub { $_[1]->isa('PPI::Statement::Sub') and ($_[1]->name eq 'new') })})[0];
-	eval $New_Sub or warn $@;
-}
-close $mod_dir;
 
-use Data::Dumper;
-print Dumper(\%actions);
+	require $_;
+
+	for (keys(%{$VAR1})) {
+		$actions{$_} = $VAR1->{$_};
+	}
+}
+
+$Formatter->start;
+
+for (sort(keys(%actions))) {
+	$Formatter->action($actions{$_});
+}
+
+$Formatter->finish;
