0abbbec8bccf1c59ebc627316f63c16425cd3435
Sebastian Willing <sewi@cpan.org>
2009-10-24 19:38:06 +0000

    Add autocomplete brackets and " to the PHP module
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@8876 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-PHP/lib/Padre/Document/PHP.pm
+++ Padre-Plugin-PHP/lib/Padre/Document/PHP.pm
@@ -11,4 +11,28 @@ our @ISA     = 'Padre::Document';
 
 sub comment_lines_str { return '#' }
 
+sub event_on_char {
+	my ( $self, $editor, $event ) = @_;
+
+	my $main   = Padre->ide->wx->main;
+	my $config = Padre->ide->config;
+
+	$editor->Freeze;
+
+	$self->autocomplete_matching_char($editor,$event,
+			34  => 34,  # " "
+			39  => 39,  # ' '
+			40  => 41,  # ( )
+			60  => 62,  # < >
+			91  => 93,  # [ ]
+			123 => 125, # { }
+		);
+
+	$editor->Thaw;
+
+	$main->on_autocompletion($event) if $config->autocomplete_always;
+
+	return;
+}
+
 1;
