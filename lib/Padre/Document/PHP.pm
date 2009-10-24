5793c31b0332eaff92094e61181bddace6d9a315
Sebastian Willing <sewi@cpan.org>
2009-10-24 19:51:16 +0000

    Add autocomplete features to PHP
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Padre-Plugin-PHP@8877 18eddd70-d622-4293-bd05-fe65668e8bde

--- lib/Padre/Document/PHP.pm
+++ lib/Padre/Document/PHP.pm
@@ -35,4 +35,108 @@ sub event_on_char {
 	return;
 }
 
+sub autocomplete {
+	my $self  = shift;
+	my $event = shift;
+
+	my $editor = $self->editor;
+	my $pos    = $editor->GetCurrentPos;
+	my $line   = $editor->LineFromPosition($pos);
+	my $first  = $editor->PositionFromLine($line);
+
+	# line from beginning to current position
+	my $prefix = $editor->GetTextRange( $first, $pos );
+	my $suffix = $editor->GetTextRange( $pos,   $pos + 15 );
+	$suffix = $1 if $suffix =~ /^(\w*)/; # Cut away any non-word chars
+
+	# The second parameter may be a reference to the current event or the next
+	# char which will be added to the editor:
+	my $nextchar;
+	if ( defined($event) and ( ref($event) eq 'Wx::KeyEvent' ) ) {
+		my $key = $event->GetUnicodeKey;
+		$nextchar = chr($key);
+	} elsif ( defined($event) and ( !ref($event) ) ) {
+		$nextchar = $event;
+	}
+
+	# check for hashs
+	elsif ( $prefix =~ /(\$\w+(?:\-\>)?)\[([\'\"]?)([\$\&]?\w*)$/ ) {
+		my $hashname   = $1;
+		my $textmarker = $2;
+		my $keyprefix  = $3;
+
+		my $last = $editor->GetLength();
+		my $text = $editor->GetTextRange( 0, $last );
+
+		my %words;
+		while ( $text =~ /\Q$hashname\E\[(([\'\"]?)\Q$keyprefix\E.+?\2)\]/g ) {
+			$words{$1} = 1;
+		}
+
+		return (
+			length( $textmarker . $keyprefix ),
+			sort {
+				my $a1 = $a;
+				my $b1 = $b;
+				$a1 =~ s/^([\'\"])(.+)\1/$2/;
+				$b1 =~ s/^([\'\"])(.+)\1/$2/;
+				$a1 cmp $b1;
+				} ( keys(%words) )
+		);
+
+	}
+
+	$prefix =~ s{^.*?((\w+::)*\w+)$}{$1};
+	my $last      = $editor->GetLength();
+	my $text      = $editor->GetTextRange( 0, $last );
+	my $pre_text  = $editor->GetTextRange( 0, $first + length($prefix) );
+	my $post_text = $editor->GetTextRange( $first, $last );
+
+	my $regex;
+	eval { $regex = qr{\b(\Q$prefix\E\w+(?:::\w+)*)\b} };
+	if ($@) {
+		return ("Cannot build regex for '$prefix'");
+	}
+
+	my %seen;
+	my @words;
+	push @words, grep { !$seen{$_}++ } reverse( $pre_text =~ /$regex/g );
+	push @words, grep { !$seen{$_}++ } ( $post_text =~ /$regex/g );
+
+	if ( @words > 20 ) {
+		@words = @words[ 0 .. 19 ];
+	}
+
+	# Suggesting the current word as the only solution doesn't help
+	# anything, but your need to close the suggestions window before
+	# you may press ENTER/RETURN.
+	if ( ( $#words == 0 ) and ( $prefix eq $words[0] ) ) {
+		return;
+	}
+
+	# While typing within a word, the rest of the word shouldn't be
+	# inserted.
+	if ( defined($suffix) ) {
+		for ( 0 .. $#words ) {
+			$words[$_] =~ s/\Q$suffix\E$//;
+		}
+	}
+
+	# This is the final result if there is no char which hasn't been
+	# saved to the editor buffer until now
+	return ( length($prefix), @words ) if !defined($nextchar);
+
+	# Finally cut out all words which do not match the next char
+	# which will be inserted into the editor (by the current event)
+	my @final_words;
+	for (@words) {
+
+		# Accept everything which has prefix + next char + at least one other char
+		next if !/^\Q$prefix$nextchar\E./;
+		push @final_words, $_;
+	}
+
+	return ( length($prefix), @final_words );
+}
+
 1;
