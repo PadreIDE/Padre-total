ca6725267a399c9876a5cddd29be8b5e9060bfdc
Gabor Szabo <szabgab@gmail.com>
2009-11-30 06:50:40 +0000

    [XL] add padre.sh
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@9313 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ Perl-Dist-XL/share/files/padre.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#echo $HOME
+#echo `dirname $0`
+
+path_to_perl=`perl -MCwd -MFile::Basename -e 'print dirname(dirname(Cwd::abs_path(shift)))' $0`
+#echo $path_to_perl
+#echo `dirname $path_to_sh`
+
+export LD_LIBRARY_PATH=$path_to_perl/lib/site_perl/5.10.1/i686-linux-thread-multi/Alien/wxWidgets/gtk_2_8_10_uni/lib/:$LD_LIBRARY_PATH
+
+$path_to_perl/bin/perl $path_to_perl/bin/padre $@
