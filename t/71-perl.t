PadreIDE/Padre-total

commit 0454018e55a676929291a1c458e2097788c94949

[Padre] Enable/Disable Perl-specific refactoring tools based on document type
git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Padre@8089 18eddd70-d622-4293-bd05-fe65668e8bde

lib/Padre/Wx/Menu/Perl.pm
@@ -208,7 +208,10 @@ sub refresh {
 	# but not in a Perl document.
 	$self->{find_brace}->Enable($perl);
 	$self->{find_variable}->Enable($perl);
+	$self->{find_variable}->Enable($perl);
 	$self->{rename_variable}->Enable($perl);
+	$self->{introduce_temporary}->Enable($perl);
+	$self->{extract_subroutine}->Enable($perl);
 
 	# Apply config-driven state
 	$self->{autocomplete_brackets}->Check( $config->autocomplete_brackets );

commit 879598a8dd6430318752ca32c5b22d205217c69a

Adding regression test for the bugs introduced with r8089
git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk/Padre@9374 18eddd70-d622-4293-bd05-fe65668e8bde

t/71-perl.t
@@ -9,7 +9,7 @@ BEGIN {
 		plan skip_all => 'Needs DISPLAY';
 		exit 0;
 	}
-	plan( tests => 24 );
+	plan( tests => 25 );
 }
 use Test::NoWarnings;
 use File::Spec::Functions ':ALL';
@@ -185,6 +185,37 @@ SCOPE: {
 		}
 	);
 }
+
+# Regression test for get_functions
+SCOPE: {
+	my $editor = t::lib::Padre::Editor->new;
+	my $file   = catfile( $files, 'perl_functions.pl' );
+	my $doc    = Padre::Document->new(
+		filename => $file,
+	);
+	$doc->set_editor($editor);
+	$editor->configure_editor($doc);
+
+	my @functions = $doc->get_functions;
+	is_deeply(
+		\@functions,
+		[ qw{
+			guess_indentation_style
+			guess_filename
+			keywords
+			two_lines
+			three_lines
+		} ],
+		'Found expected Perl functions',
+	);
+}
+
+
+
+
+
+######################################################################
+# Support Functions
 
 sub find_var_simple {
 	my $doc     = shift;