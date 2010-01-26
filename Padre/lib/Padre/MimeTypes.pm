PadreIDE/Padre-total

commit 5667e84004a015d28c090a3c90899b33f3056503

Fixed uninitialized warnings when Padre starts up
git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@10290 18eddd70-d622-4293-bd05-fe65668e8bde

Padre/lib/Padre/MimeTypes.pm
@@ -711,7 +711,13 @@ sub is_perl6 {
 
 
 sub menu_view_mimes {
-	return map { $MIME_TYPES{$_}{name} => $_ } keys %MIME_TYPES;
+	my %menu_view_mimes = ();
+	for my $mime_type (keys %MIME_TYPES) {
+		if($MIME_TYPES{$mime_type}{name}) {
+			$menu_view_mimes{$mime_type}{name} = $mime_type;
+		}
+	}
+	return %menu_view_mimes;
 }
 
 1;

commit 252354f178679effdc28d8c44fbf15aaba3bcc75

Fixed a bug introduced by my r10290 fix. Sorry :)
git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@10297 18eddd70-d622-4293-bd05-fe65668e8bde

Padre/lib/Padre/MimeTypes.pm
@@ -713,8 +713,9 @@ sub is_perl6 {
 sub menu_view_mimes {
 	my %menu_view_mimes = ();
 	for my $mime_type ( keys %MIME_TYPES ) {
-		if ( $MIME_TYPES{$mime_type}{name} ) {
-			$menu_view_mimes{$mime_type}{name} = $mime_type;
+		my $mime_type_name = $MIME_TYPES{$mime_type}{name};
+		if ( $mime_type_name ) {
+			$menu_view_mimes{$mime_type_name} = $mime_type;
 		}
 	}
 	return %menu_view_mimes;