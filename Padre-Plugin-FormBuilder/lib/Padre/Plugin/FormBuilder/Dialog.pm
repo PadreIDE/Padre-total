d6740866aeaa633cb2709997f2b5fafc69eb1146
Adam Kennedy <adamk@cpan.org>
2010-07-03 09:01:58 +0000

    Adding dialog class, awesomely generated by itself :)
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@11753 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ Padre-Plugin-FormBuilder/lib/Padre/Plugin/FormBuilder/Dialog.pm
@@ -0,0 +1,97 @@
+package Padre::Plugin::FormBuilder::Dialog;
+
+use 5.008;
+use strict;
+use warnings;
+use Padre::Wx             ();
+use Padre::Wx::Role::Main ();
+
+our $VERSION = '0.01';
+our @ISA     = qw{
+	Padre::Wx::Role::Main
+	Wx::Dialog
+};
+
+sub new {
+	my $class  = shift;
+	my $parent = shift;
+
+	my $self = $class->SUPER::new(
+		$parent,
+		-1,
+		'',
+		Wx::wxDefaultPosition,
+		Wx::wxDefaultSize,
+		Wx::wxDEFAULT_DIALOG_STYLE,
+	);
+
+	my $file = Wx::StaticText->new(
+		$self,
+		-1,
+		'Importing: $file',
+	);
+
+	my $line1 = Wx::StaticLine->new(
+		$self,
+		-1,
+		Wx::wxDefaultPosition,
+		Wx::wxDefaultSize,
+	);
+
+	$self->{select} = Wx::Choice->new(
+		$self,
+		-1,
+		Wx::wxDefaultPosition,
+		Wx::wxDefaultSize,
+		[ ],
+	);
+
+	my $line2 = Wx::StaticLine->new(
+		$self,
+		-1,
+		Wx::wxDefaultPosition,
+		Wx::wxDefaultSize,
+	);
+
+	$self->{preview} = Wx::Button->new(
+		$self,
+		-1,
+		'Preview',
+	);
+
+	$self->{generate} = Wx::Button->new(
+		$self,
+		-1,
+		'Generate',
+	);
+
+	$self->{close} = Wx::Button->new(
+		$self,
+		-1,
+		'Close',
+	);
+
+	my $buttons = Wx::BoxSizer->new( Wx::wxHORIZONTAL );
+	$buttons->Add( $self->{preview}, 0, Wx::wxALL, 5 );
+	$buttons->Add( $self->{generate}, 0, Wx::wxBOTTOM | Wx::wxTOP, 5 );
+	$buttons->AddSpacer(50);
+	$buttons->Add( $self->{close}, 0, Wx::wxALL, 5 );
+
+	my $sizer2 = Wx::BoxSizer->new( Wx::wxVERTICAL );
+	$sizer2->Add( $file, 0, Wx::wxALL | Wx::wxEXPAND, 5 );
+	$sizer2->Add( $line1, 0, Wx::wxBOTTOM | Wx::wxEXPAND | Wx::wxTOP, 0 );
+	$sizer2->Add( $self->{select}, 0, Wx::wxALL | Wx::wxEXPAND, 5 );
+	$sizer2->AddSpacer(50);
+	$sizer2->Add( $line2, 0, Wx::wxEXPAND, 0 );
+	$sizer2->Add( $buttons, 0, Wx::wxEXPAND, 5 );
+
+	my $sizer1 = Wx::BoxSizer->new( Wx::wxHORIZONTAL );
+	$sizer1->Add( $sizer2, 1, Wx::wxEXPAND, 5 );
+
+	$self->SetSizer($sizer1);
+	$sizer1->SetSizeHints($self);
+
+	return $self;
+}
+
+1;
