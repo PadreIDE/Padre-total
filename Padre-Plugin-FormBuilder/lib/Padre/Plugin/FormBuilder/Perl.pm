f90b697f27cb022159a3e18ecae69391c122df1d
Adam Kennedy <adamk@cpan.org>
2010-07-12 06:13:07 +0000

    Bumping dependencies to get permission integration
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@11868 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-FormBuilder/lib/Padre/Plugin/FormBuilder/Perl.pm
+++ Padre-Plugin-FormBuilder/lib/Padre/Plugin/FormBuilder/Perl.pm
@@ -78,7 +78,7 @@ L<Padre>
 
 =head1 COPYRIGHT
 
-Copyright 2009 Adam Kennedy.
+Copyright 2010 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
