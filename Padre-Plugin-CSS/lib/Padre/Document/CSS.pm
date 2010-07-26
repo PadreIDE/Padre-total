c9f2ce93de6909b0030ff08816f3dcd337e93514
chorny <chorny@svngit.padre.perlide.org>
2010-07-26 06:49:09 +0000

    Autocompletition support
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@12008 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-CSS/lib/Padre/Document/CSS.pm
+++ Padre-Plugin-CSS/lib/Padre/Document/CSS.pm
@@ -45,4 +45,123 @@ sub find_help_topic {
 }
 
 
+sub event_on_char {
+	my ( $self, $editor, $event ) = @_;
+
+	my $main   = Padre->ide->wx->main;
+	my $config = Padre->ide->config;
+
+	$editor->Freeze;
+
+	$self->autocomplete_matching_char(
+		$editor, $event,
+		34  => 34,  # " "
+		39  => 39,  # ' '
+		40  => 41,  # ( )
+		60  => 62,  # < >
+		91  => 93,  # [ ]
+		123 => 125, # { }
+	);
+
+	$editor->Thaw;
+
+	$main->on_autocompletion($event) if $config->autocomplete_always;
+
+	return;
+}
+
+sub autocomplete {
+	my $self  = shift;
+	my $event = shift;
+
+	my $editor = $self->editor;
+	my $pos    = $editor->GetCurrentPos;
+	my $line   = $editor->LineFromPosition($pos);
+	my $first  = $editor->PositionFromLine($line);
+
+	# line from beginning to current position
+	my $prefix = $editor->GetTextRange( $first, $pos );
+	my $suffix = $editor->GetTextRange( $pos,   $pos + 15 );
+	$suffix = $1 if $suffix =~ /^(\w*)/; # Cut away any non-word chars
+
+	# The second parameter may be a reference to the current event or the next
+	# char which will be added to the editor:
+	my $nextchar;
+	if ( defined($event) and ( ref($event) eq 'Wx::KeyEvent' ) ) {
+		my $key = $event->GetUnicodeKey;
+		$nextchar = chr($key);
+	} elsif ( defined($event) and ( !ref($event) ) ) {
+		$nextchar = $event;
+	}
+
+	$prefix =~ s{^.*?((?:\w+-)*\w+)$}{$1};
+	my $last      = $editor->GetLength();
+	my $text      = $editor->GetTextRange( 0, $last );
+	my $pre_text  = $editor->GetTextRange( 0, $first + length($prefix) );
+	my $post_text = $editor->GetTextRange( $first, $last );
+
+	my $regex;
+	eval { $regex = qr{\b(\Q$prefix\E\w+(?:-\w+)*)\b} };
+	if ($@) {
+		return ("Cannot build regex for '$prefix'");
+	}
+	my @keywords=qw/background-repeat font-variant/;
+
+	my %seen;
+	my @words;
+	push @words, grep { $_ =~ $regex and !$seen{$_}++} @keywords;
+	push @words, grep { !$seen{$_}++ } reverse( $pre_text =~ /$regex/g );
+	push @words, grep { !$seen{$_}++ } ( $post_text =~ /$regex/g );
+
+	if ( @words > 20 ) {
+		@words = @words[ 0 .. 19 ];
+	}
+
+	# Suggesting the current word as the only solution doesn't help
+	# anything, but your need to close the suggestions window before
+	# you may press ENTER/RETURN.
+	if ( ( $#words == 0 ) and ( $prefix eq $words[0] ) ) {
+		return;
+	}
+
+	# While typing within a word, the rest of the word shouldn't be
+	# inserted.
+	if ( defined($suffix) ) {
+		for ( 0 .. $#words ) {
+			$words[$_] =~ s/\Q$suffix\E$//;
+		}
+	}
+
+	# This is the final result if there is no char which hasn't been
+	# saved to the editor buffer until now
+	return ( length($prefix), @words ) if !defined($nextchar);
+
+	# Finally cut out all words which do not match the next char
+	# which will be inserted into the editor (by the current event)
+	my @final_words;
+	for (@words) {
+
+		# Accept everything which has prefix + next char + at least one other char
+		next if !/^\Q$prefix$nextchar\E./;
+		push @final_words, $_;
+	}
+
+	return ( length($prefix), @final_words );
+}
+
+sub autoclean {
+	my $self = shift;
+
+	my $editor = $self->editor;
+	my $text   = $editor->GetText;
+
+	$text =~ s/[\s\t]+([\r\n]*?)$/$1/mg;
+	$text .= "\n" if $text !~ /\n$/;
+
+	$editor->SetText($text);
+
+	return 1;
+
+}
+
 1;
