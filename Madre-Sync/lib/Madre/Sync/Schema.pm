1a0b517929a4c03af66b415fbc52e58cd93529be
mattp <mattp@svngit.padre.perlide.org>
2010-07-30 16:10:39 +0000

    Schema::Loader to file. includes User email validation and config TimeStamp
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@12114 18eddd70-d622-4293-bd05-fe65668e8bde

--- Madre-Sync/lib/Madre/Sync/Schema.pm
+++ Madre-Sync/lib/Madre/Sync/Schema.pm
@@ -1,16 +1,21 @@
 package Madre::Sync::Schema;
 
-use Moose;
-use namespace::autoclean;
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces(
+    result_namespace => 'Result',
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.05003 @ 2010-07-30 10:32:47
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:YfCbPSAuG7foiBEsFpXb9g
 
 our $VERSION = '0.01';
-
-BEGIN {
-	extends 'DBIx::Class::Schema::Loader';
-}
-
-__PACKAGE__->naming( 'current' );
-
-__PACKAGE__->meta->make_immutable;
 
 1;
