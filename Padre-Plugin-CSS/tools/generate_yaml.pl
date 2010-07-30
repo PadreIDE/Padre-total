748c79abadc7edc8cc3232bcaa82061ced912cad
chorny <chorny@svngit.padre.perlide.org>
2010-07-30 11:37:12 +0000

    css property list generator
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@12108 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ Padre-Plugin-CSS/tools/generate_yaml.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+use 5.010;
+use strict;
+use warnings;
+use File::Slurp;
+
+my $html = read_file('properties.html');
+
+  use Web::Scraper;
+  my $scraper = scraper {
+      process "tr", "properties[]" => scraper {
+          process "//td[1]/a/span", "names[]" => 'TEXT'; #array
+          process "//td[2]", comment => 'TEXT';
+      };
+  };
+  my $res = $scraper->scrape(\$html);
+
+open my $out,'>','out.yml';
+foreach my $e (@{ $res->{properties} }) {
+  next unless keys %$e;
+  my @names=@{$e->{names}};
+  my $c=$e->{comment};
+  $c=~s/\Q || / /g;
+  $c=~s/\Q{1,4}/(REPLACE_four)/g;
+  foreach my $name (@names) {
+    if ($name=~/^'([^']+)'$/) {
+      $name=$1;
+      say $out "  $name: $c";
+    } else {
+      die "Bad name: $name";
+    }
+  }
+}
