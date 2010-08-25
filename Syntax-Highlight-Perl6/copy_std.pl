90e17061a20b88553df2647763e20cb5216f7515
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2010-08-25 08:34:16 +0000

    [S:H:P6] Updated STD.pm6 copying script
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@12359 18eddd70-d622-4293-bd05-fe65668e8bde

--- Syntax-Highlight-Perl6/copy_std.pl
+++ Syntax-Highlight-Perl6/copy_std.pl
@@ -1,34 +1,52 @@
 #!/usr/bin/perl
 
+#imports
 use strict;
 use warnings;
 use feature 'say';
 use Carp;
 use Cwd;
 
+# some constants
 my $PUGS = '../../pugs';
 my $STD = "$PUGS/src/perl6";
 my $status;
 
+# svn update to the latest
 say "\n--Running 'svn update'";
 $status = system("svn update $PUGS"); 
 die "Could not svn update pugs\n" if $status != 0;
 
+# make STD.pm6
 my $make = $^O eq 'MSWin32' ? 'dmake.exe' : 'make';
 say "\n--Running 'make clean all' for STD.pm6";
 my $cwd = getcwd;
 chdir $STD or die "Could not change dir to $STD\n";
-$status = system("$make clean all dist"); 
+$status = system("$make clean all"); 
 die "Could not make STD.pm6\n" if $status != 0;
 
-say 'Building and copying STD.pm6 files';
+# copy STD.pm6 files
+say "\n--Copying STD.pm6 files";
 chdir $cwd or die "Could not change dir to $cwd\n";
 
-$status = system("cp $STD/dist/lib/* lib");
-die "Could not copy dist/lib" if $status != 0;
+# list of files to copy
+my %files = (
+    "*.pmc" => "lib",
+    "Actions.pm" => "lib",
+    "LazyMap.pm" => "lib",
+    "mangle.pl"  => "lib",
+    "uniprops"   => "lib",
+    "syml/CORE.syml" => "lib/Syntax/Highlight/Perl6/syml",
+);
 
-$status = system("cp $STD/dist/lib6/* lib");
-die "Could not copy dist/lib" if $status != 0;
+# copy files
+for my $file (keys %files) {
+    my $dest = $files{$file};
+    my $src = "$STD/$file";
+    my $cmd = "perl -MExtUtils::Command -e cp $src $dest";
+    say $cmd;
+    my $status = system($cmd);
+    die "Could not copy $src" if $status != 0;
+}
 
-$status = system("cp $STD/dist/syml/* lib");
-die "Could not copy dist/syml" if $status != 0;
+say "\nThanks!\nPlease remember to '$make test' and then commit changed files if it works";
