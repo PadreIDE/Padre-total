2df3dedb16f3d62837f3af48bb99d9e21a478658
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2010-10-24 12:03:45 +0000

    [Perl6-Refactor] Removed pod clutter that dzil can auto-generate.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@12791 18eddd70-d622-4293-bd05-fe65668e8bde

--- Perl6-Refactor/lib/Perl6/Refactor.pm
+++ Perl6-Refactor/lib/Perl6/Refactor.pm
@@ -2,7 +2,7 @@ package Perl6::Refactor;
 
 use Moose;
 
-our $VERSION = '0.02';
+# ABSTRACT: Perl6::Refactor - Refactors Perl 6 code
 
 sub rename_variable {
 	my $self = shift;
@@ -21,10 +21,6 @@ sub find_variable_declaration {
 
 __END__
 
-=head1 NAME
-
-Perl6::Refactor - Refactors Perl 6 code
-
 =head1 SYNOPSIS
 
 Perl 6 Refactor includes tools for renaming variables, finding variables 
@@ -35,55 +31,3 @@ Perhaps a little code snippet.
     use Perl6::Refactor;
 
     my $foo = Perl6::Refactor->new();
-
-=head1 METHODS
-
-=head2 rename_variable
-
-=head2 find_variable_declaration
-
-=head1 AUTHOR
-
-Ahmad M. Zawawi, C<< <ahmad.zawawi at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-perl6-refactor at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Perl6-Refactor>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Perl6::Refactor
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Perl6-Refactor>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Perl6-Refactor>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Perl6-Refactor>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Perl6-Refactor/>
-
-=back
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright (C) 2009 Ahmad M. Zawawi C<< <ahmad.zawawi at gmail.com> >>
-
-This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
