96c4570ae79dff04d265212594ae15bdea7c2080
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2010-10-27 12:38:17 +0000

    [tools] Added initial version of check_translations.pl
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@12903 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ tools/check_translations.pl
@@ -0,0 +1,24 @@
+use strict;
+use warnings;
+
+__END__
+
+=head1 NAME
+
+check_translations - Checks Padre plugin translations for problems
+
+=head1 DESCRIPTION
+
+Checks Padre plugin translations for the following problems at the moment:
+
+
+=head1 AUTHOR
+
+Ahmad M. Zawawi C<< <ahmad.zawawi at gmail.com> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 C<< <ahmad.zawawi at gmail.com> >>
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl 5 itself.
