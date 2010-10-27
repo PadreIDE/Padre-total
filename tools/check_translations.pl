d91a880ce4fe732ee0eb285d4ec5894d8400ea66
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2010-10-27 13:10:39 +0000

    [tools] Perl tidy. Translation checks are working now. Updated POD
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@12904 18eddd70-d622-4293-bd05-fe65668e8bde

--- tools/check_translations.pl
+++ tools/check_translations.pl
@@ -1,6 +1,38 @@
 use strict;
 use warnings;
 
+use File::Spec ();
+
+foreach my $plugin_dir ( glob("Padre-Plugin-*") ) {
+    my $locale = File::Spec->catdir( $plugin_dir, 'share', 'locale' );
+    if ( -d $locale ) {
+        ( my $plugin_name = $plugin_dir ) =~ s/Padre-Plugin-(\w+)/$1/;
+        foreach my $pofile ( glob( File::Spec->catfile( $locale, '*.po' ) ) ) {
+            if ( $pofile =~ /Padre__Plugin__$plugin_name-(.+?)\.po/ ) {
+                if ( open( my $fh, $pofile ) ) {
+                    while ( my $line = <$fh> ) {
+                        if ( $line =~ /^"Project-Id-Version:\s+(.+?)\\n"/ ) {
+                            my $project_id_version = $1;
+                            unless ( $project_id_version =~ /$plugin_dir/i ) {
+                                print
+                                  "Project-Id-Version Mismatch at $pofile\n";
+                            }
+                            last;
+                        }
+                    }
+                    close $fh;
+                }
+                else {
+                    print "Could not open $pofile for reading\n";
+                }
+            }
+            else {
+                print "Found mismatched filename $pofile\n";
+            }
+        }
+    }
+}
+
 __END__
 
 =head1 NAME
@@ -9,8 +41,15 @@ check_translations - Checks Padre plugin translations for problems
 
 =head1 DESCRIPTION
 
-Checks Padre plugin translations for the following problems at the moment:
+Checks Padre plugin translations for the following:
 
+=over 4
+
+=item * A PO file must have the format Padre__Plugin__PluginName.po
+
+=item * "Project-Id-Version" should be the same as the Plugin project dir
+
+=back
 
 =head1 AUTHOR
 
