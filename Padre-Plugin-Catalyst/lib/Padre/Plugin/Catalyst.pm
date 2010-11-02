e9c5483c80128ae6c1c7d8781bb7de93c667d86e
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2010-11-02 08:43:17 +0000

    [Catalyst] Bumped Padre plug-in API compatibility to 0.47
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@12993 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Catalyst/lib/Padre/Plugin/Catalyst.pm
+++ Padre-Plugin-Catalyst/lib/Padre/Plugin/Catalyst.pm
@@ -14,11 +14,7 @@ sub plugin_name {'Catalyst'}
 
 # Declare the Padre interfaces this plugin uses
 sub padre_interfaces {
-	'Padre::Plugin' => 0.43,
-
-		#    'Padre::Document::Perl' => 0.16,
-		#    'Padre::Wx::Main'       => 0.16,
-		#    'Padre::DB'             => 0.16,
+	'Padre::Plugin' => 0.47,;
 }
 
 sub plugin_icon {
