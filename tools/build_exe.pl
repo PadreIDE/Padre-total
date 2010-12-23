c2f0dad74a581beb1a8a55c38390deacb328f685
Gabor Szabo <szabgab@gmail.com>
2010-12-23 16:13:41 +0000

    include every dll for wx and the Clone module - the executable seems to work now
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@13435 18eddd70-d622-4293-bd05-fe65668e8bde

--- tools/build_exe.pl
+++ tools/build_exe.pl
@@ -14,21 +14,31 @@ chdir $dir;
 
 unlink $exe_file if -e $exe_file;
 
-my @dlls = qw(
-	wxbase28u_gcc_custom.dll 
-	wxmsw28u_adv_gcc_custom.dll
-	wxmsw28u_core_gcc_custom.dll
-	mingwm10.dll
-);
+# my @dlls = qw(
+	# wxbase28u_gcc_custom.dll 
+	# wxmsw28u_stc_gcc_custom.dll
+	# wxmsw28u_adv_gcc_custom.dll
+	# wxmsw28u_core_gcc_custom.dll
+	# mingwm10.dll
+# );
+# Here is a list of dlls we need to include http://www.wxperl.co.uk/packaging/mswdepends.html
+# but for now, let's just include everything
+my $path = "c:/strawberry/perl/vendor/lib/Alien/wxWidgets/msw_2_8_10_uni_gcc_3_4/lib";
+my @dlls = glob "$path/*.*";
+
+my @modules = qw(Clone);
 
 my $cmd = "pp -o $exe_file script/padre";
 foreach my $dll (@dlls) {
-	$cmd .= " -l c:/strawberry/perl/vendor/lib/Alien/wxWidgets/msw_2_8_10_uni_gcc_3_4/lib/$dll";
+	$cmd .= " -l $dll";
+}
+foreach my $m (@modules) {
+	$cmd .= " -M $m";
 }
 print "$cmd\n";
 system $cmd;
 print "============================= DONE ===============\n";
 print "Lunching $exe_file\n";
-system $exe_file;
+#system $exe_file;
 
 
