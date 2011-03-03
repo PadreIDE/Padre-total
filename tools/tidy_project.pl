9c1462f002160b04924f7f6f04cd81d24f076a08
Gabor Szabo <szabgab@gmail.com>
2011-03-03 10:59:55 +0000

    tidy the tidy script
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@13891 18eddd70-d622-4293-bd05-fe65668e8bde

--- tools/tidy_project.pl
+++ tools/tidy_project.pl
@@ -4,19 +4,20 @@ use strict;
 use warnings;
 
 eval("require Perl::Tidy");
-if($@) {
+if ($@) {
 	die "Please install Perl::Tidy (e.g. cpan Perl::Tidy)";
 }
 
 my $ver = '20101217';
-if ($Perl::Tidy::VERSION ne $ver) {
+if ( $Perl::Tidy::VERSION ne $ver ) {
 	die "Please install version $ver of Perl::Tidy";
+
 	# Make sure everyone uses the exact same version!
 }
 
 
 #
-use Cwd                   qw{ cwd };
+use Cwd qw{ cwd };
 use File::Spec::Functions qw{ catfile catdir };
 use File::Find::Rule;
 use FindBin qw{ $Bin };
@@ -27,28 +28,29 @@ die "cannot find perltidy configuration file: $perltidyrc\n"
 	unless -e $perltidyrc;
 
 # build list of perl files to reformat
-my @pmfiles = @ARGV
+my @pmfiles =
+	  @ARGV
 	? @ARGV
-	: grep {/^lib/}	File::Find::Rule->file->name("*.pm")->relative->in(cwd);
-my @tfiles = @ARGV
+	: grep {/^lib/} File::Find::Rule->file->name("*.pm")->relative->in(cwd);
+my @tfiles =
+	  @ARGV
 	? @ARGV
-	: grep {/^x?t/}	File::Find::Rule->file->name("*.t")->relative->in(cwd);
-my @examples  = @ARGV
+	: grep {/^x?t/} File::Find::Rule->file->name("*.t")->relative->in(cwd);
+my @examples =
+	  @ARGV
 	? @ARGV
 	: grep {/^share.examples/} File::Find::Rule->file->name("*.pl")->relative->in(cwd);
 
-my @files = (@pmfiles,@tfiles, @examples);
+my @files = ( @pmfiles, @tfiles, @examples );
 
-my @extras = ('Makefile.PL', 'Build.PL', 'dev.pl', 'script/padre',);
+my @extras = ( 'Makefile.PL', 'Build.PL', 'dev.pl', 'script/padre', );
 for my $extra (@extras) {
 	push @files, $extra if -f $extra;
 }
 
 # formatting documents
-eval {
-	Perl::Tidy::perltidy(argv => "--backup-and-modify-in-place --profile=$perltidyrc @files");
-};
-if($@) {
+eval { Perl::Tidy::perltidy( argv => "--backup-and-modify-in-place --profile=$perltidyrc @files" ); };
+if ($@) {
 	print "Perl::Tidy failed with the following error:\n$@\n";
 }
 
