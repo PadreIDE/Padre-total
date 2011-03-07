cb2e6fb3da050fc0c4c9fa6284d00e48a3fc2571
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2011-03-07 12:29:39 +0000

    Added an empty Wx::Scintilla project.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@13925 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ Wx-Scintilla/lib/Wx/Scintilla.pm
@@ -0,0 +1,72 @@
+package Wx::Scintilla;
+
+# ABSTRACT: wxWidgets Scintilla library
+
+use warnings;
+use strict;
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Wx::Scintilla;
+
+    my $stc = Wx::Scintilla->new();
+
+=head1 SUBROUTINES/METHODS
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-wx-scintilla at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Wx-Scintilla>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Wx::Scintilla
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Wx-Scintilla>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Wx-Scintilla>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Wx-Scintilla>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Wx-Scintilla/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Ahmad M. Zawawi.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Wx::Scintilla
