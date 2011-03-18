87fae886af295b2993c76ef997f896993010fede
Gabor Szabo <szabgab@gmail.com>
2011-03-18 13:00:22 +0000

    Hyppolit: fix stupid regex bug
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@14110 18eddd70-d622-4293-bd05-fe65668e8bde

--- Hyppolit/lib/Hyppolit.pm
+++ Hyppolit/lib/Hyppolit.pm
@@ -174,7 +174,7 @@ sub irc_public {
 		} else {
 			#$irc->yield(privmsg => $channel, "I don't know what $1 is");
 		}
-	} elsif ($text =~ /$\s*op\s*me\s*$/) {
+	} elsif ($text =~ /^\s*op\s*me\s*$/) {
 		if ($config->{trusted}{$nick}) {
 			set_op($irc, $channel, $nick);
 		} else {
