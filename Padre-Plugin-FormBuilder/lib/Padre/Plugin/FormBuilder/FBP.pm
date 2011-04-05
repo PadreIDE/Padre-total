d0a86cf414f8d42a0d94e394ffa6a2ca825e8d56
Adam Kennedy <adamk@cpan.org>
2011-04-05 23:11:44 +0000

    Updating dialog design
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@14213 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-FormBuilder/lib/Padre/Plugin/FormBuilder/FBP.pm
+++ Padre-Plugin-FormBuilder/lib/Padre/Plugin/FormBuilder/FBP.pm
@@ -32,35 +32,24 @@ sub new {
 	$self->{file} = Wx::StaticText->new(
 		$self,
 		-1,
-		Wx::gettext("Importing:"),
+		Wx::gettext("Dialog Source"),
 	);
-
-	$self->{line1} = Wx::StaticLine->new(
-		$self,
-		-1,
-		Wx::wxDefaultPosition,
-		Wx::wxDefaultSize,
-		Wx::wxLI_HORIZONTAL,
-	);
-
-	my $m_staticText3 = Wx::StaticText->new(
-		$self,
-		-1,
-		Wx::gettext("Common Settings"),
-	);
-	$m_staticText3->SetFont(
+	$self->{file}->SetFont(
 		Wx::Font->new( Wx::wxNORMAL_FONT->GetPointSize, 70, 90, 92, 0, "" )
 	);
 
-	$self->{padre} = Wx::CheckBox->new(
+	$self->{browse} = Wx::FilePickerCtrl->new(
 		$self,
 		-1,
-		Wx::gettext("Generate dialog code for use in Padre or a Padre plugin"),
+		"",
+		"Select a file",
+		"*.*",
 		Wx::wxDefaultPosition,
 		Wx::wxDefaultSize,
+		Wx::wxFLP_DEFAULT_STYLE,
 	);
 
-	$self->{m_staticline5} = Wx::StaticLine->new(
+	$self->{line1} = Wx::StaticLine->new(
 		$self,
 		-1,
 		Wx::wxDefaultPosition,
@@ -111,14 +100,6 @@ sub new {
 		Wx::gettext("Generates embedded tracking data in the dialog code")
 	);
 
-	$self->{m_staticline4} = Wx::StaticLine->new(
-		$self,
-		-1,
-		Wx::wxDefaultPosition,
-		Wx::wxDefaultSize,
-		Wx::wxLI_HORIZONTAL,
-	);
-
 	$self->{generate} = Wx::Button->new(
 		$self,
 		-1,
@@ -133,30 +114,69 @@ sub new {
 		},
 	);
 
+	$self->{line2} = Wx::StaticLine->new(
+		$self,
+		-1,
+		Wx::wxDefaultPosition,
+		Wx::wxDefaultSize,
+		Wx::wxLI_HORIZONTAL,
+	);
+
+	my $m_staticText3 = Wx::StaticText->new(
+		$self,
+		-1,
+		Wx::gettext("Common Settings"),
+	);
+	$m_staticText3->SetFont(
+		Wx::Font->new( Wx::wxNORMAL_FONT->GetPointSize, 70, 90, 92, 0, "" )
+	);
+
+	$self->{padre} = Wx::CheckBox->new(
+		$self,
+		-1,
+		Wx::gettext("Generate dialog code for use in Padre or a Padre plugin"),
+		Wx::wxDefaultPosition,
+		Wx::wxDefaultSize,
+	);
+
+	$self->{m_staticline4} = Wx::StaticLine->new(
+		$self,
+		-1,
+		Wx::wxDefaultPosition,
+		Wx::wxDefaultSize,
+		Wx::wxLI_HORIZONTAL,
+	);
+
 	$self->{cancel} = Wx::Button->new(
 		$self,
 		Wx::wxID_CANCEL,
-		Wx::gettext("Cancel"),
+		Wx::gettext("Close"),
 	);
+
+	my $bSizer6 = Wx::BoxSizer->new(Wx::wxHORIZONTAL);
+	$bSizer6->Add( $self->{browse}, 1, Wx::wxALL | Wx::wxEXPAND, 5 );
 
 	my $bSizer5 = Wx::BoxSizer->new(Wx::wxHORIZONTAL);
 	$bSizer5->Add( $self->{select}, 1, Wx::wxALIGN_CENTER_VERTICAL | Wx::wxALL | Wx::wxEXPAND, 5 );
 	$bSizer5->Add( $self->{preview}, 0, Wx::wxALIGN_CENTER_VERTICAL | Wx::wxALL, 5 );
 
 	my $buttons = Wx::BoxSizer->new(Wx::wxHORIZONTAL);
-	$buttons->Add( $self->{generate}, 0, Wx::wxALL, 5 );
 	$buttons->Add( 50, 0, 1, Wx::wxEXPAND, 5 );
 	$buttons->Add( $self->{cancel}, 0, Wx::wxALL, 5 );
 
 	my $sizer2 = Wx::BoxSizer->new(Wx::wxVERTICAL);
-	$sizer2->Add( $self->{file}, 0, Wx::wxALL | Wx::wxEXPAND, 5 );
+	$sizer2->Add( $self->{file}, 0, Wx::wxALIGN_CENTER_VERTICAL | Wx::wxALL, 5 );
+	$sizer2->Add( $bSizer6, 1, Wx::wxEXPAND, 5 );
+	$sizer2->Add( 0, 5, 1, Wx::wxEXPAND, 5 );
 	$sizer2->Add( $self->{line1}, 0, Wx::wxBOTTOM | Wx::wxEXPAND | Wx::wxTOP, 0 );
-	$sizer2->Add( $m_staticText3, 0, Wx::wxALL | Wx::wxEXPAND, 5 );
-	$sizer2->Add( $self->{padre}, 0, Wx::wxALL, 5 );
-	$sizer2->Add( $self->{m_staticline5}, 0, Wx::wxBOTTOM | Wx::wxEXPAND | Wx::wxTOP, 5 );
 	$sizer2->Add( $self->{m_staticText4}, 0, Wx::wxALL | Wx::wxEXPAND, 5 );
 	$sizer2->Add( $bSizer5, 0, Wx::wxEXPAND, 0 );
 	$sizer2->Add( $self->{associate}, 0, Wx::wxALL, 5 );
+	$sizer2->Add( $self->{generate}, 0, Wx::wxALL, 5 );
+	$sizer2->Add( 0, 5, 1, Wx::wxEXPAND, 5 );
+	$sizer2->Add( $self->{line2}, 0, Wx::wxBOTTOM | Wx::wxEXPAND | Wx::wxTOP, 5 );
+	$sizer2->Add( $m_staticText3, 0, Wx::wxALL | Wx::wxEXPAND, 5 );
+	$sizer2->Add( $self->{padre}, 0, Wx::wxALL, 5 );
 	$sizer2->Add( 0, 5, 1, Wx::wxEXPAND, 5 );
 	$sizer2->Add( $self->{m_staticline4}, 0, Wx::wxEXPAND, 5 );
 	$sizer2->Add( $buttons, 0, Wx::wxEXPAND, 5 );
