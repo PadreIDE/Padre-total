2feb8629822d477fd355c4523e6963f0ded9ec5a
Sebastian Willing <sewi@cpan.org>
2011-06-18 15:03:08 +0000

    Oops, forgot to tidy up :-)
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@14652 18eddd70-d622-4293-bd05-fe65668e8bde

--- tools/padre_paste.pl
+++ tools/padre_paste.pl
@@ -4,25 +4,30 @@ use LWP::UserAgent;
 
 my $ua = LWP::UserAgent->new;
 
-if ($#ARGV < 1) {
- print "Syntax: $0 <nick> <subject of paste>\n";
- exit 1;
+if ( $#ARGV < 1 ) {
+	print "Syntax: $0 <nick> <subject of paste>\n";
+	exit 1;
 }
 
 my %data = (
-channel => '#padre',
-nick => shift(@ARGV),
-summary => join(' ',@ARGV),
-paste => join('',<STDIN>),
+	channel => '#padre',
+	nick    => shift(@ARGV),
+	summary => join( ' ', @ARGV ),
+	paste   => join( '', <STDIN> ),
 );
 
-my $req = HTTP::Request->new(POST => 'http://paste.scsys.co.uk/paste');
+my $req = HTTP::Request->new( POST => 'http://paste.scsys.co.uk/paste' );
 $req->content_type('application/x-www-form-urlencoded');
-$req->content(join('&',map {
-  my $Val = $data{$_};
-  $Val =~ s/(\W)/"%".uc(unpack("H*",$1))/ge;
-  $Val =~ s/\%20/\+/g;
-  $_.'='.$Val;
- } (keys(%data))));
+$req->content(
+	join(
+		'&',
+		map {
+			my $Val = $data{$_};
+			$Val =~ s/(\W)/"%".uc(unpack("H*",$1))/ge;
+			$Val =~ s/\%20/\+/g;
+			$_ . '=' . $Val;
+			} ( keys(%data) )
+	)
+);
 
 my $result = $ua->request($req);
