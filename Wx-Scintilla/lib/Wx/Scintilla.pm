a86dbe997f9ae13005570a4de45782e62d9dbd81
markd <markd@svngit.padre.perlide.org>
2011-06-20 23:02:42 +0000

    Removed File::ShareDir lookup as scintilla DLL now side by side with XS DLL
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@14697 18eddd70-d622-4293-bd05-fe65668e8bde

--- Wx-Scintilla/lib/Wx/Scintilla.pm
+++ Wx-Scintilla/lib/Wx/Scintilla.pm
@@ -7,26 +7,9 @@ use Wx;
 
 our $VERSION = '0.14';
 
-# Add Wx::Scintilla distribution directory to PATH on windows so that Wx can load it
-my $wx_scintilla_path;
-if ( $ENV{HARNESS_ACTIVE} ) {
-	$wx_scintilla_path = './blib/arch/auto/Wx/Scintilla/';
-} else {
-	eval { require File::ShareDir; $wx_scintilla_path = File::ShareDir::dist_dir('Wx-Scintilla'); 1; };
-	if ($@) {
-
-		# fallback
-		$wx_scintilla_path = './blib/arch/auto/Wx/Scintilla/';
-	}
-}
-$ENV{PATH} = $wx_scintilla_path . ';' . $ENV{PATH} if ( $^O eq 'MSWin32' );
-
 # Define Perl 6 lexer
 use constant wxSCINTILLA_LEX_PERL6 => 98;
 
-# Load scintilla's DLL through Wx and ask XSLoader to load it
-# NOTE: Do not use Wx::boot since it is buggy and causes test suite to fail while upgrading
-Wx::load_dll('scintilla');
 require XSLoader;
 XSLoader::load 'Wx::Scintilla', $VERSION;
 
