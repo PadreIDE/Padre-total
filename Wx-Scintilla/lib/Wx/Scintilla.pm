6ea960f48b2d774585b3b5fd0131e099ca527f38
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2011-06-20 06:45:27 +0000

    POD Updates
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@14671 18eddd70-d622-4293-bd05-fe65668e8bde

--- Wx-Scintilla/lib/Wx/Scintilla.pm
+++ Wx-Scintilla/lib/Wx/Scintilla.pm
@@ -296,7 +296,4 @@ Copyright 2011 Ahmad M. Zawawi.
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
 
-The full text of the license can be found in the
-LICENSE file included with this module.
-
 =cut
