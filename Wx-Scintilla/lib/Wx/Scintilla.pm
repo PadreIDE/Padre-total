05e30f38544076bd0ad8b337cffc7426d6593bc0
markd <markd@svngit.padre.perlide.org>
2011-06-28 16:36:27 +0000

    Wx::Scintilla - note cannot co-exist with Wx::STC
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@14759 18eddd70-d622-4293-bd05-fe65668e8bde

--- Wx-Scintilla/lib/Wx/Scintilla.pm
+++ Wx-Scintilla/lib/Wx/Scintilla.pm
@@ -1,14 +1,19 @@
 package Wx::Scintilla;
 
-
 use strict;
 use warnings;
 use Wx;
+use Carp;
 
 our $VERSION = '0.17';
 
 # Define Perl 6 lexer
 use constant wxSCINTILLA_LEX_PERL6 => 98;
+
+# check for loaded Wx::STC
+if(exists($INC{'Wx/STC.pm'})) {
+	croak('Wx::Scintilla cannot be loaded alongside Wx::STC. Choose one and only one of the modules. ');
+}
 
 require XSLoader;
 XSLoader::load 'Wx::Scintilla', $VERSION;
@@ -30,7 +35,7 @@ our $VERSION = '0.17';
 #Defeat the indexer by splitting the package declaration on two lines
 #<<<  do not let perltidy touch this
 package
-	Wx::Event;
+    Wx::Event;
 # Don't set global Wx::Event version
 #>>>
 
@@ -38,35 +43,35 @@ use strict;
 
 {
 
-	# Disable Wx::EVT_STC_* event warning redefinition
-	no warnings 'redefine';
+    # Disable Wx::EVT_STC_* event warning redefinition
+    no warnings 'redefine';
 
-	sub EVT_STC_CHANGE($$$)            { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_CHANGE,            $_[2] ) }
-	sub EVT_STC_STYLENEEDED($$$)       { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_STYLENEEDED,       $_[2] ) }
-	sub EVT_STC_CHARADDED($$$)         { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_CHARADDED,         $_[2] ) }
-	sub EVT_STC_SAVEPOINTREACHED($$$)  { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_SAVEPOINTREACHED,  $_[2] ) }
-	sub EVT_STC_SAVEPOINTLEFT($$$)     { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_SAVEPOINTLEFT,     $_[2] ) }
-	sub EVT_STC_ROMODIFYATTEMPT($$$)   { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_ROMODIFYATTEMPT,   $_[2] ) }
-	sub EVT_STC_KEY($$$)               { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_KEY,               $_[2] ) }
-	sub EVT_STC_DOUBLECLICK($$$)       { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_DOUBLECLICK,       $_[2] ) }
-	sub EVT_STC_UPDATEUI($$$)          { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_UPDATEUI,          $_[2] ) }
-	sub EVT_STC_MODIFIED($$$)          { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_MODIFIED,          $_[2] ) }
-	sub EVT_STC_MACRORECORD($$$)       { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_MACRORECORD,       $_[2] ) }
-	sub EVT_STC_MARGINCLICK($$$)       { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_MARGINCLICK,       $_[2] ) }
-	sub EVT_STC_NEEDSHOWN($$$)         { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_NEEDSHOWN,         $_[2] ) }
-	sub EVT_STC_POSCHANGED($$$)        { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_POSCHANGED,        $_[2] ) }
-	sub EVT_STC_PAINTED($$$)           { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_PAINTED,           $_[2] ) }
-	sub EVT_STC_USERLISTSELECTION($$$) { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_USERLISTSELECTION, $_[2] ) }
-	sub EVT_STC_URIDROPPED($$$)        { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_URIDROPPED,        $_[2] ) }
-	sub EVT_STC_DWELLSTART($$$)        { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_DWELLSTART,        $_[2] ) }
-	sub EVT_STC_DWELLEND($$$)          { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_DWELLEND,          $_[2] ) }
-	sub EVT_STC_START_DRAG($$$)        { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_START_DRAG,        $_[2] ) }
-	sub EVT_STC_DRAG_OVER($$$)         { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_DRAG_OVER,         $_[2] ) }
-	sub EVT_STC_DO_DROP($$$)           { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_DO_DROP,           $_[2] ) }
-	sub EVT_STC_ZOOM($$$)              { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_ZOOM,              $_[2] ) }
-	sub EVT_STC_HOTSPOT_CLICK($$$)     { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_HOTSPOT_CLICK,     $_[2] ) }
-	sub EVT_STC_HOTSPOT_DCLICK($$$)    { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_HOTSPOT_DCLICK,    $_[2] ) }
-	sub EVT_STC_CALLTIP_CLICK($$$)     { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_CALLTIP_CLICK,     $_[2] ) }
+    sub EVT_STC_CHANGE($$$)            { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_CHANGE,            $_[2] ) }
+    sub EVT_STC_STYLENEEDED($$$)       { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_STYLENEEDED,       $_[2] ) }
+    sub EVT_STC_CHARADDED($$$)         { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_CHARADDED,         $_[2] ) }
+    sub EVT_STC_SAVEPOINTREACHED($$$)  { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_SAVEPOINTREACHED,  $_[2] ) }
+    sub EVT_STC_SAVEPOINTLEFT($$$)     { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_SAVEPOINTLEFT,     $_[2] ) }
+    sub EVT_STC_ROMODIFYATTEMPT($$$)   { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_ROMODIFYATTEMPT,   $_[2] ) }
+    sub EVT_STC_KEY($$$)               { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_KEY,               $_[2] ) }
+    sub EVT_STC_DOUBLECLICK($$$)       { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_DOUBLECLICK,       $_[2] ) }
+    sub EVT_STC_UPDATEUI($$$)          { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_UPDATEUI,          $_[2] ) }
+    sub EVT_STC_MODIFIED($$$)          { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_MODIFIED,          $_[2] ) }
+    sub EVT_STC_MACRORECORD($$$)       { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_MACRORECORD,       $_[2] ) }
+    sub EVT_STC_MARGINCLICK($$$)       { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_MARGINCLICK,       $_[2] ) }
+    sub EVT_STC_NEEDSHOWN($$$)         { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_NEEDSHOWN,         $_[2] ) }
+    sub EVT_STC_POSCHANGED($$$)        { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_POSCHANGED,        $_[2] ) }
+    sub EVT_STC_PAINTED($$$)           { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_PAINTED,           $_[2] ) }
+    sub EVT_STC_USERLISTSELECTION($$$) { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_USERLISTSELECTION, $_[2] ) }
+    sub EVT_STC_URIDROPPED($$$)        { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_URIDROPPED,        $_[2] ) }
+    sub EVT_STC_DWELLSTART($$$)        { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_DWELLSTART,        $_[2] ) }
+    sub EVT_STC_DWELLEND($$$)          { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_DWELLEND,          $_[2] ) }
+    sub EVT_STC_START_DRAG($$$)        { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_START_DRAG,        $_[2] ) }
+    sub EVT_STC_DRAG_OVER($$$)         { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_DRAG_OVER,         $_[2] ) }
+    sub EVT_STC_DO_DROP($$$)           { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_DO_DROP,           $_[2] ) }
+    sub EVT_STC_ZOOM($$$)              { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_ZOOM,              $_[2] ) }
+    sub EVT_STC_HOTSPOT_CLICK($$$)     { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_HOTSPOT_CLICK,     $_[2] ) }
+    sub EVT_STC_HOTSPOT_DCLICK($$$)    { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_HOTSPOT_DCLICK,    $_[2] ) }
+    sub EVT_STC_CALLTIP_CLICK($$$)     { $_[0]->Connect( $_[1], -1, &Wx::wxEVT_STC_CALLTIP_CLICK,     $_[2] ) }
 }
 
 1; # The end of Wx::Scintilla? :)
@@ -81,88 +86,88 @@ Wx::Scintilla - wxWidgets Perl XS wrapper for the Scintilla editor component
 
 =head1 SYNOPSIS
 
-	#----> My first scintilla Wx editor :)
-	package My::Scintilla::Editor;
+    #----> My first scintilla Wx editor :)
+    package My::Scintilla::Editor;
 
-	use strict;
-	use warnings;
+    use strict;
+    use warnings;
 
-	# Load Wx::Scintilla
-	use Wx::Scintilla ();    # replaces use Wx::STC
-	use base 'Wx::ScintillaTextCtrl';    # replaces Wx::StyledTextCtrl
+    # Load Wx::Scintilla
+    use Wx::Scintilla ();    # replaces use Wx::STC
+    use base 'Wx::ScintillaTextCtrl';    # replaces Wx::StyledTextCtrl
 
-	use Wx qw(:everything);
-	use Wx::Event;
+    use Wx qw(:everything);
+    use Wx::Event;
 
-	# Override the constructor to Enable Perl support in the editor
-	sub new {
-		my ( $class, $parent ) = @_;
-		my $self = $class->SUPER::new( $parent, -1, [ -1, -1 ], [ 750, 700 ] );
+    # Override the constructor to Enable Perl support in the editor
+    sub new {
+        my ( $class, $parent ) = @_;
+        my $self = $class->SUPER::new( $parent, -1, [ -1, -1 ], [ 750, 700 ] );
 
-		# Set the font
-		my $font = Wx::Font->new( 10, wxTELETYPE, wxNORMAL, wxNORMAL );
-		$self->SetFont($font);
-		$self->StyleSetFont( wxSTC_STYLE_DEFAULT, $font );
-		$self->StyleClearAll();
+        # Set the font
+        my $font = Wx::Font->new( 10, wxTELETYPE, wxNORMAL, wxNORMAL );
+        $self->SetFont($font);
+        $self->StyleSetFont( wxSTC_STYLE_DEFAULT, $font );
+        $self->StyleClearAll();
 
-		# Set the various Perl lexer colors
-		$self->StyleSetForeground( 0,  Wx::Colour->new( 0x00, 0x00, 0x7f ) );
-		$self->StyleSetForeground( 1,  Wx::Colour->new( 0xff, 0x00, 0x00 ) );
-		$self->StyleSetForeground( 2,  Wx::Colour->new( 0x00, 0x7f, 0x00 ) );
-		$self->StyleSetForeground( 3,  Wx::Colour->new( 0x7f, 0x7f, 0x7f ) );
-		$self->StyleSetForeground( 4,  Wx::Colour->new( 0x00, 0x7f, 0x7f ) );
-		$self->StyleSetForeground( 5,  Wx::Colour->new( 0x00, 0x00, 0x7f ) );
-		$self->StyleSetForeground( 6,  Wx::Colour->new( 0xff, 0x7f, 0x00 ) );
-		$self->StyleSetForeground( 7,  Wx::Colour->new( 0x7f, 0x00, 0x7f ) );
-		$self->StyleSetForeground( 8,  Wx::Colour->new( 0x00, 0x00, 0x00 ) );
-		$self->StyleSetForeground( 9,  Wx::Colour->new( 0x7f, 0x7f, 0x7f ) );
-		$self->StyleSetForeground( 10, Wx::Colour->new( 0x00, 0x00, 0x7f ) );
-		$self->StyleSetForeground( 11, Wx::Colour->new( 0x00, 0x00, 0xff ) );
-		$self->StyleSetForeground( 12, Wx::Colour->new( 0x7f, 0x00, 0x7f ) );
-		$self->StyleSetForeground( 13, Wx::Colour->new( 0x40, 0x80, 0xff ) );
-		$self->StyleSetForeground( 17, Wx::Colour->new( 0xff, 0x00, 0x7f ) );
-		$self->StyleSetForeground( 18, Wx::Colour->new( 0x7f, 0x7f, 0x00 ) );
-		$self->StyleSetBold( 12, 1 );
-		$self->StyleSetSpec( wxSTC_H_TAG, "fore:#0000ff" );
+        # Set the various Perl lexer colors
+        $self->StyleSetForeground( 0,  Wx::Colour->new( 0x00, 0x00, 0x7f ) );
+        $self->StyleSetForeground( 1,  Wx::Colour->new( 0xff, 0x00, 0x00 ) );
+        $self->StyleSetForeground( 2,  Wx::Colour->new( 0x00, 0x7f, 0x00 ) );
+        $self->StyleSetForeground( 3,  Wx::Colour->new( 0x7f, 0x7f, 0x7f ) );
+        $self->StyleSetForeground( 4,  Wx::Colour->new( 0x00, 0x7f, 0x7f ) );
+        $self->StyleSetForeground( 5,  Wx::Colour->new( 0x00, 0x00, 0x7f ) );
+        $self->StyleSetForeground( 6,  Wx::Colour->new( 0xff, 0x7f, 0x00 ) );
+        $self->StyleSetForeground( 7,  Wx::Colour->new( 0x7f, 0x00, 0x7f ) );
+        $self->StyleSetForeground( 8,  Wx::Colour->new( 0x00, 0x00, 0x00 ) );
+        $self->StyleSetForeground( 9,  Wx::Colour->new( 0x7f, 0x7f, 0x7f ) );
+        $self->StyleSetForeground( 10, Wx::Colour->new( 0x00, 0x00, 0x7f ) );
+        $self->StyleSetForeground( 11, Wx::Colour->new( 0x00, 0x00, 0xff ) );
+        $self->StyleSetForeground( 12, Wx::Colour->new( 0x7f, 0x00, 0x7f ) );
+        $self->StyleSetForeground( 13, Wx::Colour->new( 0x40, 0x80, 0xff ) );
+        $self->StyleSetForeground( 17, Wx::Colour->new( 0xff, 0x00, 0x7f ) );
+        $self->StyleSetForeground( 18, Wx::Colour->new( 0x7f, 0x7f, 0x00 ) );
+        $self->StyleSetBold( 12, 1 );
+        $self->StyleSetSpec( wxSTC_H_TAG, "fore:#0000ff" );
 
-		# set the lexer to Perl 5
-		$self->SetLexer(wxSTC_LEX_PERL);
+        # set the lexer to Perl 5
+        $self->SetLexer(wxSTC_LEX_PERL);
 
-		return $self;
-	}
+        return $self;
+    }
 
-	#----> DEMO EDITOR APPLICATION
+    #----> DEMO EDITOR APPLICATION
 
-	# First, define an application object class to encapsulate the application itself
-	package DemoEditorApp;
+    # First, define an application object class to encapsulate the application itself
+    package DemoEditorApp;
 
-	use strict;
-	use warnings;
-	use Wx;
-	use base 'Wx::App';
+    use strict;
+    use warnings;
+    use Wx;
+    use base 'Wx::App';
 
-	# We must override OnInit to build the window
-	sub OnInit {
-		my $self = shift;
+    # We must override OnInit to build the window
+    sub OnInit {
+        my $self = shift;
 
-		my $frame = Wx::Frame->new(
-		undef,                           # no parent window
-		-1,                              # no window id
-		'My First Scintilla Editor!',    # Window title
-		);
+        my $frame = Wx::Frame->new(
+        undef,                           # no parent window
+        -1,                              # no window id
+        'My First Scintilla Editor!',    # Window title
+        );
 
-		my $editor = My::Scintilla::Editor->new(
-		$frame,                          # Parent window
-		);
+        my $editor = My::Scintilla::Editor->new(
+        $frame,                          # Parent window
+        );
 
-		$frame->Show(1);
-		return 1;
-	}
+        $frame->Show(1);
+        return 1;
+    }
 
-	# Create the application object, and pass control to it.
-	package main;
-	my $app = DemoEditorApp->new;
-	$app->MainLoop;
+    # Create the application object, and pass control to it.
+    package main;
+    my $app = DemoEditorApp->new;
+    $app->MainLoop;
 
 
 =head1 DESCRIPTION
@@ -180,6 +185,10 @@ The ambitious goal of this project is to provide fresh Perl 5 and maybe 6
 support in L<Wx> while preserving compatibility with Wx::StyledTextCtrl
 and continually contribute it back to Scintilla project.
 
+Note: You cannot load Wx::STC and Wx::Scintilla in the same application. They
+are mutually exclusive. The wxSTC_... events are handled by one library or
+the other.
+
 =head1 MANUAL
 
 If you are looking for more API documentation, please consult L<Wx::Scintilla::Manual>
@@ -192,7 +201,7 @@ platforms. Please let me know if you can help out :)
 
 On Debian/Ubuntu, you need to install the following via:
 
-	sudo apt-get install libgtk2.0-dev
+    sudo apt-get install libgtk2.0-dev
 
 On MacOS 64-bit by default you need to install a 32-bit Perl in order to
 install wxWidgets 2.8.x. Please refer to 
