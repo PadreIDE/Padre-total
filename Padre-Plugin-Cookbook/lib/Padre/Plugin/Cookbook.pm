0ead8ce4c08346e7d3d808adcb6e7f2290c02546
Adam Kennedy <adamk@cpan.org>
2011-07-07 08:02:58 +0000

    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@14845 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Cookbook/lib/Padre/Plugin/Cookbook.pm
+++ Padre-Plugin-Cookbook/lib/Padre/Plugin/Cookbook.pm
@@ -3,22 +3,10 @@ package Padre::Plugin::Cookbook;
 use 5.010001;
 use strict;
 use warnings;
-use diagnostics;
-use utf8;
-use autodie;
+use Padre::Plugin ();
 
-# Avoids regex performance penalty
-use English qw( -no_match_vars );
-
-# Version required
-use version; our $VERSION = qv(0.14);
-use parent qw(Padre::Plugin);
-
-# use TryCatch;
-use Try::Tiny;
-# use Data::Dumper;
-use Data::Printer;
-use Carp;
+our $VERSION = '0.014';
+our @ISA     = 'Padre::Plugin';
 
 #######
 # Define Padre Interfaces required
@@ -31,10 +19,6 @@ sub padre_interfaces {
 
 		# used by MainFB by Padre::Plugin::FormBuilder
 		'Padre::Wx::Role::Main' => 0.84,
-
-		# used by MainFb by Padre::Plugin::FormBuilder
-		# removed as advised by Sewi
-		# 'Wx::Dialog' => 0.00,
 	);
 }
 
@@ -51,23 +35,18 @@ sub plugin_name {
 sub menu_plugins_simple {
 	my $self = shift;
 	return $self->plugin_name => [
-		'Plug-ins - wxDialogs...' => [
-			'Recipe-01 - Hello World' => sub { $self->load_dialog_recipe01_main(); },
-
-			# 'Recipe-01 - Hello World' => sub { $self->load_dialog_recipexx_main('Recipe01'); },
-
-			'Recipe-02 - Fun with widgets' => sub { $self->load_dialog_recipe02_main(); },
-
-			# 'Recipe-02 - Fun with widgets' => sub { $self->load_dialog_recipexx_main('Recipe02'); },
-
-			'Recipe-03 - inc About dialog' => sub { $self->load_dialog_recipe03_main(); },
-
-			# 'Recipe-03 - out & About' => sub { $self->load_dialog_recipexx_main('Recipe03'); },
-
-			'Recipe-04 - ConfigDB RC1' => sub { $self->load_dialog_recipe04_main(); },
-
-			# 'Recipe-04 - ConfigDB RC1' => sub { $self->load_dialog_recipexx_main('Recipe04'); },
-		],
+		'01 - Hello World' => sub {
+			$self->load_dialog_recipe01_main;
+		},
+		'02 - Fun with widgets' => sub {
+			$self->load_dialog_recipe02_main;
+		},
+		'03 - About dialogs' => sub {
+			$self->load_dialog_recipe03_main;
+		},
+		'04 - ConfigDB RC1' => sub {
+			$self->load_dialog_recipe04_main;
+		},
 	];
 }
 
@@ -199,6 +178,10 @@ sub load_dialog_recipe04_main {
 }
 
 ### Testing below
+#use Try::Tiny;
+#use Carp;
+#use Data::Printer;
+#
 ########
 # Composed Method,
 # Load Recipe-xx Main Dialog, only once
@@ -237,11 +220,12 @@ sub load_dialog_recipe04_main {
 # catch {
 # say '* info method ' . $recipe_num . '::set_up not found, ok';
 # };
-
+#
 #    return;
 #}
 
 1;
+
 __END__
 
 =head1 NAME
@@ -256,11 +240,9 @@ This document describes Padre::Plugin::Cookbook version 0.14
 
 Cookbook is just an example Padre::Plugin uing a WxDialog, showing minimal requirements.
 
-=head1 SUBROUTINES/METHODS
+=head1 METHODS
 
-=over 4
-
-=item padre_interfaces ( )
+=head2 padre_interfaces
 
 Required method with minimum requirements
 
@@ -277,7 +259,7 @@ Called by Padre::Wx::Dialog::PluginManager
 
 	my @needs = $plugin->padre_interfaces;
 
-=item plugin_name ( )
+=head2 plugin_name
 
 Required method with minimum requirements
 
@@ -291,7 +273,7 @@ Called by Padre::Wx::Dialog::PluginManager
 	$self->{label}->SetLabel( $plugin->plugin_name );
 
 
-=item menu_plugins_simple ()
+=head2 menu_plugins_simple
 
 This is where you defined your plugin menu name, note hyphen for clarity.
 
@@ -303,7 +285,7 @@ This is where you defined your plugin menu name, note hyphen for clarity.
 		],
 	];
 
-=item plugin_disable ()
+=head2 plugin_disable
 
 Required method with minimum requirements
 
@@ -314,53 +296,34 @@ Required method with minimum requirements
     $self->unload('Padre::Plugin::Cookbook::Recipe03::Main');
     $self->unload('Padre::Plugin::Cookbook::Recipe03::FBP::MainFB');
 
-=item load_dialog_recipe01_main ()
+=head2 load_dialog_recipe01_main
 
 loads our dialog Main, only allows one instance!
 
     require Padre::Plugin::Cookbook::Recipe01::Main;
     $self->{dialog} = Padre::Plugin::Cookbook::Recipe01::Main->new($main);
 
-
-=item load_dialog_recipe02_main ()
+=head2 load_dialog_recipe02_main
 
 loads our dialog Main, only allows one instance!
 
     require Padre::Plugin::Cookbook::Recipe02::Main;
     $self->{dialog} = Padre::Plugin::Cookbook::Recipe02::Main->new($main);
 
-
-=item load_dialog_recipe03_main ()
+=head2 load_dialog_recipe03_main
 
 loads our dialog Main, only allows one instance!
 
     require Padre::Plugin::Cookbook::Recipe03::Main;
     $self->{dialog} = Padre::Plugin::Cookbook::Recipe03::Main->new($main);
 
-  
-=item load_dialog_recipe04_main ()
+=head2 load_dialog_recipe04_main
 
 loads our dialog Main, only allows one instance!
 
     require Padre::Plugin::Cookbook::Recipe04::Main;
     $self->{dialog} = Padre::Plugin::Cookbook::Recipe04::Main->new($main);
     $self->{dialog}->Show;
-
-    
-=back
-
-=head1 CONFIGURATION AND ENVIRONMENT
- 
-Padre::Plugin::Cookbook requires no configuration files or environment variables.
-
-
-=head1 DEPENDENCIES
-
-Padre::Plugin::Cookbook::Recipe01::Main, Padre::Plugin::Cookbook::Recipe01::FBP::MainFB, 
-Padre::Plugin::Cookbook::Recipe02::Main, Padre::Plugin::Cookbook::Recipe02::FBP::MainFB,
-Padre::Plugin::Cookbook::Recipe03::Main, Padre::Plugin::Cookbook::Recipe03::FBP::MainFB,
-Padre::Plugin::Cookbook::Recipe03::About, Padre::Plugin::Cookbook::Recipe03::FBP::AboutFB,
-Padre::Plugin, Padre::Wx::Role::Main, Wx::Dialog,
 
 =head1 AUTHOR
 
@@ -373,25 +336,4 @@ Copyright (c) 2008-2011 The Padre development team as listed in Padre.pm.
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
 
-=head1 DISCLAIMER OF WARRANTY
-
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
-FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
-PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
-ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
-YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
-NECESSARY SERVICING, REPAIR, OR CORRECTION.
-
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
-REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
-LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
-OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
-THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
-RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
-FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
-SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGES.
+=cut
