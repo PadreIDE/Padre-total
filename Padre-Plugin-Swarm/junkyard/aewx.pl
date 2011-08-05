a2cbedc4d7e0c4c2c6882f099c2b66e49b47f114
submersible_toaster <submersible_toaster@svngit.padre.perlide.org>
2011-08-05 03:57:53 +0000

    proof of concept - latency of threads->kill signal is awful.
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@15247 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Swarm/junkyard/aewx.pl
+++ Padre-Plugin-Swarm/junkyard/aewx.pl
@@ -1,67 +1,75 @@
 #!/usr/bin/perl
 package MyApp;
 use threads;
-use Wx;
+use threads::shared;
+use Thread::Queue;
+use Wx ':everything';
 use Wx::Event;
 use base 'Wx::App';
 use strict;
 use warnings;
 use Data::Dumper;
 
+our $wx_notify : shared;
+$wx_notify = Wx::NewEventType();
+
 sub OnInit {
    my $self = shift;
-
-    my $frame = Wx::Frame->new( undef,           # parent window
+   
+   my $queue = Thread::Queue->new;
+   my $tid = threads->create(\&background, 1, $queue , $self );
+   $self->{queue} = $queue; 
+   $self->{mythread} = $tid;
+   
+   my $frame = Wx::Frame->new( undef,           # parent window
                                 -1,              # ID -1 means any
                                 'wxPerl rules',  # title
                                 [-1, -1],         # default position
                                 [250, 150],       # size
                                );
-   my $button = Wx::Button->new( $frame , -1 , 'Say Something' );
-   Wx::Event::EVT_BUTTON( $self, $button , \&OnButtonClick );
+
+   my $s = Wx::BoxSizer->new( wxVERTICAL );
+   my $t = Wx::StaticText->new( $frame , -1 , 'My Static Text' ,  [-1,-1], [-1,-1] );
+   $self->{text} = $t;
+   
+   my $te = Wx::TextCtrl->new( $frame , -1 ,'' ,  [-1,-1] , [-1,-1],
+                                wxTE_PROCESS_ENTER );
+   $self->{entry} = $te;
+   
+    $s->Add($t,0, Wx::wxALL | Wx::wxEXPAND );
+    $s->Add($te, 0 , Wx::wxALL | Wx::wxEXPAND);
+   Wx::Event::EVT_TEXT_ENTER( $self, $te , \&OnTextEnter );
+   
+   Wx::Event::EVT_COMMAND( $self, -1 , $wx_notify , \&OnThreadEvent );
+   $frame->SetSizer($s);
+   $frame->Layout;
    $frame->Show(1);
 
 
 }
 
-sub OnButtonClick {
-   warn "Button Clicked - @_";
-   my $self = shift;
-   if ( $self->{queue} ) {
-       warn "Enqueued message";
-       $self->{queue}->enqueue( 'Message from WX - button pushed' );
-       if ( $self->{thr} ) {
-           $self->{thr}->kill('SIGINT');
-       }
-   }
+sub OnTextEnter {
+    my ($self) = @_;
+   print STDERR "Button Clicked - @_";
+   print STDERR "Enqueued message";
+   $self->{queue}->enqueue( $self->{entry}->GetValue );
+   $self->{mythread}->kill('SIGINT');
+
 }
 
-1;
-
-package main;
-use threads;
-use Thread::Queue;
-
-my $queue = Thread::Queue->new;
-my $tid = threads->create(\&background, 1, $queue );
-my $app = MyApp->new;
-$app->{queue} = $queue;
-$app->{thr}   = $tid;
-
-
-
-warn "Created $tid";
-
-$app->MainLoop;
-
-$tid->kill('SIGTERM')->join;
-
-exit 0;
+sub OnThreadEvent {
+    warn "Got thread event @_";
+    my ($self,$evt) = @_;
+    $self->{text}->SetLabel( $evt->GetData );
+    
+}
+###############################################################################
 
 ### Thread
 sub background {
    my $freq = shift;
    my $q = shift;
+   my $frame = shift;
 
 local $ENV{PERL_ANYEVENT_MODEL} = 'Perl';
 require AnyEvent;
@@ -79,7 +87,7 @@ require JSON;
    ) or die $!;
    $client->mcast_add('239.255.255.1'); #should have the interface
    $client->mcast_loopback( 1 );
-   my $g = AnyEvent->io( poll=>'r' , fh => $client , cb => sub { client_socket_read($client) } );
+   my $g = AnyEvent->io( poll=>'r' , fh => $client , cb => sub { client_socket_read($client,$frame) } );
    my $wakeup= AnyEvent->signal( signal => 'INT' , cb => sub { wx_queue_read($q,$client) } );
 
    my $r = $bailout->recv;
@@ -94,18 +102,33 @@ sub timer_poll {
 
 sub client_socket_read {
     my $client = shift;
-    my $message;
+    my $frame = shift;
+    my $message; 
     $client->recv( $message, 65535 );
     warn "Got message : $message";
+    my $payload = Wx::PlThreadEvent->new( -1, $wx_notify, $message ) ;
+    Wx::PostEvent( $frame, $payload );
 }
 
 sub wx_queue_read {
     my $q = shift;
     my $c = shift;
     while ( defined(my $m = $q->dequeue_nb) ) {
-        warn "Got message '$m'";
+        print STDERR "Got message '$m'";
         my $msg = JSON::encode_json( { type=>'chat' , body=>$m , from=>$0.$$ } );
         $c->mcast_send( $msg , '239.255.255.1:12000' ); 
     }
 
 }
+
+1;
+
+package main;
+
+
+my $app = MyApp->new;
+$app->MainLoop;
+
+
+
+exit 0;
