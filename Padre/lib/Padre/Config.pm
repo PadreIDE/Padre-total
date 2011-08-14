PadreIDE/Padre-total

commit b0f136fe1f64fc9ddaaa3c6a3c69589f2ba14265

Added feature_devel_traceuse (disabled by default) and feature_devel_traceuse_options which makes Padre run scripts with Devel::TraceUse=<opt> to show the modules that are loaded recursively (DOLMEN++ for the idea)
git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@15476 18eddd70-d622-4293-bd05-fe65668e8bde

Padre/lib/Padre/Config.pm
@@ -1359,6 +1359,25 @@ setting(
 		. _T('This requires an installed Devel::EndStats and a Padre restart'),
 );
 
+# Enable experimental Run with Devel::TraceUse support.
+setting(
+	name    => 'feature_devel_traceuse',
+	type    => Padre::Constant::BOOLEAN,
+	store   => Padre::Constant::HUMAN,
+	default => 0,
+	help    => _T('Enable or disable the Run with Devel::TraceUse if it is installed. ')
+		. _T('This requires an installed Devel::TraceUse and a Padre restart'),
+);
+
+# Specify Devel::TraceUse options for experimental Run with Devel::TraceUse support
+setting(
+	name    => 'feature_devel_traceuse_options',
+	type    => Padre::Constant::ASCII,
+	store   => Padre::Constant::HUMAN,
+	default => '',
+	help    => _T('Specify Devel::TraceUse options. 'feature_devel_traceuse' must be enabled.'),
+);
+
 # Window menu list shorten common path
 setting(
 	name    => 'window_list_shorten_path',

Padre/lib/Padre/Document/Perl.pm
@@ -356,6 +356,8 @@ sub get_command {
 	push @commands, '-d'                          if $debug;
 	push @commands, '-Mdiagnostics(-traceonly)'   if $trace;
 	push @commands, '-MDevel::EndStats=verbose,1' if $config->feature_devel_endstats;
+	my $devel_traceuse_options = $config->feature_devel_traceuse_options;
+	push @commands, '-d:TraceUse'. ($devel_traceuse_options ne '' ? "=$devel_traceuse_options" : '') if $config->feature_devel_traceuse;
 	push @commands, "$run_args{interpreter}";
 	if (Padre::Constant::WIN32) {
 		push @commands, qq{"$shortname"$script_args};

Padre/t/04-config.t
@@ -2,7 +2,7 @@
 
 use strict;
 use warnings;
-use constant NUMBER_OF_CONFIG_OPTIONS => 130;
+use constant NUMBER_OF_CONFIG_OPTIONS => 132;
 
 # Move of Debug to Run Menu
 use Test::More tests => NUMBER_OF_CONFIG_OPTIONS * 2 + 28;

commit 7b50c25fed822847c0945e2e6ff82a1bd27944a7

Fix a syntax error that was introduced in r15476 (azawawi--)
git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@15477 18eddd70-d622-4293-bd05-fe65668e8bde

Padre/lib/Padre/Config.pm
@@ -1375,7 +1375,7 @@ setting(
 	type    => Padre::Constant::ASCII,
 	store   => Padre::Constant::HUMAN,
 	default => '',
-	help    => _T('Specify Devel::TraceUse options. 'feature_devel_traceuse' must be enabled.'),
+	help    => _T(q{Specify Devel::TraceUse options. 'feature_devel_traceuse' must be enabled.}),
 );
 
 # Window menu list shorten common path