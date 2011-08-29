19f47e3a5c57183ab2e778d1b7798369052406c9
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2011-08-29 07:25:55 +0000

    [Autoformat] Perl tidy and converted project from Dist::Zilla into Module::Build. Added missing tests
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@15835 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Autoformat/lib/Padre/Plugin/Autoformat.pm
+++ Padre-Plugin-Autoformat/lib/Padre/Plugin/Autoformat.pm
@@ -5,65 +5,66 @@ package Padre::Plugin::Autoformat;
 use strict;
 use warnings;
 
-use File::Basename        qw{ fileparse };
+use File::Basename qw{ fileparse };
 use File::Spec::Functions qw{ catfile };
 use base qw{ Padre::Plugin };
 
-our $VERSION='1.23';
+our $VERSION = '1.23';
 
 # -- padre plugin api, refer to Padre::Plugin
 
 # plugin name
-sub plugin_name { 'Autoformat' }
+sub plugin_name {'Autoformat'}
 
 # plugin icon
 sub plugin_icon {
-    # find icon path using Padre API
-    my $dir = File::Spec->catdir( Padre::Util::share('Autoformat'), 'icons' );
-    my $icon_file = File::Spec->catfile( $dir, 'justify.png' );
 
-    # create and return icon
-    return Wx::Bitmap->new( $icon_file, Wx::wxBITMAP_TYPE_PNG );
+	# find icon path using Padre API
+	my $dir = File::Spec->catdir( Padre::Util::share('Autoformat'), 'icons' );
+	my $icon_file = File::Spec->catfile( $dir, 'justify.png' );
+
+	# create and return icon
+	return Wx::Bitmap->new( $icon_file, Wx::wxBITMAP_TYPE_PNG );
 }
 
 # padre interfaces
 sub padre_interfaces {
-    'Padre::Plugin'     => 0.68,
-    'Padre::Wx::Editor' => 0.89,
+	'Padre::Plugin' => 0.68, 'Padre::Wx::Editor' => 0.89,;
 }
 
 # plugin menu.
 sub menu_plugins_simple {
-    my ($self) = @_;
-    Wx::gettext('Autoformat') => [
-        #'About'                    => 'show_about',
-        Wx::gettext("Autoformat\tCtrl+Shift+J") => 'autoformat',
-    ];
+	my ($self) = @_;
+	Wx::gettext('Autoformat') => [
+
+		#'About'                    => 'show_about',
+		Wx::gettext("Autoformat\tCtrl+Shift+J") => 'autoformat',
+	];
 }
 
 
 # -- public methods
 
 sub autoformat {
-    my ($self) = @_;
+	my ($self) = @_;
 
-    my $main     = $self->main;
-    my $current  = $main->current;
-    my $document = $current->document;
-    my $editor   = $current->editor;
-    return unless $editor;
+	my $main     = $self->main;
+	my $current  = $main->current;
+	my $document = $current->document;
+	my $editor   = $current->editor;
+	return unless $editor;
 
-    # no selection means autoformat current paragraph
-    if ( not $editor->GetSelectedText ) {
-        my ($b, $e) = $editor->current_paragraph;
-        return if $b == $e; # in between paragraphs
-        $editor->SetSelection($b, $e);
-    }
+	# no selection means autoformat current paragraph
+	if ( not $editor->GetSelectedText ) {
+		my ( $b, $e ) = $editor->current_paragraph;
+		return if $b == $e; # in between paragraphs
+		$editor->SetSelection( $b, $e );
+	}
 
-    require Text::Autoformat;
-    my $messy  = $editor->GetSelectedText;
-    my $tidied = Text::Autoformat::autoformat($messy);
-    $editor->ReplaceSelection($tidied);
+	require Text::Autoformat;
+	my $messy  = $editor->GetSelectedText;
+	my $tidied = Text::Autoformat::autoformat($messy);
+	$editor->ReplaceSelection($tidied);
 }
 
 
