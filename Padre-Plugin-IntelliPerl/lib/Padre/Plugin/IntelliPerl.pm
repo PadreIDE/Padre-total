250184a4a961e9b8cde8d1c93332441f84b8cf36
Kevin Dawson <kevin@dawson10.plus.com>
2011-09-08 23:39:15 +0000

    update interfaces to v0.91 Padre::Plugin api 2.2
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@16117 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-IntelliPerl/lib/Padre/Plugin/IntelliPerl.pm
+++ Padre-Plugin-IntelliPerl/lib/Padre/Plugin/IntelliPerl.pm
@@ -10,37 +10,43 @@ use Devel::IntelliPerl;
 our $VERSION = '0.01';
 
 sub plugin_name {
-  return "IntelliPerl";
+	return Wx::gettext('IntelliPerl');
 }
 
 sub menu_plugins_simple {
-  my $self = shift;
-  return $self->plugin_name => [
-                                _T("Intellicomplete\tAlt-I") => sub { $self->intellicomplete },
-                               ];
+	my $self = shift;
+	return $self->plugin_name => [
+		_T("Intellicomplete\tAlt-I") => sub { $self->intellicomplete },
+	];
 }
 
 sub padre_interfaces {
-        return 'Padre::Plugin' => '0.26';
-      }
+	return (
+		'Padre::Plugin'  => '0.91',
+		'Padre::Wx'      => '0.91',
+		'Padre::Util'    => '0.91',
+		'Padre::Current' => '0.91',
+	);
+}
 
 sub intellicomplete {
-  my $self = shift;
-  my $doc = Padre::Current->document;
-  my $editor = $doc->editor;
-  my $pos    = $editor->GetCurrentPos;
-  my $line   = $editor->LineFromPosition($pos);
-  my $first  = $editor->PositionFromLine($line);
-  my $col = $pos - $first;
-  $line = $line + 1;
-  $col = $col + 1;
-  my $source = $editor->GetText();
-  my $ip = Devel::IntelliPerl->new(source => $source, line_number => $line, column => $col);
-  my @list = $ip->methods();
-  my $prefix = $ip->prefix();
-  if(@list) {
-    $editor->AutoCompShow( length($prefix), join " ", @list );
-  }
+	my $self   = shift;
+	my $doc    = Padre::Current->document;
+	my $editor = $doc->editor;
+	my $pos    = $editor->GetCurrentPos;
+	my $line   = $editor->LineFromPosition($pos);
+	my $first  = $editor->PositionFromLine($line);
+	my $col    = $pos - $first;
+	$line = $line + 1;
+	$col  = $col + 1;
+	my $source = $editor->GetText();
+	my $ip     = Devel::IntelliPerl->new( source => $source, line_number => $line, column => $col );
+	my @list   = $ip->methods();
+	my $prefix = $ip->prefix();
+
+	if (@list) {
+		$editor->AutoCompShow( length($prefix), join " ", @list );
+	}
 }
 
 1;
