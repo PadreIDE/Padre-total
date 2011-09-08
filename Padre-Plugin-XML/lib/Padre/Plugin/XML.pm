ba508f47a1a4912443a942d1e36cd41141fe08dd
Kevin Dawson <kevin@dawson10.plus.com>
2011-09-08 09:46:55 +0000

    update interfaces to v0.91 Padre::Plugin api 2.2
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@16055 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-XML/lib/Padre/Plugin/XML.pm
+++ Padre-Plugin-XML/lib/Padre/Plugin/XML.pm
@@ -10,7 +10,7 @@ use base 'Padre::Plugin';
 use Padre::Wx ();
 
 sub padre_interfaces {
-	'Padre::Plugin' => 0.81, 'Padre::Document' => 0.81;
+	'Padre::Plugin' => 0.91, 'Padre::Document' => 0.91;
 }
 
 sub registered_documents {
