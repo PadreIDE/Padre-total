7925d165e21aa12730cb42ffc408f4ee4eda3969
Kevin Dawson <kevin@dawson10.plus.com>
2011-09-23 08:50:21 +0000

    tweak and tidy
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@16451 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Patch/lib/Padre/Plugin/Patch/Main.pm
+++ Padre-Plugin-Patch/lib/Padre/Plugin/Patch/Main.pm
@@ -484,18 +484,18 @@ sub test_svn {
 	my $self = shift;
 
 	use Sort::Versions;
-	my $svn_version          = 0;
+	my $svn_client_version   = 0;
 	my $required_svn_version = '1.6.2';
 
-	if ( eval { $svn_version = qx{svn --version --quiet} } ) {
-		$svn_version =~ s/\n//;
-
+	if ( eval { $svn_client_version = qx{svn --version --quiet} } ) {
+		chomp($svn_client_version);
+		
 		# This is so much better, now we are testing for version as well
-		if ( versioncmp( $required_svn_version, $svn_version, ) == -1 ) {
-			TRACE("Found local SVN v$svn_version") if DEBUG;
+		if ( versioncmp( $required_svn_version, $svn_client_version, ) == -1 ) {
+			TRACE("Found local SVN v$svn_client_version") if DEBUG;
 			return 1;
 		} else {
-			TRACE("Found SVN v$svn_version but require v$required_svn_version") if DEBUG;
+			TRACE("Found SVN v$svn_client_version but require v$required_svn_version") if DEBUG;
 		}
 	} else {
 		TRACE("SVN not Found error: $@") if DEBUG;
