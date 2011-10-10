5317cf9c0c26622840742a1d9c8bf68fe4f50a0c
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2011-10-10 09:46:58 +0000

    Update parse_wx_scintilla_h.pl to remove wxSTC_ prefix and add an underscore prefix to constants with leading digits. At also writes to Wx::Scintilla::Constants
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@16897 18eddd70-d622-4293-bd05-fe65668e8bde

--- Wx-Scintilla/share/parse_wx_scintilla_h.pl
+++ Wx-Scintilla/share/parse_wx_scintilla_h.pl
@@ -1,17 +1,27 @@
 use strict;
 use warnings;
 
-my $filename = 'wx-scintilla/include/WxScintilla.h';
-if(open my $fh, $filename) {
-	print "use constant {\n";
-	while(my $line = <$fh>) {
-		if($line =~ /^\s*#define\s+(wxSTC_.+)\s+(.+)\s*$/) {
-			my ($name, $val) = ("$1", $2);
-			print "$name => $val,\n";
-		}
-	}
-	print "};\n";
-	close $fh;
-} else {
-	die "Cannot open $filename\n";
+my $filename     = '../wx-scintilla/include/WxScintilla.h';
+my $out_filename = '../lib/Wx/Scintilla/Constants.pm';
+
+open my $fh, $filename
+  or die "Cannot open $filename\n";
+open my $output, '>', $out_filename
+  or die "Cannot open $out_filename\n";
+print $output <<'CODE';
+package Wx::Scintilla::Constants;
+
+use constant {
+CODE
+
+while ( my $line = <$fh> ) {
+    if ( $line =~ /^\s*#define\s+wxSTC_(.+)\s+(.+)\s*$/ ) {
+
+        my ( $name, $val ) = ( $1, $2 );
+        $name =~ s/^(\d)/_$1/;
+        print $output "$name => $val,\n";
+    }
 }
+print $output "};\n";
+close $output;
+close $fh;
