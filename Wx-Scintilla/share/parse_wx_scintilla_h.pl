b06ac35f8cc023d0bb558bf7bd582c8f231b4988
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2011-10-18 09:29:34 +0000

    Drop a better parser for Scintilla.iface that generate *true* scintilla constants along with POD for them
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@17055 18eddd70-d622-4293-bd05-fe65668e8bde

--- Wx-Scintilla/share/parse_wx_scintilla_h.pl
+++ Wx-Scintilla/share/parse_wx_scintilla_h.pl
@@ -2,28 +2,60 @@
 
 use strict;
 use warnings;
+use Perl::Tidy ();
 
-my $filename     = '../wx-scintilla/include/WxScintilla.h';
-my $out_filename = '../lib/Wx/Scintilla/Constants.pm';
+my $filename = '../wx-scintilla/src/scintilla/include/Scintilla.iface';
+my $constants_filename = '../lib/Wx/Scintilla/Constants.pm';
 
-open my $fh, $filename
-  or die "Cannot open $filename\n";
-open my $output, '>', $out_filename
-  or die "Cannot open $out_filename\n";
-print $output <<'CODE';
-package Wx::Scintilla::Constants;
-
+print "Parsing $filename\n";
+open my $fh, $filename or die "Cannot open $filename\n";
+my $docs = "=pod\n";
+$docs .= "=head2 Constants\n";
+my $source = <<'CODE';
 use constant {
 CODE
-
+my $doc_comment = undef;
 while ( my $line = <$fh> ) {
-    if ( $line =~ /^\s*#define\s+wxSTC_(\S+)\s+(.+?)\s*$/ ) {
+    if ( $line =~ /^\s*$/ ) {
 
-        my ( $name, $val ) = ( $1, $2 );
-        $name =~ s/^(\d)/_$1/;
-        print $output "    $name => $val,\n";
+        # Empty line separator
+        $doc_comment = undef;
+    }
+    elsif ( $line =~ /^##/ ) {
+
+        # ignore pure comments
+    }
+    elsif ( $line =~ /^(#.+?)$/ ) {
+
+        # Store documentation comments
+        $doc_comment .= "\t$1\n";
+    }
+    elsif ( $line =~ /^\s*val\s+(\w+)\s*=(.+?)\s*$/ ) {
+        if ( defined $doc_comment ) {
+            $source .= $doc_comment;
+            $doc_comment =~ s/\s+#\s+//g;
+            $docs .= "\n$doc_comment\n";
+            $doc_comment = undef;
+        }
+        $source .= "\t$1 => $2,\n";
+        $docs .= sprintf( "%-20s (%s)\n\n", $1, $2 );
     }
 }
-print $output "};\n\n1;\n";
-close $output;
+$docs   .= "=cut\n";
+$source .= "};\n";
+$source .= "\n\n$docs";
+close $fh;
+print "Perl tidy output in memory\n";
+my $output = '';
+Perl::Tidy::perltidy(
+    source      => \$source,
+    destination => \$output,
+    argv        => '--indent-block-comments',
+);
+
+print "Writing to $constants_filename\n";
+open my $constants_fh, '>', $constants_filename
+  or die "Cannot open $constants_filename\n";
+print $constants_fh $output;
+close $constants_fh;
 close $fh;
