9681904a12277528fd577cd73ed90c38eb00e57c
submersible_toaster <submersible_toaster@svngit.padre.perlide.org>
2011-11-29 21:12:32 +0000

    updated templates for texturing
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@17550 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-OXP/share/povray/script.pl
+++ Padre-Plugin-OXP/share/povray/script.pl
@@ -1,56 +1,114 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-
-
+use Text::CSV;
+use File::Temp;
 use Digest::JHash 'jhash';
+use Data::Dumper;
 
 my $max = 1 << 31;
 $max*=2;
-my $seed = jhash rand();
-my $norm_seed = $seed/$max; # normalised to 0..1
 my $n_planets = 8 * 256;
 
-my $planet_type = $seed % $n_planets+1;
-my $planet_specifier = $seed % int(sqrt($n_planets+1));
+my $galaxy_csv = shift @ARGV;
+my $render = shift @ARGV;
 
-# Barren/rocky , Gas Giant , Continental , Artificial
-my $formation_seed = $planet_type % 4;
-my %formations = (
-        0 => 'Rocky',
-        1 => 'Gas' ,
-        2 => 'Continental',
-        3 => 'Artificial'
-);
-
-# Choose a colour map as $Formation + $colour_seed
-my $colour_seed = $planet_type % 11;
-
-# Choose a pattern based on 
-my $pattern_seed= $planet_specifier % 5;
-
-
-
-print "planet type $planet_type, $planet_specifier",$/;
-
-my $macro = $formations{$formation_seed};
 
 my $POV_template = <<POV;
-#version unofficial megapov 1.21
+#version unofficial megapov 1.21;
 #include "cm_camera.inc"
-
+#include "pp_textures.inc"
+#debug  "%s" 
 #declare Radius = 1;
 
 sphere { <0,0,0> Radius
         hollow no_shadow inverse double_illuminate
         material {
-                $macro($colour_seed,$pattern_seed)
+                %s(
+                        %d,  // colour
+                        %d,  // pattern
+                        %d,  // galaxy X
+                        %d,  // galaxy Y
+                        %f // float modifier 1
+                        %f // float modifier 2
+                )
         }
 }
 CubeMapBoxes(Radius)
-CubeLight(4,Radius)
+//CubeLight(4,Radius)
 CubeMapCamera()
 
 POV
 
-print $POV_template;
+
+my $csv = Text::CSV->new;
+open( my $fh , $galaxy_csv ) or die $!;
+my $headings = $csv->getline($fh);
+#die Dumper $headings;
+
+while ( my $row = $csv->getline($fh) ) {
+        my %info;
+        @info{@$headings} = @$row;
+        
+        my $seed = jhash $info{Name};
+        my $seed2= jhash scalar reverse $info{Name};
+        my $norm_seed = $seed/$max; # normalised to 0..1
+        my $norm_seed2= $seed2/$max; 
+        my $galaxy_X = $info{X};
+        $galaxy_X =~ s/[^\d]//g;
+        my $galaxy_Y = $info{Y};
+        $galaxy_Y =~ s/[^\d]//g;
+        my $planet_type = $seed % $n_planets+1;
+        my $planet_specifier = $seed % int(sqrt($n_planets+1));
+
+        # Barren/rocky , Gas Giant , Continental , Artificial
+        my $formation_seed = $seed % 3;
+        my %formations = (
+                2 => 'Rocky',
+                1 => 'Rocky',
+                0 => 'Gas' ,
+                
+               # 2 => 'Continental',
+               # 3 => 'Artificial'
+        );
+
+        # Choose a colour map as $Formation + $colour_seed
+        my $colour_seed = $seed2 % 8;
+
+        # Choose a pattern based on 
+        my $pattern_seed= $seed % 4;
+
+        my $macro = $formations{$formation_seed};
+
+
+        
+        my $pov_sdl = sprintf $POV_template, $info{Name},
+                $macro, 
+                $colour_seed, $pattern_seed,
+                $galaxy_X, $galaxy_Y,
+                $norm_seed, $norm_seed2;
+        
+        my $tf = File::Temp->new;
+        $tf->print( $pov_sdl );
+        $tf->close;
+        
+        
+        if ( defined $render ) {
+                my $output = 'Textures/'. $info{Name}.'.png';
+                next if -f $output;
+                my $rc = system(
+                        'megapov',
+                        '+Lcm',
+                        '-D',
+                        # '+W256', '+H1536', # medium resolution
+                        '+W128','+H768', # preview resolution
+                        '+O'.$output,
+                        '+I'.$tf->filename,
+                );
+                die $! unless $rc==0;
+                die $pov_sdl unless -f $output;
+        }
+        
+        else { print $pov_sdl }
+        
+}
--- Padre-Plugin-OXP/share/povray/template.pov
+++ Padre-Plugin-OXP/share/povray/template.pov
@@ -8,9 +8,16 @@
 sphere { <0,0,0> Radius
         hollow no_shadow inverse double_illuminate
         material {
-                Gas(2,0)
+                Gas(
+                        1,  // colour
+                        1,  // pattern
+                        0,  // galaxy X
+                        0,  // galaxy Y
+                        0.76567  // float modifier 1
+                        0.134534  // float modifier 2
+                )
         }
 }
 CubeMapBoxes(Radius)
-CubeLight(4,Radius)
+
 CubeMapCamera()
