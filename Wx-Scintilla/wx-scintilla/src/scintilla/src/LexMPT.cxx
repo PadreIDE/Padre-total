6f49f4a8e97ee2e9de864dfc862ef349acdb6ce7
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2011-12-11 08:04:57 +0000

    Upgraded to Scintilla 3.0.2. Notable changes CoffeeScript lexer
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@17649 18eddd70-d622-4293-bd05-fe65668e8bde

--- Wx-Scintilla/wx-scintilla/src/scintilla/src/LexMPT.cxx
+++ Wx-Scintilla/wx-scintilla/src/scintilla/src/LexMPT.cxx
@@ -12,11 +12,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <assert.h>
-
 #include <ctype.h>
-#ifdef _MSC_VER
-#pragma warning(disable: 4786)
-#endif
+
 #include <string>
 
 #include "ILexer.h"
