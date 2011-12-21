b3c1b64affffef9b70392cd804e4124bc5f7aff4
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2011-12-21 12:03:26 +0000

    Released 0.35_05 as stable 0.36 (Scintilla 3.0.2 is here!)
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@17747 18eddd70-d622-4293-bd05-fe65668e8bde

--- Wx-Scintilla/lib/Wx/Scintilla/Constant.pm
+++ Wx-Scintilla/lib/Wx/Scintilla/Constant.pm
@@ -10,7 +10,7 @@ use strict;
 use warnings;
 
 require Exporter;
-our $VERSION = '0.35_05';
+our $VERSION = '0.36';
 our @ISA     = 'Exporter';
 our @EXPORT  = qw(
 	INVALID_POSITION
--- Wx-Scintilla/lib/Wx/Scintilla/TextCtrl.pm
+++ Wx-Scintilla/lib/Wx/Scintilla/TextCtrl.pm
@@ -4,7 +4,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '0.35_05';
+our $VERSION = '0.36';
 our @ISA     = 'Wx::ScintillaTextCtrl';
 
 1;
