fe8dae98202123ed844e7447b3c469b01655f9b0
Adam Kennedy <adamk@cpan.org>
2012-02-03 11:45:24 +0000

    Timeline in reverse chronological order
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@18181 18eddd70-d622-4293-bd05-fe65668e8bde

--- Madre/lib/Madre/Timeline.pm
+++ Madre/lib/Madre/Timeline.pm
@@ -7,20 +7,19 @@ use ORLite::Migrate::Timeline ();
 our $VERSION = '0.01';
 our @ISA     = 'ORLite::Migrate::Timeline';
 
-sub upgrade1 {
+sub upgrade3 {
    my $self = shift;
 
    $self->do(<<'END_SQL');
-   CREATE TABLE user ( 
-       user_id  INTEGER  NOT NULL PRIMARY KEY,
-       email    TEXT     NOT NULL,
-       password TEXT     NOT NULL,
-       created  DATETIME NOT NULL DEFAULT ( datetime('now') ),
+   CREATE TABLE instance (
+      instance_id TEXT     NOT NULL PRIMARY KEY,
+      created     DATETIME NOT NULL DEFAULT ( datetime('now') ),
+      modified    DATETIME NOT NULL DEFAULT ( datetime('now') ),
+      padre       TEXT     NULL
+      perl        TEXT     NULL,
+      osname      TEXT     NULL,
+      data        BLOB     NULL
    )
-END_SQL
-
-   $self->do(<<'END_SQL');
-   CREATE UNIQUE INDEX index_user_email ON user ( email COLLATE NOCASE )
 END_SQL
 
    return 1;
@@ -50,19 +49,20 @@ END_SQL
    return 1;
 }
 
-sub upgrade3 {
+sub upgrade1 {
    my $self = shift;
 
    $self->do(<<'END_SQL');
-   CREATE TABLE instance (
-      instance_id TEXT     NOT NULL PRIMARY KEY,
-      created     DATETIME NOT NULL DEFAULT ( datetime('now') ),
-      modified    DATETIME NOT NULL DEFAULT ( datetime('now') ),
-      padre       TEXT     NULL
-      perl        TEXT     NULL,
-      osname      TEXT     NULL,
-      data        BLOB     NULL
+   CREATE TABLE user ( 
+       user_id  INTEGER  NOT NULL PRIMARY KEY,
+       email    TEXT     NOT NULL,
+       password TEXT     NOT NULL,
+       created  DATETIME NOT NULL DEFAULT ( datetime('now') ),
    )
+END_SQL
+
+   $self->do(<<'END_SQL');
+   CREATE UNIQUE INDEX index_user_email ON user ( email COLLATE NOCASE )
 END_SQL
 
    return 1;
