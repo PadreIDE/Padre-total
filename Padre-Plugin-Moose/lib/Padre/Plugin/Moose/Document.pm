dbf4917ab1a1ee09f275afad6f5122e2361dc876
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2012-02-29 08:54:45 +0000

    [P-P-Moose] Keep processing it there was snippet completion otherwise consume the TAB event
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@18488 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Moose/lib/Padre/Plugin/Moose/Document.pm
+++ Padre-Plugin-Moose/lib/Padre/Plugin/Moose/Document.pm
@@ -11,7 +11,7 @@ our @ISA = 'Padre::Document::Perl';
 
 # Override SUPER::set_editor to hook up the key down event
 sub set_editor {
-	my $self = shift;
+	my $self   = shift;
 	my $editor = shift;
 
 	$self->SUPER::set_editor($editor);
@@ -19,44 +19,49 @@ sub set_editor {
 	# Load snippets
 	require YAML::Tiny;
 	require File::ShareDir;
-	my $snippets = YAML::Tiny::LoadFile( File::ShareDir::dist_file('Padre-Plugin-Moose', 'snippets.yml') );
+	my $snippets = YAML::Tiny::LoadFile( File::ShareDir::dist_file( 'Padre-Plugin-Moose', 'snippets.yml' ) );
 
 	# TODO Padre should fire event_key_down instead of this hack :)
 	# Register keyboard event handler for the current editor
-	Wx::Event::EVT_KEY_DOWN($editor, undef);
-	Wx::Event::EVT_KEY_DOWN($editor, sub {
-		my $event = $_[1];
+	Wx::Event::EVT_KEY_DOWN( $editor, undef );
+	Wx::Event::EVT_KEY_DOWN(
+		$editor,
+		sub {
+			my $event = $_[1];
 
-		if($event->GetKeyCode == Wx::WXK_TAB) {
-			my $position       = $editor->GetCurrentPos;
-			my $start_position = $editor->PositionFromLine( $editor->LineFromPosition($position) );
-			my $line           = $editor->GetTextRange( $start_position, $position );
+			my $snippet_added = 0;
+			if ( $event->GetKeyCode == Wx::WXK_TAB ) {
+				my $position       = $editor->GetCurrentPos;
+				my $start_position = $editor->PositionFromLine( $editor->LineFromPosition($position) );
+				my $line           = $editor->GetTextRange( $start_position, $position );
 
-			my $cursor = '$0';
-			for my $e ( keys %$snippets ) {
-				my $v = $snippets->{$e};
-				if ( $line =~ /^\s*\Q$e\E$/ ) {
-					$editor->SetTargetStart( $position - length($e) );
-					$editor->SetTargetEnd($position);
-					my $m = $v;
-					$m =~ s/\$\d//g;
-					$editor->ReplaceTarget($m);
-					if ( $v =~ /(\Q$cursor\E)/g ) {
-						$editor->GotoPos( $position - length($e) + pos($v) - length($cursor) );
+				my $cursor = '$0';
+				for my $e ( keys %$snippets ) {
+					my $v = $snippets->{$e};
+					if ( $line =~ /^\s*\Q$e\E$/ ) {
+						$editor->SetTargetStart( $position - length($e) );
+						$editor->SetTargetEnd($position);
+						my $m = $v;
+						$m =~ s/\$\d//g;
+						$editor->ReplaceTarget($m);
+						if ( $v =~ /(\Q$cursor\E)/g ) {
+							$editor->GotoPos( $position - length($e) + pos($v) - length($cursor) );
+						}
+						$snippet_added = 1;
+						last;
 					}
-					last;
 				}
-			}
-			
-			# Consume the TAB :)
-			$event->StopPropagation;
-		} else {
-			# Keep processing
-			$event->Skip(1);
-		}
 
-		return;
-	});
+
+			}
+
+			# Keep processing it there was snippet completion
+			# Other consume the TAB key down event
+			$event->Skip(1) unless ($snippet_added);
+
+			return;
+		}
+	);
 }
 
 1;
