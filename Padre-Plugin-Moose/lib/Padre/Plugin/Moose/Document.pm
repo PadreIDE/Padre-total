9b679426d4aa3b0fc26b71246622b3d9a45ce79c
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2012-02-29 08:35:24 +0000

    [P-P-Moose] More documentation and TODO on this hack
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@18486 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Moose/lib/Padre/Plugin/Moose/Document.pm
+++ Padre-Plugin-Moose/lib/Padre/Plugin/Moose/Document.pm
@@ -9,12 +9,14 @@ our $VERSION = '0.14';
 
 our @ISA = 'Padre::Document::Perl';
 
+# Override SUPER::set_editor to hook up the key down event
 sub set_editor {
 	my $self = shift;
 	my $editor = shift;
 
 	$self->SUPER::set_editor($editor);
 
+	# TODO Padre should fire event_key_down instead of this hack :)
 	Wx::Event::EVT_KEY_DOWN($editor, undef);
 	Wx::Event::EVT_KEY_DOWN($editor, sub {
 		my $event = $_[1];
