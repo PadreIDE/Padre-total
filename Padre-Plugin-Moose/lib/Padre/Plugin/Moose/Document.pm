c6d45c6a4b097145de2966d213b154bd81353e27
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2012-02-29 09:00:04 +0000

    [P-P-Moose] More refactoring and documentation
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@18489 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Moose/lib/Padre/Plugin/Moose/Document.pm
+++ Padre-Plugin-Moose/lib/Padre/Plugin/Moose/Document.pm
@@ -16,53 +16,68 @@ sub set_editor {
 
 	$self->SUPER::set_editor($editor);
 
-	# Load snippets
-	require YAML::Tiny;
-	require File::ShareDir;
-	my $snippets = YAML::Tiny::LoadFile( File::ShareDir::dist_file( 'Padre-Plugin-Moose', 'snippets.yml' ) );
-
 	# TODO Padre should fire event_key_down instead of this hack :)
 	# Register keyboard event handler for the current editor
 	Wx::Event::EVT_KEY_DOWN( $editor, undef );
 	Wx::Event::EVT_KEY_DOWN(
 		$editor,
 		sub {
-			my $event = $_[1];
-
-			my $snippet_added = 0;
-			if ( $event->GetKeyCode == Wx::WXK_TAB ) {
-				my $position       = $editor->GetCurrentPos;
-				my $start_position = $editor->PositionFromLine( $editor->LineFromPosition($position) );
-				my $line           = $editor->GetTextRange( $start_position, $position );
-
-				my $cursor = '$0';
-				for my $e ( keys %$snippets ) {
-					my $v = $snippets->{$e};
-					if ( $line =~ /^\s*\Q$e\E$/ ) {
-						$editor->SetTargetStart( $position - length($e) );
-						$editor->SetTargetEnd($position);
-						my $m = $v;
-						$m =~ s/\$\d//g;
-						$editor->ReplaceTarget($m);
-						if ( $v =~ /(\Q$cursor\E)/g ) {
-							$editor->GotoPos( $position - length($e) + pos($v) - length($cursor) );
-						}
-						$snippet_added = 1;
-						last;
-					}
-				}
-
-
-			}
-
-			# Keep processing it there was snippet completion
-			# Other consume the TAB key down event
-			$event->Skip(1) unless ($snippet_added);
-
-			return;
+			$self->on_key_down(@_);
 		}
 	);
 }
+
+sub on_key_down {
+	my $self   = shift;
+	my $editor = shift;
+	my $event  = shift;
+
+	# Load snippets everytime
+	require YAML::Tiny;
+	require File::ShareDir;
+	my $snippets = YAML::Tiny::LoadFile( File::ShareDir::dist_file( 'Padre-Plugin-Moose', 'snippets.yml' ) );
+
+	# If it is tab key down event, we cycle through snippets
+	# to find a ^match.
+	# If there is a match, we paste the snippet and position the cursor to
+	# the first variable
+	# in this case $0
+
+	#TODO TAB to other variables
+	#TODO draw a box around values
+	my $snippet_added = 0;
+	if ( $event->GetKeyCode == Wx::WXK_TAB ) {
+		my $position       = $editor->GetCurrentPos;
+		my $start_position = $editor->PositionFromLine( $editor->LineFromPosition($position) );
+		my $line           = $editor->GetTextRange( $start_position, $position );
+
+		my $cursor = '$0';
+		for my $e ( keys %$snippets ) {
+			my $v = $snippets->{$e};
+			if ( $line =~ /^\s*\Q$e\E$/ ) {
+				$editor->SetTargetStart( $position - length($e) );
+				$editor->SetTargetEnd($position);
+				my $m = $v;
+				$m =~ s/\$\d//g;
+				$editor->ReplaceTarget($m);
+				if ( $v =~ /(\Q$cursor\E)/g ) {
+					$editor->GotoPos( $position - length($e) + pos($v) - length($cursor) );
+				}
+				$snippet_added = 1;
+				last;
+			}
+		}
+
+
+	}
+
+	# Keep processing it there was snippet completion
+	# Other consume the TAB key down event
+	$event->Skip(1) unless ($snippet_added);
+
+	return;
+}
+
 
 1;
 
