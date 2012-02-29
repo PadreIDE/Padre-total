bb2dbebe1922bec62d08ebadefa19c3583316756
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2012-02-29 12:02:13 +0000

    [P-P-Moose] added private/testcase.pl
    git-svn-id: file:///home/jurosz/dalsi/padre-svn-git/svn-mirror/trunk@18492 18eddd70-d622-4293-bd05-fe65668e8bde

---
+++ Padre-Plugin-Moose/private/testcase.pl
@@ -0,0 +1,24 @@
+use Moose;
+use Moose::Util::TypeConstraints;
+
+BUILD
+DEMOLISH
+after
+around
+augment
+before
+class
+classtype
+coerce
+excludes
+ext
+has
+hasl
+meth
+override
+requires
+role
+script
+subtype
+type
+with
