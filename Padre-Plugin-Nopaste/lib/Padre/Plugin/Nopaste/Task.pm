e5478ffdc2aaeb3ff52594125e48ec139543ac4b
Kevin Dawson <kevin@dawson10.plus.com>
2012-08-24 19:42:15 +0000

    so pp-nopaste now kind of works, but more to do
    git-svn-id: file:///home/mj/devel/padre-svn-git/svn-mirror/trunk@19267 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-Nopaste/lib/Padre/Plugin/Nopaste/Task.pm
+++ Padre-Plugin-Nopaste/lib/Padre/Plugin/Nopaste/Task.pm
@@ -1,13 +1,148 @@
 package Padre::Plugin::Nopaste::Task;
 
-use 5.008;
+use v5.10;
 use strict;
 use warnings;
 use Padre::Task ();
 use Padre::Logger;
 
-our $VERSION = '0.3.1';
-our @ISA     = 'Padre::Task';
+use App::Nopaste 'nopaste';
+use App::Nopaste::Service::Shadowcat;
+
+our $VERSION = '0.4_1';
+
+# our @ISA     = 'Padre::Task';
+use parent qw{ Padre::Task };
+
+# Turn on $OUTPUT_AUTOFLUSH
+local $| = 1;
+use Data::Printer {
+	caller_info => 1,
+	colored     => 1,
+};
+
+# Constructor
+sub new {
+	my $self = shift->SUPER::new(@_);
+
+	# Assert required command parameter
+	unless ( defined $self->{text} ) {
+		die "Failed to provide any text to the Nopaste task\n";
+	}
+
+	return $self;
+}
+
+
+
+# sub prepare {
+# my $self = @_;
+# say 'task prepare';
+# my $main    = $self->{document}->main;
+# my $current = $main->current;
+# my $editor  = $current->editor;
+# return unless $editor;
+
+# p $editor->GetSelectedText;
+# p $editor->GetText;
+
+# # no selection means send current file
+# $self->{text} = $editor->GetSelectedText // $editor->GetText;
+
+# p $self->{text};
+
+# say 'errr';
+# }
+
+sub run {
+	my $self = shift;
+
+	say 'task run';
+
+	$self->process();
+
+	return 1;
+}
+
+sub process {
+	my $self = shift;
+	say 'start task process';
+
+	# require App::Nopaste;
+
+	# say $self->{text};
+
+	# my $url = App::Nopaste::nopaste( $self->{text} );
+
+	# say $url;
+
+	# # show result in output section
+	# if ( defined $url ) {
+	# my $text = "Text successfully nopasted at: $url\n";
+	# $self->{err}     = 0;
+	# $self->{message} = $text;
+	# } else {
+	# my $text = "Error while nopasting text\n";
+	# $self->{err}     = 1;
+	# $self->{message} = $text;
+	# }
+
+
+
+
+	my $url = nopaste(
+
+		# text => "Full text to paste (the only mandatory argument)",
+		text => $self->{text},
+
+		# desc          => "This is a test no-paste",
+		nick => $self->{nick},
+
+		# lang          => "perl",
+		chan => "#padre",
+
+		# private       => 1,                        # default: 0
+		# this is the default, but maybe you want to do something different
+		error_handler => sub {
+			my ( $error, $service ) = @_;
+			warn "$service: $error";
+		},
+		warn_handler => sub {
+			my ( $warning, $service ) = @_;
+			warn "$service: $warning";
+		},
+
+		# you may specify the services to use - but you don't have to
+		services => [ "Shadowcat", ],
+
+		# services => ["Shadowcat", "Gist"],
+	);
+
+	my $output;
+
+	# # show result in output section
+	if ( defined $url ) {
+		$output          = "Text successfully nopasted at: $url\n";
+		$self->{err}     = 0;
+		$self->{message} = $output;
+	} else {
+		$output          = "Error while nopasting text\n";
+		$self->{err}     = 1;
+		$self->{message} = $output;
+	}
+
+	say $output;
+
+	say 'end of task process';
+
+
+
+	return;
+}
+
+1;
+
+__END__
 
 =pod
 
@@ -21,53 +156,6 @@ Padre::Plugin::Nopaste::Task - Padre::Task subclass doing nopaste job
 =head1 DESCRIPTION
 
 Async thread that does real nopaste
-
-=cut
-
-sub prepare {
-    my ($self) = @_;
-
-    my $main     = $self->{document}->main;
-    my $current  = $main->current;
-    my $editor   = $current->editor;
-    return unless $editor;
-
-    # no selection means send current file
-    $self->{text} = $editor->GetSelectedText || $editor->GetText;
-}
-
-sub run {
-	my $self = shift;
-
-	$self->process();
-
-	return 1;
-}
-
-sub process {
-    my ($self) = @_;
-
-    require App::Nopaste;
-    my $url = App::Nopaste::nopaste($self->{text});
-
-    # show result in output section
-    if ( defined $url ) {
-        my $text = "Text successfully nopasted at: $url\n";
-        $self->{err}=0;
-        $self->{message}=$text;
-    } else {
-        my $text = "Error while nopasting text\n";
-        $self->{err}=1;
-        $self->{message}=$text;
-    }
-    return;
-}
-
-1;
-
-__END__
-
-=pod
 
 =head1 Standard Padre::Task API
 
