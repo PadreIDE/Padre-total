37f1ea87c9649081e61e3b832382e741964a8e06
Kevin Dawson <kevin@dawson10.plus.com>
2013-12-16 01:44:20 +0000

    update changes to xs v0.11 to svn also in cpan
    git-svn-id: file:///home/mj/devel/padre-svn-git/svn-mirror/trunk@19820 18eddd70-d622-4293-bd05-fe65668e8bde

--- Padre-Plugin-XS/lib/Padre/Plugin/XS.pm
+++ Padre-Plugin-XS/lib/Padre/Plugin/XS.pm
@@ -1,26 +1,19 @@
 package Padre::Plugin::XS;
 
 use 5.010001;
-use strictures 1;
-
-# use warnings;
-# use strict;
+use warnings;
+use strict;
 
 use Padre::Unload;
-use Padre::Wx ();
-use Padre::Current;
-use Padre::Logger;
-use constant DEBUG => 1;
-use Modern::Perl;
 use Try::Tiny;
 
-our $VERSION = '0.11_01';
+our $VERSION = '0.11';
 use parent qw(Padre::Plugin);
 
 # Child modules we need to unload when disabled
 use constant CHILDREN => qw{
 	Padre::Plugin::XS
-	Padre::Plugin::XS::C
+	Padre::Plugin::XS::PerlXS
 	Padre::Plugin::XS::Document
 	Perl::APIReference
 };
@@ -37,10 +30,10 @@ sub plugin_name {
 #######
 sub padre_interfaces {
 	return (
-		'Padre::Plugin'   => '0.94',
-		'Padre::Document' => '0.94',
-		'Padre::Wx'       => '0.94',
-		'Padre::Logger'   => '0.94',
+		'Padre::Plugin'   => '0.98',
+		'Padre::Document' => '0.98',
+		'Padre::Wx'       => '0.98',
+		'Padre::Logger'   => '0.98',
 	);
 }
 
@@ -82,133 +75,6 @@ sub registered_documents {
 	);
 }
 
-######
-# new api 
-######
-# sub registered_highlighters {
-	# 'Padre::Plugin::XS::C' => {
-		# name => _T('XS HighLighter'),
-		# mime => [
-			# qw{
-				# text/x-csrc
-				# text/x-c++src
-				# text/x-perlxs
-				# }
-		# ],
-		# },
-		# ;
-# }
-
-######
-# old api
-######
-# sub provided_highlighters {
-	# return (
-		# [ 'Padre::Plugin::XS', 'XS highlighter', 'Scintilla C lexer with additional XS keywords' ],
-	# );
-# }
-
-######
-# old api
-######
-# sub highlighting_mime_types {
-	# return (
-		# 'Padre::Plugin::XS' => [
-			# 'text/x-perlxs',
-		# ],
-	# );
-# }
-
-sub colorize {
-	my $self = shift;
-	my $module = $self->highlighter;
-
-	# these are arguments: (maybe use for from/to?)
-	#$current->editor->GetEndStyled,
-	#$event->GetPosition
-TRACE("Call $module") if DEBUG;
-
-	my $doc = Padre::Current->document;
-
-	#my $mime_type = $doc->get_mimetype;
-	my $editor = $doc->editor;
-
-	# TODO we might need not remove all the color, just from a certain section
-	# TODO reuse the $first passed to the method
-	#$doc->remove_color;
-
-	my $c_keywords = join(
-		' ', qw(
-			auto break case char const continue default do
-			double else enum extern float for goto if
-			int long register return short signed sizeof static
-			struct switch typedef union unsigned void volatile while
-			)
-	);
-	my $pp_keywords = join(
-		' ',
-		'#define', '#include', '#if', '#ifdef', '#endif', '#undef',
-		qw(
-			__FILE__ __LINE__ __DATE__ __TIME__
-			__STDC__ __STDC_VERSION__ __STDC_HOSTED__
-			__cplusplus __OBJC__ __ASSEMBLER__
-			__GNUC__ __COUNTER__ __GFORTRAN__
-			__GNUC_MINOR__ __GNUC_PATCHLEVEL__
-			__GNUG__ __STRICT_ANSI__ __ELF__ __VERSION__
-			__OPTIMIZE__ __OPTIMIZE_SIZE__ __NO_INLINE__
-			__GNUC_PATCHLEVEL__
-			)
-	);
-
-	# TODO: there are lots more gnu specific pp keywords...
-
-	my $perl_simple_types = join(
-		' ', qw(
-			I32 U32 STRLEN
-			)
-	);
-	my $perlapi_structs = join(
-		' ', qw(
-			SV AV HV HE IV NV PV OP
-			)
-	);
-
-	# TODO: Add colon where appropriate? Does that work at all?
-	my $xs_keywords = join(
-		' ', qw(
-			MODULE PACKAGE ALIAS
-			OUTPUT RETVAL
-			CODE PPCODE PREFIX
-			INIT NO_INIT PREINIT
-			POSTCALL NO_OUTPUT CLEANUP
-			INPUT SCOPE C_ARGS
-			OUTLIST IN IN_OUTLIST IN_OUT
-			BOOT REQUIRE VERSIONCHECK
-			PROTOTYPES ENABLE DISABLE
-			OVERLOAD FALLBACK INTERFACE
-			INTERFACE_MACRO INCLUDE CASE
-			THIS NO_INIT DESTROY
-			)
-	);
-
-	my $c_keywords_docs = 'FIXME TODO';
-
-	# TODO: cache
-	my $perlapi_keywords = join( ' ', keys %{ $doc->keywords } );
-
-	$editor->SetLexer(Wx::wxSTC_LEX_CPP);
-	$editor->SetProperty( "styling.within.preprocessor", 1 );
-
-	# normal keywords (here: C)
-	$editor->SetKeyWords( 0, $c_keywords . ' ' . $pp_keywords . ' ' . $perl_simple_types );
-
-	# perlapi stuff and xs stuff
-	$editor->SetKeyWords( 1, $perlapi_structs . ' ' . $xs_keywords . ' ' . $perlapi_keywords );
-	$editor->SetKeyWords( 2, $c_keywords_docs );
-	$editor->SetProperty( "braces.cpp.style", 10 );
-
-	return ();
-}
 
 #######
 # Add icon to Plugin
@@ -217,6 +83,7 @@ TRACE("Call $module") if DEBUG;
 sub plugin_icon {
 	require Padre::Wx::Icon;
 	Padre::Wx::Icon::find('logo');
+	return;
 }
 
 
@@ -273,6 +140,21 @@ sub plugin_disable {
 	return 1;
 }
 
+########
+# Composed Method clean_dialog
+########
+sub clean_dialog {
+	my $self = shift;
+
+	# Close the main dialog if it is hanging around
+	if ( $self->{dialog} ) {
+		$self->{dialog}->Hide;
+		$self->{dialog}->Destroy;
+		delete $self->{dialog};
+	}
+
+	return 1;
+}
 
 1;
 
@@ -281,14 +163,22 @@ __END__
 
 =pod
 
+=encoding UTF-8
+
 =head1 NAME
 
 Padre::Plugin::XS - Padre support for perl XS (and perlapi)
 
-=head1 SYNOPSIS
+=head1 VERSION
+
+version: 0.11
+
+=head1 DESCRIPTION
 
 This plugin is intended to extend Padre's support for editing XS
 and C-using-perlapi.
+
+=head1 SYNOPSIS
 
 Currently the plugin implements limited syntax highlighting and
 calltips using a configurable version of the perlapi. After installing
@@ -302,6 +192,36 @@ Once this plug-in is installed the user can switch the highlighting of
 XS files to use the highlighter via the Preferences menu of L<Padre>.
 The default XS syntax highlighting of Padre is abysmal. You're very
 encouraged to enable the C<XS highlighter> feature.
+
+To use this Plugin you need to make sure B<editor_calltips> is enabled in
+Tools-Preferences-Advance
+
+Also see the L<wiki|http://padre.perlide.org/trac/wiki/PadrePluginXS> page.
+
+=head1 METHODS
+
+=over 4
+
+=item * clean_dialog
+
+=item * menu_plugins_simple
+
+=item * padre_interfaces
+
+=item * plugin_about
+
+=item * plugin_disable
+
+=item * plugin_enable
+
+=item * plugin_icon
+
+=item * plugin_name
+
+=item * registered_documents
+
+=back
+
 
 =head1 AUTHORS
 
@@ -318,11 +238,15 @@ Many thanks to Gabor Szabo, who wrote the Kate plugin upon this is based.
 I'm grateful to Herbert Breunung for writing Kephra and getting STC syntax highlighting more
 right that us. Looking at his code has helped me write this.
 
-=head1 COPYRIGHT AND LICENSE
+=head1 COPYRIGHT
 
-This software is copyright (c) 2010-2013 by Steffen Mueller
+Copyright E<copy> 2010-2013 the Padre::Plugin::XS
+L</AUTHOR> and L</CONTRIBUTORS> as listed above.
 
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
